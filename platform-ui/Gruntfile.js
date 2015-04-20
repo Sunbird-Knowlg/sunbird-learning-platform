0a899b6098544713cc49934a550bb461315ee10e
SanthoshVasabhaktula <santhosh@canopusconsulting.com>
2015-04-20 12:48:53 +0530

    grunt task to handle qa/dev builds

--- platform-ui/Gruntfile.js
+++ platform-ui/Gruntfile.js
@@ -9,6 +9,45 @@ module.exports = function(grunt) {
                     output: 'outputdir'
                 }
             }
+        },
+        copy: {
+            dev: {
+                files: [{src: ['conf/appConfig_dev.json'], dest: 'conf/appConfig.json'}]
+            },
+            qa: {
+                files: [{src: ['conf/appConfig_qa.json'], dest: 'conf/appConfig.json'}]
+            },
+            prod: {
+                files: [{src: ['conf/appConfig_prod.json'], dest: 'conf/appConfig.json'}]
+            }
+        },
+        'string-replace': {
+            dev: {
+                files: {
+                  'routes/routes.js': 'routes/routes.js'
+                },
+                options: {
+                    replacements: [{
+                        pattern: '/oauth2callback',
+                        replacement: '/user/google/login/response'
+                    }]
+                }
+            },
+            qa: {
+                files: {
+                  'routes/routes.js': 'routes/routes.js'
+                },
+                options: {
+                    replacements: [{
+                        pattern: '/user/google/login/response',
+                        replacement: '/oauth2callback'
+                    }]
+                }
+            }
         }
     });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-string-replace');
+    grunt.registerTask('default', ['copy:qa','string-replace:qa']);
 };
