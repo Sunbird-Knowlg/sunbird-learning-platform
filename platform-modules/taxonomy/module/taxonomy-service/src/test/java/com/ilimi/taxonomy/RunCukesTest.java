2b4cacd1e8508fba8568d81f11740448d52549de
Mahesh <mahesh@canopusconsulting.com>
2015-05-12 17:43:07 +0530

    remove test files

--- platform-modules/taxonomy/module/taxonomy-service/src/test/java/com/ilimi/taxonomy/RunCukesTest.java
+++
@@ -1,9 +0,0 @@
-package com.ilimi.taxonomy;
-
-import org.junit.runner.RunWith;
-
-import cucumber.api.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-public class RunCukesTest {
-}
--- platform-modules/taxonomy/module/taxonomy-service/src/test/resources/com/ilimi/taxonomy/getAllTaxomomies.feature
+++
@@ -1,6 +0,0 @@
-Feature: Get Taxonomies
-
-  Scenario: Get all taxomomies
-    When I get all taxonomy data
-    Then I will get the resopnse status value 202
-
