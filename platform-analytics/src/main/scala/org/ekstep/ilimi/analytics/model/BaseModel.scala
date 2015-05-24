73f7b45102dc1ebd016b5ed02fd95a5024435710
SanthoshVasabhaktula <santhosh@canopusconsulting.com>
2015-05-24 20:49:06 +0530

    platform-analytics - initial commit

---
+++ platform-analytics/src/main/scala/org/ekstep/ilimi/analytics/model/BaseModel.scala
@@ -0,0 +1,50 @@
+package org.ekstep.ilimi.analytics.model
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.ekstep.ilimi.analytics.conf.AppConf
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.DataFrame
+
+trait BaseModel extends Serializable {
+    
+    var location:String = null;
+    var sc: SparkContext = null;
+    var hiveCtx: HiveContext = null;
+    
+    def validate(args: Array[String]) : Boolean;
+    
+    def compute(args: Array[String]);
+    
+    def initializeSparkContext(location: String) = {
+        this.location = location;
+        val conf = new SparkConf().setMaster("local").setAppName("TestSparkDriver");
+        val sc = new SparkContext(conf);
+        if("S3".equals(location)) {
+            sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", AppConf.getConfig("s3_aws_key"));
+            sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", AppConf.getConfig("s3_aws_secret"));
+        }
+        Console.println("### Spark Context instantiated ###");
+    }
+    
+    def closeSparkContext() {
+        sc.stop();
+    }
+    
+    def getPath(relPath: String) : String = this.location match {
+        case "S3" => AppConf.getConfig("s3_bucket") + relPath;
+        case _ => relPath
+    } 
+        
+    def loadInput(input: String, table: String) {
+        Console.println("### Fetching Input:" + getPath(input) + " ###");
+        val events = hiveCtx.jsonFile(getPath(input)).persist();
+        Console.println("### Data fetched. # of records - " + events.count() + " ###");
+        events.registerTempTable(table);
+    }
+    
+    def queryData(hiveSql: String) : DataFrame = {
+        hiveCtx.sql(hiveSql);
+    }
+
+}
---
+++ platform-analytics/src/main/scala/org/ekstep/ilimi/analytics/model/game/ConceptProficiencyModel.scala
@@ -0,0 +1,87 @@
+package org.ekstep.ilimi.analytics.model.game
+
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.Row
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Map
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
+import org.apache.commons.math3.stat.StatUtils
+import org.ekstep.ilimi.analytics.dao.EffectivenessStatsDAO
+import org.ekstep.ilimi.analytics.conf.AppConf
+import org.apache.spark.sql.hive.HiveContext
+import org.ekstep.ilimi.analytics.model.BaseModel
+
+object ConceptProficiencyModel extends BaseModel {
+
+    /**
+     * Arguments that are required in specific order
+     * 1. input
+     * 2. output
+     * 3. File Location
+     * 4. Game Data - [List of comma separated value of GameId,Before Screener Game, After Screener Game, Concept in the specific order]
+     */
+    def main(args: Array[String]) {
+        AppConf.init();
+        val t1 = System.currentTimeMillis;
+        compute(args);
+        val t2 = System.currentTimeMillis;
+        Console.println("Time taken to compute - " + (t2 - t1) / 1000);
+    }
+
+    def validate(args: Array[String]): Boolean = {
+        Console.println("### Arguments - " + args.mkString(" # ") + " ###");
+        return true;
+    }
+
+    def compute(args: Array[String]) {
+        if (validate(args))
+            computeGameEffectiveness(args(0), args(1), args(2), args(3));
+        else
+            throw new Exception("Invalid arguments")
+    }
+
+    def computeGameEffectiveness(input: String, output: String, location: String, gameData: String) {
+
+        var gd: Array[String] = gameData.split(",");
+        val gameId = gd(0);
+        val beforeScreener = gd(1);
+        val afterScreener = gd(2);
+        val conceptId = gd(3);
+        initializeSparkContext(location);
+        loadInput(input, "telemetry_events");
+        Console.println("### Computing concept improvement stats ###");
+        val validSessions = queryData("SELECT sid FROM telemetry_events where edata.eks.gid in ('" + beforeScreener + "', '" + afterScreener + "')").distinct.collect().map { x => x.getString(0) }
+        val results = queryData("SELECT uid, ts, eid, sid, edata.eks.gid, edata.eks.score, edata.eks.maxscore FROM telemetry_events where eid in ('GE_LAUNCH_GAME', 'GE_GAME_END', 'OE_ASSESS') and sid in ('" + validSessions.mkString("','") + "')");
+        val userPairs = results.map(row => (row.getString(0), Array(row))).reduceByKey((a, b) => a ++ b, 1).mapValues(rows => {
+            rows.sortBy { row => row.getLong(1) };
+            var gameMap: Map[String, (Float, Int)] = Map();
+            var currGame: String = null;
+            rows.foreach { row =>
+                {
+                    row.getString(2) match {
+                        case "GE_LAUNCH_GAME" =>
+                            currGame = row.getString(4);
+                            if (!gameMap.contains(currGame)) {
+                                gameMap(currGame) = (0, 0);
+                            }
+                        case "OE_ASSESS" =>
+                            if (null != currGame) {
+                                var currVal = gameMap(currGame);
+                                gameMap(currGame) = (currVal._1 + (row.getLong(5).floatValue() / row.getLong(6).floatValue()), currVal._2 + 1);
+                            }
+                        case _ => {}
+                    }
+                }
+            }
+
+            ((gameMap(beforeScreener)._1 / gameMap(beforeScreener)._2) * 100, (gameMap(afterScreener)._1 / gameMap(afterScreener)._2) * 100);
+        }).mapValues(f => (f._1, f._2, f._2 - f._1, (f._2 - f._1)/f._1));
+
+        val outputJson = userPairs.map(x => "{\"uid\":" + x._1 + ",\"before_score\":" + x._2._1 + ",\"after_score\":" + x._2._2 + ",\"difference\":" + x._2._3 + "}")
+        Console.println("### Saving Concept Improvement stats to " + getPath(output + "/concept_improvement") + " ###");
+        outputJson.saveAsTextFile(getPath(output + "/concept_improvement"))
+        EffectivenessStatsDAO.saveConceptEffectivness(userPairs.collect(), gameId, conceptId);
+        closeSparkContext;
+    }
+
+}
