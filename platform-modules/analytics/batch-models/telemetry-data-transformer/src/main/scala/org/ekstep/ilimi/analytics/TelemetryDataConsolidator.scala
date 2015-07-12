10a9d1fd958687a46d643573d6e94912bf949e2f
SanthoshVasabhaktula <santhosh@canopusconsulting.com>
2015-07-12 16:28:17 +0530

    Telemetry data transformer module. Convert all the production logs backed by EP into a single file which contains one event per line

---
+++ platform-modules/analytics/batch-models/telemetry-data-transformer/src/main/scala/org/ekstep/ilimi/analytics/TelemetryDataConsolidator.scala
@@ -0,0 +1,17 @@
+package org.ekstep.ilimi.analytics
+
+import org.ekstep.ilimi.analytics.conf.AppConf
+import org.ekstep.ilimi.analytics.util.Application
+import org.ekstep.ilimi.analytics.model.TelemetryDataConsolidatorModel
+
+object TelemetryDataConsolidator extends Application {
+    
+    def main(i: String, o: Option[String], l: Option[String], p: Option[String], outputDir: String) {
+        AppConf.init();
+        val t1 = System.currentTimeMillis;
+        TelemetryDataConsolidatorModel.compute(i, o, outputDir, l.getOrElse("local"), p.getOrElse(AppConf.getConfig("default.parallelization")).toInt)
+        val t2 = System.currentTimeMillis;
+        Console.println("### Model run complete - Time taken to compute - " + (t2 - t1) / 1000 + " ###");
+    }
+
+}
