4249747c4f7b6242cee102207aa8c93d2102eccd
jitendrasinghsankhwar <jitendrasinghsankhwar@gmail.com>
2015-07-29 16:33:59 +0530

    Assessment Item Set load test

--- platform-modules/service/module/ekstep-service/src/test/java/com/ilimi/assessmentservice/load/test/AssessmentItemLoadTest.java
+++ platform-modules/service/module/ekstep-service/src/test/java/com/ilimi/assessmentservice/load/test/AssessmentItemLoadTest.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.lang3.StringUtils;
@@ -78,96 +79,96 @@ public class AssessmentItemLoadTest extends AbstractTestNGSpringContextTests{
     	System.out.println("QuestionIds Size : " + questionIds.size());
     }
     
-//    @Test(threadPoolSize = PS, invocationCount = IC )
-//    public void createQuestionMCQ() {
-//    	MockMvc mockMvc;
-//    	mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-//        String contentString = "{ \"request\": { \"assessment_item\": {\"objectType\": \"AssessmentItem\", \"metadata\": { \"title\": \"Select a char of vowels - 1.\", \"body\": { \"content_type\": \"text/html\", \"content\": \"Select a char of vowels.\" }, \"question_type\": \"mcq\", \"description\": \"GeometryTest\", \"options\": [ { \"content_type\": \"text/html\", \"content\": \"A\", \"is_answer\": true }, { \"content_type\": \"text/html\", \"content\": \"B\", \"is_answer\": false }, { \"content_type\": \"text/html\", \"content\": \"C\", \"is_answer\": false } ], \"code\": \"Q1\", \"difficulty_level\": \"low\", \"num_answers\": 1, \"owner\": \"Ilimi\", \"used_for\": \"assessment\", \"score\": 3, \"max_time\": 120, \"rendering_metadata\": [ { \"interactivity\": [ \"drag-drop\", \"zoom\" ], \"keywords\": [ \"compare\", \"multi-options\" ], \"rendering_hints\": { \"styles\": \"css styles that will override the theme level styles for this one item\", \"view-mode\": \"landscape\" } } ] }, \"outRelations\": [ { \"endNodeId\": \"Num:C1:SC1\", \"relationType\": \"associatedTo\" } ] } } }";
-//        String path = "/assessmentitem";
-//        try {
-//        	long t1 = System.currentTimeMillis();
-//			actions = mockMvc.perform(MockMvcRequestBuilders.post(path).param("taxonomyId", "numeracy").contentType(MediaType.APPLICATION_JSON).content(contentString.getBytes()).header("user-id", "ilimi"));
-//	        long t2 = System.currentTimeMillis();
-//	        sum[0] = sum[0] + (t2 - t1);
-//			Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//
-//        Response resp = jsonToObject(actions);
-//        Assert.assertEquals("successful", resp.getParams().getStatus());
-//		Map<String, Object> result = resp.getResult();
-//		String nodeId = (String) result.get("node_id");
-//		questionIds.add(nodeId);      
-//    } 
-//    
-//    @Test(threadPoolSize = PS, invocationCount = IC )
-//    public void createQuestionMMCQ() {
-//    	long t1 = System.currentTimeMillis();
-//    	MockMvc mockMvc;
-//    	mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-//        String contentString = "{ \"request\": { \"assessment_item\": { \"objectType\": \"AssessmentItem\", \"metadata\": { \"title\": \"Select vowels letters.\", \"body\": { \"content_type\": \"text/html\", \"content\": \"Select vowels letters.\" }, \"question_type\": \"mmcq\", \"description\": \"GeometryTest\", \"options\": [ { \"content_type\": \"text/html\", \"content\": \"A\", \"is_answer\": true }, { \"content_type\": \"text/html\", \"content\": \"B\", \"is_answer\": false }, { \"content_type\": \"text/html\", \"content\": \"C\", \"is_answer\": true }, { \"content_type\": \"text/html\", \"content\": \"E\", \"is_answer\": true } ], \"code\": \"MMCQ1\", \"difficulty_level\": \"low\", \"num_answers\": 3, \"owner\": \"Ilimi\", \"used_for\": \"assessment\", \"score\": 3, \"max_time\": 120, \"rendering_metadata\": [ { \"interactivity\": [ \"drag-drop\", \"zoom\" ], \"keywords\": [ \"compare\", \"multi-options\" ], \"rendering_hints\": { \"styles\": \"css styles that will override the theme level styles for this one item\", \"view-mode\": \"landscape\" } } ] }, \"outRelations\": [ { \"endNodeId\": \"Num:C1:SC1\", \"relationType\": \"associatedTo\" } ] } } }";
-//        String path = "/assessmentitem";
-//        try {
-//			actions = mockMvc.perform(MockMvcRequestBuilders.post(path).param("taxonomyId", "numeracy").contentType(MediaType.APPLICATION_JSON).content(contentString.getBytes()).header("user-id", "ilimi"));
-//			Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//        long t2 = System.currentTimeMillis();
-//        sum[4] = sum[4] + (t2 - t1);
-//    } 
-//    
-//    @Test(threadPoolSize = PS, invocationCount = IC )
-//    public void createQuestionFTB() {
-//    	long t1 = System.currentTimeMillis();
-//    	MockMvc mockMvc;
-//    	mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-//        String contentString = "{ \"request\": { \"assessment_item\": { \"objectType\": \"AssessmentItem\", \"metadata\": { \"title\": \"Write first letter of vowels in english.\", \"body\": { \"content_type\": \"text/html\", \"content\": \"Write first letter of vowels in english.\" }, \"question_type\": \"ftb\", \"description\": \"Literacy Test\", \"answer\": [ \"a\", \"A\" ], \"code\": \"FTB_1\", \"difficulty_level\": \"low\", \"num_answers\": 2, \"owner\": \"Ilimi\", \"used_for\": \"assessment\", \"score\": 3, \"max_time\": 120, \"rendering_metadata\": [ { \"interactivity\": [ \"drag-drop\", \"zoom\" ], \"keywords\": [ \"compare\", \"multi-options\" ], \"rendering_hints\": { \"styles\": \"css styles that will override the theme level styles for this one item\", \"view-mode\": \"landscape\" } } ] }, \"outRelations\": [ { \"endNodeId\": \"Num:C1:SC1\", \"relationType\": \"associatedTo\" } ] } } }";
-//        String path = "/assessmentitem";
-//        try {
-//			actions = mockMvc.perform(MockMvcRequestBuilders.post(path).param("taxonomyId", "numeracy").contentType(MediaType.APPLICATION_JSON).content(contentString.getBytes()).header("user-id", "ilimi"));
-//			Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//        long t2 = System.currentTimeMillis();
-//        sum[5] = sum[5] + (t2 - t1);
-//    } 
-//    
-//    @Test(threadPoolSize = PS, invocationCount = IC )
-//    public void createQuestionMTF() {
-//    	long t1 = System.currentTimeMillis();
-//    	MockMvc mockMvc;
-//    	mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-//        String contentString = "{ \"request\": { \"assessment_item\": { \"objectType\": \"AssessmentItem\", \"metadata\": { \"title\": \"Match the capital letters with small letters\", \"body\": { \"content_type\": \"text/html\", \"content\": \"Select a char of vowels.\" }, \"question_type\": \"mtf\", \"description\": \"GeometryTest\", \"code\": \"MTFQ_1\", \"lhs_options\": [ { \"content_type\": \"text/html\", \"content\": \"K\", \"index\": 4 }, { \"content_type\": \"text/html\", \"content\": \"C\", \"index\": 1 }, { \"content_type\": \"text/html\", \"content\": \"B\", \"index\": 2 }, { \"content_type\": \"text/html\", \"content\": \"D\", \"index\": 3 } ], \"rhs_options\": [ { \"content_type\": \"text/html\", \"content\": \"c\", \"index\": 1 }, { \"content_type\": \"text/html\", \"content\": \"b\", \"index\": 2 }, { \"content_type\": \"text/html\", \"content\": \"d\", \"index\": 3 }, { \"content_type\": \"text/html\", \"content\": \"k\", \"index\": 4 } ], \"difficulty_level\": \"low\", \"num_answers\": 3, \"owner\": \"Ilimi\", \"used_for\": \"assessment\", \"score\": 3, \"max_time\": 120, \"rendering_metadata\": [ { \"interactivity\": [ \"drag-drop\", \"zoom\" ], \"keywords\": [ \"compare\", \"multi-options\" ], \"rendering_hints\": { \"styles\": \"css styles that will override the theme level styles for this one item\", \"view-mode\": \"landscape\" } } ] }, \"outRelations\": [ { \"endNodeId\": \"Num:C1:SC1\", \"relationType\": \"associatedTo\" } ] } } }";
-//        String path = "/assessmentitem";
-//        try {
-//			actions = mockMvc.perform(MockMvcRequestBuilders.post(path).param("taxonomyId", "numeracy").contentType(MediaType.APPLICATION_JSON).content(contentString.getBytes()).header("user-id", "ilimi"));
-//			Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//        long t2 = System.currentTimeMillis();
-//        sum[6] = sum[6] + (t2 - t1);
-//    } 
-//    
-//    @Test(threadPoolSize = PS, invocationCount = IC )
-//    public void createQuestionSpeech() {
-//    	long t1 = System.currentTimeMillis();
-//    	MockMvc mockMvc;
-//    	mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-//        String contentString = "{ \"request\": { \"assessment_item\": { \"objectType\": \"AssessmentItem\", \"metadata\": { \"title\": \"Spell the letter 'a'.\", \"body\": { \"content_type\": \"text/html\", \"content\": \"Spell the letter 'a'.\" }, \"question_type\": \"speech_question\", \"description\": \"Literacy Test\", \"answer\": [ \"http://platform.ekstep.in/sounds/letter_a.mp3\" ], \"code\": \"SPQ_1\", \"difficulty_level\": \"low\", \"num_answers\": 1, \"owner\": \"Ilimi\", \"used_for\": \"assessment\", \"score\": 3, \"max_time\": 120, \"rendering_metadata\": [ { \"interactivity\": [ \"drag-drop\", \"zoom\" ], \"keywords\": [ \"compare\", \"multi-options\" ], \"rendering_hints\": { \"styles\": \"css styles that will override the theme level styles for this one item\", \"view-mode\": \"landscape\" } } ] }, \"outRelations\": [ { \"endNodeId\": \"Num:C1:SC1\", \"relationType\": \"associatedTo\" } ] } } }";
-//        String path = "/assessmentitem";
-//        try {
-//			actions = mockMvc.perform(MockMvcRequestBuilders.post(path).param("taxonomyId", "numeracy").contentType(MediaType.APPLICATION_JSON).content(contentString.getBytes()).header("user-id", "ilimi"));
-//			Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//        long t2 = System.currentTimeMillis();
-//        sum[7] = sum[7] + (t2 - t1);
-//    } 
+    @Test(threadPoolSize = PS, invocationCount = IC )
+    public void createQuestionMCQ() {
+    	MockMvc mockMvc;
+    	mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        String contentString = "{ \"request\": { \"assessment_item\": {\"objectType\": \"AssessmentItem\", \"metadata\": { \"title\": \"Select a char of vowels - 1.\", \"body\": { \"content_type\": \"text/html\", \"content\": \"Select a char of vowels.\" }, \"question_type\": \"mcq\", \"description\": \"GeometryTest\", \"options\": [ { \"content_type\": \"text/html\", \"content\": \"A\", \"is_answer\": true }, { \"content_type\": \"text/html\", \"content\": \"B\", \"is_answer\": false }, { \"content_type\": \"text/html\", \"content\": \"C\", \"is_answer\": false } ], \"code\": \"Q1\", \"difficulty_level\": \"low\", \"num_answers\": 1, \"owner\": \"Ilimi\", \"used_for\": \"assessment\", \"score\": 3, \"max_time\": 120, \"rendering_metadata\": [ { \"interactivity\": [ \"drag-drop\", \"zoom\" ], \"keywords\": [ \"compare\", \"multi-options\" ], \"rendering_hints\": { \"styles\": \"css styles that will override the theme level styles for this one item\", \"view-mode\": \"landscape\" } } ] }, \"outRelations\": [ { \"endNodeId\": \"Num:C1:SC1\", \"relationType\": \"associatedTo\" } ] } } }";
+        String path = "/assessmentitem";
+        try {
+        	long t1 = System.currentTimeMillis();
+			actions = mockMvc.perform(MockMvcRequestBuilders.post(path).param("taxonomyId", "numeracy").contentType(MediaType.APPLICATION_JSON).content(contentString.getBytes()).header("user-id", "ilimi"));
+	        long t2 = System.currentTimeMillis();
+	        sum[0] = sum[0] + (t2 - t1);
+			Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+        Response resp = jsonToObject(actions);
+        Assert.assertEquals("successful", resp.getParams().getStatus());
+		Map<String, Object> result = resp.getResult();
+		String nodeId = (String) result.get("node_id");
+		questionIds.add(nodeId);      
+    } 
+    
+    @Test(threadPoolSize = PS, invocationCount = IC )
+    public void createQuestionMMCQ() {
+    	long t1 = System.currentTimeMillis();
+    	MockMvc mockMvc;
+    	mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        String contentString = "{ \"request\": { \"assessment_item\": { \"objectType\": \"AssessmentItem\", \"metadata\": { \"title\": \"Select vowels letters.\", \"body\": { \"content_type\": \"text/html\", \"content\": \"Select vowels letters.\" }, \"question_type\": \"mmcq\", \"description\": \"GeometryTest\", \"options\": [ { \"content_type\": \"text/html\", \"content\": \"A\", \"is_answer\": true }, { \"content_type\": \"text/html\", \"content\": \"B\", \"is_answer\": false }, { \"content_type\": \"text/html\", \"content\": \"C\", \"is_answer\": true }, { \"content_type\": \"text/html\", \"content\": \"E\", \"is_answer\": true } ], \"code\": \"MMCQ1\", \"difficulty_level\": \"low\", \"num_answers\": 3, \"owner\": \"Ilimi\", \"used_for\": \"assessment\", \"score\": 3, \"max_time\": 120, \"rendering_metadata\": [ { \"interactivity\": [ \"drag-drop\", \"zoom\" ], \"keywords\": [ \"compare\", \"multi-options\" ], \"rendering_hints\": { \"styles\": \"css styles that will override the theme level styles for this one item\", \"view-mode\": \"landscape\" } } ] }, \"outRelations\": [ { \"endNodeId\": \"Num:C1:SC1\", \"relationType\": \"associatedTo\" } ] } } }";
+        String path = "/assessmentitem";
+        try {
+			actions = mockMvc.perform(MockMvcRequestBuilders.post(path).param("taxonomyId", "numeracy").contentType(MediaType.APPLICATION_JSON).content(contentString.getBytes()).header("user-id", "ilimi"));
+			Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        long t2 = System.currentTimeMillis();
+        sum[4] = sum[4] + (t2 - t1);
+    } 
+    
+    @Test(threadPoolSize = PS, invocationCount = IC )
+    public void createQuestionFTB() {
+    	long t1 = System.currentTimeMillis();
+    	MockMvc mockMvc;
+    	mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        String contentString = "{ \"request\": { \"assessment_item\": { \"objectType\": \"AssessmentItem\", \"metadata\": { \"title\": \"Write first letter of vowels in english.\", \"body\": { \"content_type\": \"text/html\", \"content\": \"Write first letter of vowels in english.\" }, \"question_type\": \"ftb\", \"description\": \"Literacy Test\", \"answer\": [ \"a\", \"A\" ], \"code\": \"FTB_1\", \"difficulty_level\": \"low\", \"num_answers\": 2, \"owner\": \"Ilimi\", \"used_for\": \"assessment\", \"score\": 3, \"max_time\": 120, \"rendering_metadata\": [ { \"interactivity\": [ \"drag-drop\", \"zoom\" ], \"keywords\": [ \"compare\", \"multi-options\" ], \"rendering_hints\": { \"styles\": \"css styles that will override the theme level styles for this one item\", \"view-mode\": \"landscape\" } } ] }, \"outRelations\": [ { \"endNodeId\": \"Num:C1:SC1\", \"relationType\": \"associatedTo\" } ] } } }";
+        String path = "/assessmentitem";
+        try {
+			actions = mockMvc.perform(MockMvcRequestBuilders.post(path).param("taxonomyId", "numeracy").contentType(MediaType.APPLICATION_JSON).content(contentString.getBytes()).header("user-id", "ilimi"));
+			Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        long t2 = System.currentTimeMillis();
+        sum[5] = sum[5] + (t2 - t1);
+    } 
+    
+    @Test(threadPoolSize = PS, invocationCount = IC )
+    public void createQuestionMTF() {
+    	long t1 = System.currentTimeMillis();
+    	MockMvc mockMvc;
+    	mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        String contentString = "{ \"request\": { \"assessment_item\": { \"objectType\": \"AssessmentItem\", \"metadata\": { \"title\": \"Match the capital letters with small letters\", \"body\": { \"content_type\": \"text/html\", \"content\": \"Select a char of vowels.\" }, \"question_type\": \"mtf\", \"description\": \"GeometryTest\", \"code\": \"MTFQ_1\", \"lhs_options\": [ { \"content_type\": \"text/html\", \"content\": \"K\", \"index\": 4 }, { \"content_type\": \"text/html\", \"content\": \"C\", \"index\": 1 }, { \"content_type\": \"text/html\", \"content\": \"B\", \"index\": 2 }, { \"content_type\": \"text/html\", \"content\": \"D\", \"index\": 3 } ], \"rhs_options\": [ { \"content_type\": \"text/html\", \"content\": \"c\", \"index\": 1 }, { \"content_type\": \"text/html\", \"content\": \"b\", \"index\": 2 }, { \"content_type\": \"text/html\", \"content\": \"d\", \"index\": 3 }, { \"content_type\": \"text/html\", \"content\": \"k\", \"index\": 4 } ], \"difficulty_level\": \"low\", \"num_answers\": 3, \"owner\": \"Ilimi\", \"used_for\": \"assessment\", \"score\": 3, \"max_time\": 120, \"rendering_metadata\": [ { \"interactivity\": [ \"drag-drop\", \"zoom\" ], \"keywords\": [ \"compare\", \"multi-options\" ], \"rendering_hints\": { \"styles\": \"css styles that will override the theme level styles for this one item\", \"view-mode\": \"landscape\" } } ] }, \"outRelations\": [ { \"endNodeId\": \"Num:C1:SC1\", \"relationType\": \"associatedTo\" } ] } } }";
+        String path = "/assessmentitem";
+        try {
+			actions = mockMvc.perform(MockMvcRequestBuilders.post(path).param("taxonomyId", "numeracy").contentType(MediaType.APPLICATION_JSON).content(contentString.getBytes()).header("user-id", "ilimi"));
+			Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        long t2 = System.currentTimeMillis();
+        sum[6] = sum[6] + (t2 - t1);
+    } 
+    
+    @Test(threadPoolSize = PS, invocationCount = IC )
+    public void createQuestionSpeech() {
+    	long t1 = System.currentTimeMillis();
+    	MockMvc mockMvc;
+    	mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        String contentString = "{ \"request\": { \"assessment_item\": { \"objectType\": \"AssessmentItem\", \"metadata\": { \"title\": \"Spell the letter 'a'.\", \"body\": { \"content_type\": \"text/html\", \"content\": \"Spell the letter 'a'.\" }, \"question_type\": \"speech_question\", \"description\": \"Literacy Test\", \"answer\": [ \"http://platform.ekstep.in/sounds/letter_a.mp3\" ], \"code\": \"SPQ_1\", \"difficulty_level\": \"low\", \"num_answers\": 1, \"owner\": \"Ilimi\", \"used_for\": \"assessment\", \"score\": 3, \"max_time\": 120, \"rendering_metadata\": [ { \"interactivity\": [ \"drag-drop\", \"zoom\" ], \"keywords\": [ \"compare\", \"multi-options\" ], \"rendering_hints\": { \"styles\": \"css styles that will override the theme level styles for this one item\", \"view-mode\": \"landscape\" } } ] }, \"outRelations\": [ { \"endNodeId\": \"Num:C1:SC1\", \"relationType\": \"associatedTo\" } ] } } }";
+        String path = "/assessmentitem";
+        try {
+			actions = mockMvc.perform(MockMvcRequestBuilders.post(path).param("taxonomyId", "numeracy").contentType(MediaType.APPLICATION_JSON).content(contentString.getBytes()).header("user-id", "ilimi"));
+			Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        long t2 = System.currentTimeMillis();
+        sum[7] = sum[7] + (t2 - t1);
+    } 
     
     @Test(threadPoolSize = PS, invocationCount = IC )
     public void createQuestionCanvas() {
