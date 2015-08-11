dd6a723c486291c4fc893a4ecfa275a683be0f88
Rayulu Villa <rayulu@canopusconsulting.com>
2015-08-11 18:33:03 +0530

    simple text plugin without resizing capability

--- quiz-app/public/js/app/plugin/TextPlugin.js
+++ quiz-app/public/js/app/plugin/TextPlugin.js
@@ -1,5 +1,16 @@
 var TextPlugin = Plugin.extend({
 	initPlugin: function(data) {
+		console.log('text: ' + data.$t);
+		var instance = this;
+		var text = new createjs.Text(data.$t || '', data.font || '20px Arial', data.color || '#000000');
+		var dims = this.relativeDims();
+	    text.x = dims.x;
+	    text.y = dims.y;
+	    text.lineWidth = dims.w;
+	    text.textAlign = 'left';
+	    text.textBaseline = 'middle';
+	    this._self = text;
+	    this.render();
 	}
 });
 pluginManager.registerPlugin('text', TextPlugin);
