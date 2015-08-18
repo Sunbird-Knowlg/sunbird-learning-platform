8be5e376a5d582a2334d994f7072f2f6e66bf57c
SanthoshVasabhaktula <santhosh@canopusconsulting.com>
2015-08-18 16:22:50 +0530

    Shape plugin

--- quiz-app/www/js/app/plugin/ShapePlugin.js
+++ quiz-app/www/js/app/plugin/ShapePlugin.js
@@ -1,6 +1,35 @@
 var ShapePlugin = Plugin.extend({
 	_isContainer: false,
     initPlugin: function(data) {
+    	this._self = new createjs.Shape();
+    	var graphics = this._self.graphics;
+    	var dims = this.relativeDims();
+		if(data.fill) {
+			graphics.beginFill(data.fill);
+		}
+		if(data.stroke) {
+			graphics.beginStroke(data.stroke);
+		}
+    	switch(data.type) {
+    		case 'rect':
+    			graphics.dr(dims.x, dims.y, dims.w, dims.h);
+    			if(data.hitArea) {
+		    		var hit = new createjs.Shape();
+					hit.graphics.beginFill("#000").r(dims.x, dims.y, dims.w, dims.h);
+					this._self.hitArea = hit;
+		    	}
+    			break;
+    		case 'circle':
+    			graphics.dc(dims.x, dims.y, dims.w);
+    			if(data.hitArea) {
+		    		var hit = new createjs.Shape();
+					hit.graphics.beginFill("#000").dc(dims.x, dims.y, dims.w);
+					this._self.hitArea = hit;
+		    	}
+    			break;
+    		default:
+    	}
+    	this.render();
     }
 });
 pluginManager.registerPlugin('shape', ShapePlugin);
