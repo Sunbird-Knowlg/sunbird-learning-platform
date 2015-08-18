f387d507358b3774f90f24451aa708f9f22c6f5d
Mahesh <mahesh@canopusconsulting.com>
2015-08-18 14:21:32 +0530

    Audio Plugin changes

--- quiz-app/www/js/app/plugin/AudioPlugin.js
+++ quiz-app/www/js/app/plugin/AudioPlugin.js
@@ -3,25 +3,35 @@ var AudioPlugin = Plugin.extend({
 	_isContainer: false,
 	initPlugin: function(data) {
             var instance = this;
-            var s = new createjs.Bitmap(this._theme.getAsset('sound_image'));
-            var dims = this.relativeDims();
-            var sb = s.getBounds();
-            s.x = dims.x;
-            s.y = dims.y;
-            if (dims.h && dims.h > 0) {
-                s.scaleY = dims.h / sb.height;
-            }
-            if (dims.w && dims.w > 0) {
-                s.scaleX = dims.w / sb.width;
+            var s = null;
+            if(data.parentasset) {
+                var pluginObj = pluginManager.getPluginObject(data.parentasset);
+                if(pluginObj == null) {
+                    console.log('No plugin object:'+data.parentasset);
+                }
+                s = pluginObj._self;
+            } else {
+                s = new createjs.Bitmap(this._theme.getAsset('sound_image'));
+                var dims = this.relativeDims();
+                var sb = s.getBounds();
+                s.x = dims.x;
+                s.y = dims.y;
+                if (dims.h && dims.h > 0) {
+                    s.scaleY = dims.h / sb.height;
+                }
+                if (dims.w && dims.w > 0) {
+                    s.scaleX = dims.w / sb.width;
+                }    
             }
             // createjs.Sound.registerSound("assets/thunder.mp3", data.asset);
-            var eventName = data.event || "click";
-            var asset = this._theme.getAsset(data.asset);
-            s.addEventListener(eventName, function(event) {
-                createjs.Sound.play(data.asset);
-            });
-            this._self = s;
-            if (!data.hide) {
+            var events = (data.event)?data.event.split(",") : ["click"];
+            for (var i = 0; i < events.length; i++) {
+                s.addEventListener(events[i], function(event) {
+                    createjs.Sound.play(data.asset, createjs.Sound.INTERRUPT_NONE);
+                });
+            };
+            if(!data.parentasset) {
+                this._self = s;
                 this.render();
             }
 	}
