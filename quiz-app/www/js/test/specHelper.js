885adce944c4af760d644b12abd944cb97e37fee
Purnima Tammireddy <purnimat07@gmail.com>
2015-08-31 23:42:18 +0530

    helper library to simulate events on html5 canvas objects

--- quiz-app/www/js/test/specHelper.js
+++ quiz-app/www/js/test/specHelper.js
@@ -1,4 +1,61 @@
+
 function clickOnObject(id) {
-	var stage = PluginManager.getPluginObject(id);
-	stage._self.dispatchEvent('click');
+	var obj = PluginManager.getPluginObject(id);
+	obj._self.dispatchEvent('click');
 }
+
+function getOriginalXY(id){
+	var obj = PluginManager.getPluginObject(id);
+	origX = obj._self.getOrigX;
+	origY = obj._self.getOrigY;
+	return origX, origY;
+}
+
+function isDisplayed(id){
+	var obj = PluginManager.getPluginObject(id);
+	return obj._self.isVisible();
+}
+
+function getAudioState(audioObjectId){
+	var obj = PluginManager.getPluginObject(audioObjectId);
+	return obj.state;
+}
+
+
+function isAudioPlaying(audioObjectId){
+	if (getAudioState(audioObjectId) == "play"){
+		return True;
+	}
+	else
+		return False;
+}
+
+function isAudioStopped(audioObjectId){
+	if(getAudioState(audioObjectId)=="stop"){
+		return True;
+	}
+	else
+		return False;
+}
+
+function getAudioPlayState(audioObjectId){
+	var obj = PluginManager.getPluginObject(audioObjectId);
+	return obj._self.playState;
+}
+
+function isPlayStateFinished(audioObjectId){
+	if (getAudioPlayState(audioObjectId) == "playFinished"){
+		return True;
+	}
+	else
+		return False;
+}
+
+function isPlayStateSucceeded(audioObjectId){
+	if (getAudioPlayState(audioObjectId) == "playSucceeded"){
+		return True;
+	}
+	else
+		return False;
+}
+
