c3425e4b4af296634a56ee75c87224a1d1a6121f
jitendrasinghsankhwar <jitendrasinghsankhwar@gmail.com>
2015-09-23 15:40:22 +0530

    Exit button implementation..

--- quiz-app/public/js/app/app.js
+++ quiz-app/public/js/app/app.js
@@ -226,6 +226,15 @@ angular.module('quiz', ['ionic', 'ngCordova', 'quiz.services'])
             if (_.isString(env) && env.length > 0) {
                 PlatformService.setAPIEndpoint(env);
             }
+        };
+        $scope.exitApp = function(){
+            console.log("Enter");
+            if(navigator.app)
+                navigator.app.exitApp();
+            if(navigator.device)
+                navigator.device.exitApp();
+            if(window)
+                window.close();
         }
 
     }).controller('ContentCtrl', function($scope, $http, $cordovaFile, $cordovaToast, $ionicPopover, $state, ContentService, $stateParams) {
