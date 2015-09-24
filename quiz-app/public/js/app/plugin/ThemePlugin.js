a851ec75adcc869915c4c820eea9e9060addd77b
Rayulu Villa <rayulu@ilimi.in>
2015-09-24 12:13:48 +0530

    change OE_INTERRUPT events for backend

--- quiz-app/public/js/app/plugin/ThemePlugin.js
+++ quiz-app/public/js/app/plugin/ThemePlugin.js
@@ -274,10 +274,10 @@ var ThemePlugin = Plugin.extend({
         if(this._currentStage) {
             AssetManager.stopStageAudio(this._currentStage);
         }
-        TelemetryService.interrupt("BACKGROUND", this._currentStage);
+        TelemetryService.interrupt("IDLE", this._currentStage);
     },
     resume: function() {
-        TelemetryService.interrupt("RESUME", this._currentStage);
+        //TelemetryService.interrupt("RESUME", this._currentStage);
     }
 });
 PluginManager.registerPlugin('theme', ThemePlugin);
