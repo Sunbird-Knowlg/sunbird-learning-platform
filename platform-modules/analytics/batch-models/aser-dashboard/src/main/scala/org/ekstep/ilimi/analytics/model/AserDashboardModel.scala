8ede477b7b5d4ae43d9d5c3127ec45108c730050
Santhosh Vasabhaktula <Santhosh@Santhoshs-MacBook-Pro.local>
2015-09-30 10:40:02 +0530

    module for aser dashboard/reports

---
+++ platform-modules/analytics/batch-models/aser-dashboard/src/main/scala/org/ekstep/ilimi/analytics/model/AserDashboardModel.scala
@@ -0,0 +1,99 @@
+package org.ekstep.ilimi.analytics.model
+
+import scala.collection.mutable.Buffer
+import org.json4s.DefaultFormats
+import org.json4s.Extraction
+import org.json4s.jackson.JsonMethods.parse
+import org.ekstep.ilimi.analytics.util.CommonUtil
+import java.io.File
+import org.ekstep.ilimi.analytics.util.S3Util
+import org.ekstep.ilimi.analytics.conf.AppConf
+import java.io.FileWriter
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Calendar
+import org.joda.time.Days
+import org.joda.time.LocalDate
+import scala.collection.immutable.IndexedSeq
+import org.apache.spark.SparkContext
+import org.apache.spark.Accumulator
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkFiles
+
+object AserDashboardModel extends Serializable {
+
+    def compute(job: Job) {
+
+        val config = job.config.getOrElse(Map[String, String]());
+        val p = CommonUtil.getParallelization(config);
+        @transient val sc = CommonUtil.getSparkContext(p, "GenericDashboard");
+        if(job.rscript.nonEmpty) {
+            sc.addFile(job.rscript.get);
+        }
+        
+        val rdd = executeQuery(job.query, config, sc, p);
+        val filterRdd = filter(job.filter, rdd);
+        val sortedRdd = sort(job.sort, filterRdd);
+        if(job.rscript.nonEmpty) {
+            val pipedRdd = sortedRdd.pipe(SparkFiles.get(job.rscript.get));
+            pipedRdd.collect();
+        } else {
+            Console.println("Count of events - " + sortedRdd.count());
+        }
+        CommonUtil.closeSparkContext(sc);
+    }
+
+    def executeQuery(query: Query, config: Map[String, String], sc: SparkContext, p: Int): RDD[Event] = {
+        var path = config.getOrElse("input", "") + "/*";
+        if (query.dateFilter.nonEmpty) {
+            path = CommonUtil.getInputPaths(config.getOrElse("input", ""), config.getOrElse("suffix", ""), query.dateFilter.get.from, query.dateFilter.get.to).mkString(",");
+        }
+        Console.println("## Path - " + path + " ##");
+        val rdd = sc.textFile(path, p).cache();
+        rdd.map { line =>
+            {
+                implicit val formats = DefaultFormats;
+                parse(line).extract[Event]
+            }
+        }
+    }
+
+    def filter(filter: Option[Filter], rdd: RDD[Event]): RDD[Event] = {
+        var filterRDD = rdd;
+        if (filter.nonEmpty) {
+            if (filter.get.contentId.nonEmpty) {
+                filterRDD = rdd.filter { x =>
+                    {
+                        CommonUtil.getGameId(x).equals(filter.get.contentId.get);
+                    }
+                }
+            }
+            if (filter.get.eventIds.nonEmpty) {
+                val validEvents = filter.get.eventIds.get.split(',');
+                filterRDD = rdd.filter { x =>
+                    {
+                        validEvents.contains(CommonUtil.getEventId(x));
+                    }
+                }
+            }
+        }
+        filterRDD;
+    }
+
+    def sort(sort: Option[Sort], rdd: RDD[Event]): RDD[Event] = {
+        var sortedRdd = rdd;
+        if (sort.nonEmpty) {
+            if (sort.get.by.nonEmpty) {
+                val ascending = sort.get.order.getOrElse("asc").equals("asc");
+                sortedRdd = sort.get.by.get match {
+                    case "uid" =>
+                        rdd.sortBy(f => CommonUtil.getUserId(f), ascending, 1);
+                    case "ts" =>
+                        rdd.sortBy(f => f.ts.get, ascending, 1);
+                }
+            }
+        }
+        sortedRdd;
+    }
+
+}
