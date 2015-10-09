cfb033369bf7d987fbea26fa30b110229e7c9094
jitendrasinghsankhwar <jitendrasinghsankhwar@gmail.com>
2015-10-09 17:19:05 +0530

    Deleted Quiz-App from Learning-Plateform

--- quiz-app/public/js/app/plugin/ManifestPlugin.js
+++
@@ -1,6 +0,0 @@
-var ManifestPlugin = Plugin.extend({
-	_render: false,
-	initPlugin: function(data) {
-	}
-});
-PluginManager.registerPlugin('manifest', ManifestPlugin);
--- quiz-app/public/js/app/telemetry/InActiveEvent.js
+++
@@ -1,9 +0,0 @@
-InActiveEvent = Class.extend({
-    init: function() {},
-    ext: function() {return this;},
-    flush: function() {},
-    __noSuchMethod__: function() {
-        console.log('TelemetryService is inActive');
-        return this;
-    }
-})
