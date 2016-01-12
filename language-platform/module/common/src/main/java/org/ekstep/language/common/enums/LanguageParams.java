15f7a5adf01906b9337f161500cbaa8cfb0ada16
Rayulu Villa <rayulu@ilimi.in>
2016-01-12 23:51:09 +0530

    Use RequestRouter in Tools Controller

--- language-platform/module/common/src/main/java/org/ekstep/language/common/enums/LanguageParams.java
+++ language-platform/module/common/src/main/java/org/ekstep/language/common/enums/LanguageParams.java
@@ -2,5 +2,5 @@ package org.ekstep.language.common.enums;
 
 public enum LanguageParams {
 
-    word, text, language_id, word_complexity, text_complexity;
+    word, text, language_id, word_complexity, text_complexity, words, texts;
 }
--- language-platform/module/services/rest-service/src/main/java/org/ekstep/language/controller/ToolsController.java
+++ language-platform/module/services/rest-service/src/main/java/org/ekstep/language/controller/ToolsController.java
@@ -1,92 +1,77 @@
 package org.ekstep.language.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.ekstep.language.common.LanguageMap;
+import org.ekstep.language.common.enums.LanguageActorNames;
+import org.ekstep.language.common.enums.LanguageOperations;
+import org.ekstep.language.common.enums.LanguageParams;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.ilimi.common.controller.BaseController;
 import com.ilimi.common.dto.Request;
 import com.ilimi.common.dto.Response;
-import com.ilimi.common.exception.ClientException;
-import com.ilimi.graph.dac.model.Node;
-import com.ilimi.taxonomy.enums.ContentAPIParams;
 
 @Controller
 @RequestMapping("v1/language/tools")
-public class ToolsController extends BaseController {
+public class ToolsController extends BaseLanguageController {
 
     private static Logger LOGGER = LogManager.getLogger(ToolsController.class.getName());
 
+    @SuppressWarnings("unchecked")
     @RequestMapping(value = "/lexileMeasures", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<Response> complexity(
-            @RequestParam(value = "languageId", required = true, defaultValue = "") String languageId,
-            @RequestBody Map<String, Object> map) {
+    public ResponseEntity<Response> complexity(@RequestBody Map<String, Object> map) {
         String apiId = "language.complexity";
-        if (LanguageMap.containsLanguage(languageId)) {
-            apiId = "language.complexity";
-            Request request = getRequestObject(map);
-            LOGGER.info("List | Request: " + request);
-            try {
-                Response response = null;
-                LOGGER.info("List | Response: " + response);
-                return getResponseEntity(response, apiId,
-                        (null != request.getParams()) ? request.getParams().getMsgid() : null);
-            } catch (Exception e) {
-                LOGGER.error("List | Exception: " + e.getMessage(), e);
-                return getExceptionResponseEntity(e, apiId,
-                        (null != request.getParams()) ? request.getParams().getMsgid() : null);
-            }
-        } else {
-            return getExceptionResponseEntity(new ClientException("ERR_INVALID_LANGUAGE_ID", "Language ID is invalid."),
-                    apiId, null);
-        }
-    }
-
-    @SuppressWarnings({ "unchecked", "unused" })
-    private Request getListRequestObject(Map<String, Object> requestMap) {
-        Request request = getRequest(requestMap);
-        if (null != requestMap && !requestMap.isEmpty()) {
-            Object requestObj = requestMap.get("request");
-            if (null != requestObj) {
-                try {
-                    ObjectMapper mapper = new ObjectMapper();
-                    String strRequest = mapper.writeValueAsString(requestObj);
-                    Map<String, Object> map = mapper.readValue(strRequest, Map.class);
-                    request.setRequest(map);
-                } catch (Exception e) {
-                    e.printStackTrace();
+        apiId = "language.complexity";
+        Request request = getRequestObject(map);
+        String language = (String) request.get(LanguageParams.language_id.name());
+        // TODO: throw an exception is language value is blank
+        
+        request.getContext().put(LanguageParams.language_id.name(), language);
+        LOGGER.info("List | Request: " + request);
+        try {
+            List<Request> requests = new ArrayList<Request>();
+            List<String> words = (List<String>) request.get(LanguageParams.words.name());
+            if (null != words && words.size() > 0) {
+                for (String word : words) {
+                    Request req = new Request(request);
+                    req.setManagerName(LanguageActorNames.LEXILE_MEASURES_ACTOR.name());
+                    req.setOperation(LanguageOperations.getWordComplexity.name());
+                    req.put(LanguageParams.language_id.name(), language);
+                    req.put(LanguageParams.word.name(), word);
+                    requests.add(req);
                 }
             }
-        }
-        return request;
-    }
-
-    private Request getRequestObject(Map<String, Object> requestMap) {
-        Request request = getRequest(requestMap);
-        Map<String, Object> map = request.getRequest();
-        ObjectMapper mapper = new ObjectMapper();
-        if (null != map && !map.isEmpty()) {
-            try {
-                Object obj = map.get(ContentAPIParams.content.name());
-                if (null != obj) {
-                    Node content = (Node) mapper.convertValue(obj, Node.class);
-                    request.put(ContentAPIParams.content.name(), content);
+            
+            List<String> texts = (List<String>) request.get(LanguageParams.texts.name());
+            if (null != texts && texts.size() > 0) {
+                for (String text : texts) {
+                    Request req = new Request(request);
+                    req.setManagerName(LanguageActorNames.LEXILE_MEASURES_ACTOR.name());
+                    req.setOperation(LanguageOperations.getTextComplexity.name());
+                    req.put(LanguageParams.language_id.name(), language);
+                    req.put(LanguageParams.text.name(), text);
+                    requests.add(req);
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+            
+            // TODO: send all requests to request router and get the response
+            Response response = null;
+            LOGGER.info("List | Response: " + response);
+            return getResponseEntity(response, apiId,
+                    (null != request.getParams()) ? request.getParams().getMsgid() : null);
+        } catch (Exception e) {
+            LOGGER.error("List | Exception: " + e.getMessage(), e);
+            return getExceptionResponseEntity(e, apiId,
+                    (null != request.getParams()) ? request.getParams().getMsgid() : null);
         }
-        return request;
     }
 }
