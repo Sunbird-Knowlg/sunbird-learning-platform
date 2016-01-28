54a26de7b34a8a5654245d2107a178cc22e6e0fa
Rayulu Villa <rayulu@ilimi.in>
2016-01-28 23:49:27 +0530

    round complexity measures to two decimals

--- language-platform/module/tools/lexile-algorithms/src/main/java/org/ekstep/language/measures/ParagraphMeasures.java
+++ language-platform/module/tools/lexile-algorithms/src/main/java/org/ekstep/language/measures/ParagraphMeasures.java
@@ -1,5 +1,6 @@
 package org.ekstep.language.measures;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,39 +12,48 @@ import org.ekstep.language.util.LanguageUtil;
 
 public class ParagraphMeasures {
 
-	public static ParagraphComplexity getTextComplexity(String language, String text) {
-	    if (!SyllableMap.isLanguageEnabled(language))
+    public static ParagraphComplexity getTextComplexity(String language, String text) {
+        if (!SyllableMap.isLanguageEnabled(language))
             return null;
-		if (StringUtils.isNotBlank(text)) {
-			List<String> tokens = LanguageUtil.getTokens(text);
-			List<WordComplexity> wordMeasures = new ArrayList<WordComplexity>();
-			if (null != tokens && !tokens.isEmpty()) {
-				for (String word : tokens) {
-					WordComplexity wc = WordMeasures.getWordComplexity(language, word);
-					wordMeasures.add(wc);
-				}
-			}
-			ParagraphComplexity pc = new ParagraphComplexity();
-			pc.setText(text);
-			pc.setWordMeasures(wordMeasures);
-			computeMeans(pc);
-			return pc;
-		} else {
-			return null;
-		}
-	}
+        if (StringUtils.isNotBlank(text)) {
+            List<String> tokens = LanguageUtil.getTokens(text);
+            List<WordComplexity> wordMeasures = new ArrayList<WordComplexity>();
+            if (null != tokens && !tokens.isEmpty()) {
+                for (String word : tokens) {
+                    WordComplexity wc = WordMeasures.getWordComplexity(language, word);
+                    wordMeasures.add(wc);
+                }
+            }
+            ParagraphComplexity pc = new ParagraphComplexity();
+            pc.setText(text);
+            pc.setWordMeasures(wordMeasures);
+            computeMeans(pc);
+            return pc;
+        } else {
+            return null;
+        }
+    }
 
-	private static void computeMeans(ParagraphComplexity pc) {
-		int count = pc.getWordMeasures().size();
-		double orthoComplexity = 0;
-		double phonicComplexity = 0;
-		for (WordComplexity wc : pc.getWordMeasures()) {
-			orthoComplexity += wc.getOrthoComplexity();
-			phonicComplexity += wc.getPhonicComplexity();
-		}
-		pc.setTotalOrthoComplexity(orthoComplexity);
-		pc.setTotalPhonicComplexity(phonicComplexity);
-		pc.setMeanOrthoComplexity(orthoComplexity / count);
-		pc.setMeanPhonicComplexity(phonicComplexity / count);
-	}
+    private static void computeMeans(ParagraphComplexity pc) {
+        int count = pc.getWordMeasures().size();
+        double orthoComplexity = 0;
+        double phonicComplexity = 0;
+        for (WordComplexity wc : pc.getWordMeasures()) {
+            orthoComplexity += wc.getOrthoComplexity();
+            phonicComplexity += wc.getPhonicComplexity();
+        }
+        pc.setTotalOrthoComplexity(formatDoubleValue(orthoComplexity));
+        pc.setTotalPhonicComplexity(formatDoubleValue(phonicComplexity));
+        pc.setMeanOrthoComplexity(formatDoubleValue(orthoComplexity / count));
+        pc.setMeanPhonicComplexity(formatDoubleValue(phonicComplexity / count));
+    }
+
+    private static Double formatDoubleValue(Double d) {
+        if (null != d) {
+            BigDecimal bd = new BigDecimal(d);
+            bd = bd.setScale(2, BigDecimal.ROUND_HALF_UP);
+            return bd.doubleValue();
+        }
+        return d;
+    }
 }
