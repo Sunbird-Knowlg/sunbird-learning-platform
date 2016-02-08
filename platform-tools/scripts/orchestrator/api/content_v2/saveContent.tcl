c03b88728edfd4b46b125532eb43a6943c25ad22
Rayulu Villa <rayulu@ilimi.in>
2016-02-08 17:03:51 +0530

    check for osId and contentType in content create and update API

--- platform-tools/scripts/orchestrator/api/content_v2/saveContent.tcl
+++ platform-tools/scripts/orchestrator/api/content_v2/saveContent.tcl
@@ -3,11 +3,55 @@ java::import -package java.util ArrayList List
 java::import -package java.util HashMap Map
 java::import -package com.ilimi.graph.dac.model Node
 
+proc proc_isEmpty {value} {
+	set exist false
+	java::try {
+		set hasValue [java::isnull $value]
+		if {$hasValue == 1} {
+			set exist true
+		} else {
+			set strValue [$value toString]
+			set newStrValue [java::new String $strValue] 
+			set strLength [$newStrValue length]
+			if {$strLength == 0} {
+				set exist true
+			}
+		}
+	} catch {Exception err} {
+    	set exist true
+	}
+	return $exist
+}
+
 set object_type "Content"
 set graph_id "domain"
 set resp_def_node [getDefinition $graph_id $object_type]
 set def_node [get_resp_value $resp_def_node "definition_node"]
 $content put "objectType" $object_type
-set domain_obj [convert_to_graph_node $content $def_node]
-set create_response [createDataNode $graph_id $domain_obj]
-return $create_response
+
+set osId_Error false
+set contentType [$content get "contentType"]
+set contentTypeEmpty [proc_isEmpty $contentType]
+if {!$contentTypeEmpty} {
+	set osId [$content get "osId"]
+	set osIdEmpty [proc_isEmpty $osId]
+	set osIdCheck [[java::new String [$contentType toString]] equalsIgnoreCase "Asset"]
+	if {$osIdCheck != 1 && $osIdEmpty} {
+		set osId_Error true
+	}
+	if {$osId_Error} {
+		set result_map [java::new HashMap]
+		$result_map put "error" "OSId cannot be empty"
+		set response_list [create_response $result_map]
+		return $response_list
+	} else {
+		set domain_obj [convert_to_graph_node $content $def_node]
+		set create_response [createDataNode $graph_id $domain_obj]
+		return $create_response
+	}
+} else {
+	set result_map [java::new HashMap]
+	$result_map put "error" "Content Type cannot be empty"
+	set response_list [create_response $result_map]
+	return $response_list
+}
