5672577731b0a15ff25d4f2dacf1c745fbfcc727
amarnath.gandhi <amarnath.gandhi@tarento.com>
2016-02-09 13:44:46 +0530

    Language map preoperties

--- language-platform/module/common/src/main/java/org/ekstep/language/common/LanguageMap.java
+++ language-platform/module/common/src/main/java/org/ekstep/language/common/LanguageMap.java
@@ -1,34 +1,51 @@
 package org.ekstep.language.common;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.ekstep.language.util.PropertiesUtil;
 
 public class LanguageMap {
 
-    private static Map<String, String> languageMap = new HashMap<String, String>();
+	private static Map<String, String> languageMap = new HashMap<String, String>();
+	private static Properties prop = new Properties();
+	private static InputStream input = null;
+	private static Logger LOGGER = LogManager.getLogger(PropertiesUtil.class.getName());
 
-    static {
-        languageMap.put("hi", "hindi");
-        languageMap.put("en", "english");
-        languageMap.put("te", "telugu");
-        languageMap.put("ka", "kannada");
-        languageMap.put("ta", "tamil");
-        languageMap.put("test", "testLanguage");
-        languageMap.put("testone", "testone");
-        languageMap.put("testoneload", "testoneload");
-        languageMap.put("testload", "testLoadLanguage");
-        languageMap.put("testdictionary", "testdictionary");
-        languageMap.put("testcreatedictionary", "testcreatedictionary");
-        languageMap.put("testsearch", "testsearch");
-        languageMap.put("testparser", "testparser");
-        
-    }
+	static {
+		String filename = "language-map.properties";
+		input = PropertiesUtil.class.getClassLoader().getResourceAsStream(filename);
+		if (input == null) {
+			LOGGER.error("Unable to find " + filename);
+		}
+		try {
+			prop.load(input);
+			Set<Object> keys = prop.keySet();
+			for (Object k : keys) {
+				String key = (String) k;
+				String value = (String) getProperty(key);
+				languageMap.put(key, value);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static boolean containsLanguage(String languageId) {
-        return languageMap.containsKey(languageId);
-    }
+	public static String getProperty(String key) {
+		return prop.getProperty(key);
+	}
 
-    public static String getLanguage(String languageId) {
-        return languageMap.get(languageId);
-    }
+	public static boolean containsLanguage(String languageId) {
+		return languageMap.containsKey(languageId);
+	}
+
+	public static String getLanguage(String languageId) {
+		return languageMap.get(languageId);
+	}
 }
