504cdb839d04145cdf2d5107599a73f6e6e7b90d
amarnath.gandhi <amarnath.gandhi@tarento.com>
2016-02-16 11:34:57 +0530

    Wrodlists orchestrator scripts fixes

--- language-platform/module/scripts/orchestrator/api/get-word-lists.tcl
+++ language-platform/module/scripts/orchestrator/api/get-word-lists.tcl
@@ -57,6 +57,23 @@ proc getWordLemmas {language_id word_id_list} {
 	}
 }
 
+set contains_response [containsLanguage $language_id]
+set contains_response_error [check_response_error $contains_response]
+if {$contains_response_error} {
+	puts "Error response from containsLanguage"
+	return $contains_response;
+}
+set result [$contains_response get "result"]
+set lang_eqs [$result equals "true"]
+if {!$lang_eqs} {
+	set result_map [java::new HashMap]
+	$result_map put "code" "INVALID_LANGUAGE"
+	$result_map put "message" "INVALID LANGUAGE"
+	$result_map put "responseCode" [java::new Integer 400]
+	set err_response [create_error_response $result_map]
+	return $err_response
+}
+
 set lemma_list [java::new ArrayList]
 set object_type "WordList"
 
