29dd96ef1703db1a438481bf8023aea6d7a8f5ce
Mohammad Azharuddin <azhar@ilimi.in>
2016-02-23 13:22:44 +0530

    Content Mime type Factory Created.

---
+++ platform-modules/manager/src/main/java/com/ilimi/taxonomy/content/ContentMimeTypeFactory.java
@@ -0,0 +1,40 @@
+package com.ilimi.taxonomy.content;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.ilimi.taxonomy.mgr.IMimeTypeManager;
+
+public class ContentMimeTypeFactory {
+	@Autowired @Qualifier("ECMLMimeTypeMgrImpl") IMimeTypeManager ECMLMimeTypeMgr;
+    @Autowired @Qualifier("HTMLMimeTypeMgrImpl") IMimeTypeManager HTMLMimeTypeMgr;
+    @Autowired @Qualifier("APKMimeTypeMgrImpl") IMimeTypeManager APKMimeTypeMgr;
+    @Autowired @Qualifier("CollectionMimeTypeMgrImpl") IMimeTypeManager CollectionMimeTypeMgr;
+    @Autowired @Qualifier("AssetsMimeTypeMgrImpl") IMimeTypeManager AssetsMimeTypeMgr;
+    public IMimeTypeManager getImplForService(String mimeType){
+    	IMimeTypeManager manager = ECMLMimeTypeMgr;
+    	switch (StringUtils.lowerCase(mimeType)) {
+			case "ecml":
+				manager = ECMLMimeTypeMgr;
+				break;
+			case "html":
+				manager = HTMLMimeTypeMgr;
+				break;
+			case "apk":
+				manager = APKMimeTypeMgr;
+				break;
+			case "collection":
+				manager = APKMimeTypeMgr;
+				break;
+			case "assets":
+				manager = APKMimeTypeMgr;
+				break;
+			default:
+				manager = ECMLMimeTypeMgr;
+				break;
+		}
+       return manager;
+    }
+}
+
