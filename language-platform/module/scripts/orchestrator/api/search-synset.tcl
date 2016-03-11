c44dde1e93b34fd4da125d75b96dc9926523061b
amarnath.gandhi <amarnath.gandhi@tarento.com>
2016-03-11 12:19:40 +0530

    Synsets API

--- language-platform/module/scripts/orchestrator/api/search-synset.tcl
+++ language-platform/module/scripts/orchestrator/api/search-synset.tcl
@@ -3,27 +3,80 @@ java::import -package java.util ArrayList List
 java::import -package java.util HashMap Map
 java::import -package com.ilimi.graph.dac.model Node
 
-set lemma_list [java::new ArrayList]
-set object_type "Synset"
-set error_status "Failed"
-
-set contains_response [containsLanguage $language_id]
-set contains_response_error [check_response_error $contains_response]
-if {$contains_response_error} {
-	puts "Error response from containsLanguage"
-	return $contains_response;
-}
-set result [$contains_response get "result"]
-set lang_eqs [$result equals "true"]
-if {!$lang_eqs} {
+set object_null [java::isnull $search]
+if {$object_null == 1} {
 	set result_map [java::new HashMap]
-	$result_map put "code" "INVALID_LANGUAGE"
-	$result_map put "message" "INVALID LANGUAGE"
+	$result_map put "code" "ERR_INVALID_SEARCH_REQUEST"
+	$result_map put "message" "Invalid Search Request"
 	$result_map put "responseCode" [java::new Integer 400]
-	set err_response [create_error_response $result_map]
-	return $err_response
+	set response_list [create_error_response $result_map]
+	return $response_list
+} else {
+	set invalidObjectType false
+	set object_type_param [$search get "objectType"]
+	set object_type_param_null [java::isnull $object_type_param]
+	if {$object_type_param_null == 0} {
+		set str_object_type_param [$object_type_param toString]
+		if {$str_object_type_param != "Synset"} {
+			set invalidObjectType true
+		}
+	} 
+	if {$invalidObjectType} {
+		set result_map [java::new HashMap]
+		$result_map put "code" "ERR_SYNSET_NOT_FOUND"
+		$result_map put "message" "No synset found"
+		$result_map put "responseCode" [java::new Integer 404]
+		set response_list [create_error_response $result_map]
+		return $response_list
+	} else {
+		set object_type "Synset"
+		set check_null [java::isnull $search]
+		if {$search == 1} {
+			set $search [java::new HashMap]
+		}
+
+		$search put "objectType" $object_type
+		$search put "nodeType" "DATA_NODE"
+
+		set sort [$search get "sort"]
+		set limit [$search get "limit"]
+		$search put "sortBy" $sort
+		$search put "resultSize" $limit
+
+		$search remove "sort"
+		$search remove "limit"
+
+		set returnFields false
+		set fieldList [$search get "fields"]
+		set is_fieldList_null [java::isnull $fieldList]
+		if {$is_fieldList_null == 0} {
+			$search remove "fields"
+			set returnFields true
+		}
+
+		set search_criteria [create_search_criteria $search]
+		set search_response [searchNodes $language_id $search_criteria]
+		set check_error [check_response_error $search_response]
+		if {$check_error} {
+			puts "Error response from searchNodes"
+			return $search_response;
+		} else {
+			set graph_nodes [get_resp_value $search_response "node_list"]
+			set resp_def_node [getDefinition $language_id $object_type]
+			set def_node [get_resp_value $resp_def_node "definition_node"]
+			set obj_list [java::new ArrayList]
+			java::for {Node graph_node} $graph_nodes {
+				if {$returnFields} {
+					set synset_obj [convert_graph_node $graph_node $def_node $fieldList]
+				} else {
+					set synset_obj [convert_graph_node $graph_node $def_node]
+				}
+				$obj_list add $synset_obj
+			}
+			set result_map [java::new HashMap]
+			$result_map put "content" $obj_list
+			set response_list [create_response $result_map]
+			return $response_list
+		}
+	}
 }
-
-set get_node_response [getDataNode $language_id $synset_id]
-return $get_node_response
-
