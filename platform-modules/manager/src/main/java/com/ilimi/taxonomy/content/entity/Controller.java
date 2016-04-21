c7bc79f59966427f82b0a760a5e2a734625266f4
Mohammad Azharuddin <azhar@ilimi.in>
2016-04-21 17:04:19 +0530

    ECRF POJO -- Finished

--- platform-modules/manager/src/main/java/com/ilimi/taxonomy/content/entity/Controller.java
+++ platform-modules/manager/src/main/java/com/ilimi/taxonomy/content/entity/Controller.java
@@ -1,5 +1,23 @@
 package com.ilimi.taxonomy.content.entity;
 
+import java.util.Map;
+
 public class Controller {
+	
+	private Map<String, String> data = null;
+	private String cData = null;
+	
+	public Map<String, String> getData() {
+		return data;
+	}
+	public void setData(Map<String, String> data) {
+		this.data = data;
+	}
+	public String getcData() {
+		return cData;
+	}
+	public void setcData(String cData) {
+		this.cData = cData;
+	}
 
 }
