484bbf1b629f6cdc6ee23987f17f6dace188120d
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2016-07-01 10:06:30 +0530

    health check APIs

---
+++ searchIndex-platform/module/search-service/src/main/java/org/ekstep/controller/HealthCheckController.java
@@ -0,0 +1,39 @@
+package org.ekstep.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ilimi.common.controller.BaseController;
+import com.ilimi.common.dto.Response;
+import com.ilimi.common.dto.ResponseParams;
+import com.ilimi.common.dto.ResponseParams.StatusType;
+import com.ilimi.common.exception.ResponseCode;
+
+@Controller
+@RequestMapping("health")
+public class HealthCheckController extends BaseController {
+	
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<Response> search() {
+		String name = "search-service";
+		String apiId = name + ".health";
+		Response response = new Response();
+		response.put("name", name);
+		try {
+			response.put("healthy", true);
+			return getResponseEntity(response, apiId, null);
+		} catch (Exception e) {
+			ResponseParams resStatus = new ResponseParams();
+	        resStatus.setErrmsg(e.getMessage());
+	        resStatus.setStatus(StatusType.failed.name());
+            response.setResponseCode(ResponseCode.SERVER_ERROR);
+            response.setParams(resStatus);
+            response.put("healthy", false);
+            return getResponseEntity(response, apiId, null);
+		}
+	}
+}
