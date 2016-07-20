82361f00f06eac3a2b2e7866177f134363dcdc8e
Manojvv <manojv@ilimi.in>
2016-07-20 16:50:04 +0530

    Updated definition script

--- platform-devops/ansible/roles/definition/files/definition_script.sh
+++ platform-devops/ansible/roles/definition/files/definition_script.sh
@@ -13,3 +13,31 @@ do
    variable=`cat $file`
    `curl -i -X POST -H "Content-Type: application/json" -H "user-id: ansible_user" -d '$variable' http://54.254.225.115:8080/learning-service/taxonomy/domain/definition`
 done
+
+
+cd /home/jenkins/workspace/LP_Definition_Update/docs/language_model/definitions/iso                    #move inside th folder
+
+#*****************************************************
+#Find all files required & copy the content of the file to a variable .user variable in the curl command to definition-update
+for file in *                        
+do
+   variable1=`cat $file`
+   `curl -i -X POST -H "Content-Type: application/json" -H "user-id: ansible_user" -d '$variable1' http://54.254.225.115:8080/learning-service/taxonomy/domain/definition`
+done
+
+
+
+cd /home/jenkins/workspace/LP_Definition_Update/docs/language_model/definitions/language“                    #move inside th folder
+
+#*****************************************************
+#Find all files required & copy the content of the file to a variable .user variable in the curl command to definition-update
+for file in *                        
+do
+	cd list
+   for env in *                        
+   do
+
+   variable2=`cat $file`
+   `curl -i -X POST -H "Content-Type: application/json" -H "user-id: ansible_user" -d '$variable2' http://54.254.225.115:8080/learning-service/taxonomy/domain/definition`
+done
+done
