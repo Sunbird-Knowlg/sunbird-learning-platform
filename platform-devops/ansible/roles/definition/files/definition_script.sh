8530f62978e014547bf45587cb23476fb7e3bc0f
Manojvv <manojv@ilimi.in>
2016-07-20 15:14:28 +0530

    Updated definition script

--- platform-devops/ansible/roles/definition/files/definition_script.sh
+++ platform-devops/ansible/roles/definition/files/definition_script.sh
@@ -4,12 +4,12 @@
 #DESCRIPTION:Script for lp-definition-update.
 #DATE:25-05-16
 #****************************************************
-cd Learning-Platform/docs/domain_model_v2/definitions                    #move inside th folder
+cd docs/domain_model_v2/definitions                    #move inside th folder
 
 #*****************************************************
 #Find all files required & copy the content of the file to a variable .user variable in the curl command to definition-update
 for file in *                        
 do
    variable=`cat $file`
-   curl -i -X POST -H "Content-Type: application/json" -H "user-id: ansible_user" -d '$variable' http://{{ service_url }}/learning-service/taxonomy/domain/definition
+   `curl -i -X POST -H "Content-Type: application/json" -H "user-id: ansible_user" -d '$variable' http://{{ service_url }}/learning-service/taxonomy/domain/definition`
 done
