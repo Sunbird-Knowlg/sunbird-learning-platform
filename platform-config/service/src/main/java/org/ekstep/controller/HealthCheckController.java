41ffcd5a7510850d469ea5ec4d46e9385ace9716
rashmi93 <rashminrocks93@gmail.com>
2016-07-22 14:07:09 +0530

    HealthCheckApi for config-service

--- platform-config/service/src/main/java/org/ekstep/controller/HealthCheckController.java
+++ platform-config/service/src/main/java/org/ekstep/controller/HealthCheckController.java
@@ -1,5 +1,6 @@
 package org.ekstep.controller;
 
+import org.ekstep.common.util.HttpDownloadUtility;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,25 +16,40 @@ import com.ilimi.common.exception.ResponseCode;
 @Controller
 @RequestMapping("health")
 public class HealthCheckController extends BaseController {
-	
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	private static final String folderName = "resources";
+	private static final String baseUrl = "https://ekstep-config.s3-ap-southeast-1.amazonaws.com/";
+
+	@RequestMapping(value = "/config", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<Response> search() {
-		String name = "config-service";
-		String apiId = name + ".health";
 		Response response = new Response();
-		response.put("name", name);
+		String ordinals = "";
+		String apiId  = "";
+		String resourcebundle = "";
 		try {
-			response.put("healthy", true);
-			return getResponseEntity(response, apiId, null);
+			resourcebundle = HttpDownloadUtility.readFromUrl(baseUrl + folderName + "/en.json");
+			ordinals = HttpDownloadUtility.readFromUrl(baseUrl + "ordinals.json");
+			ResponseParams params = new ResponseParams();
+			if ((!ordinals.isEmpty()) && (!resourcebundle.isEmpty())) {
+				String name = "config-service";
+				apiId = name + ".health";
+				params.setErr("0");
+				params.setStatus(StatusType.successful.name());
+				params.setErrmsg("Operation successful");
+				response.setParams(params);
+				response.put("name", name);
+				response.put("healthy", true);
+			}
+				return getResponseEntity(response, apiId, null);
+			
 		} catch (Exception e) {
 			ResponseParams resStatus = new ResponseParams();
-	        resStatus.setErrmsg(e.getMessage());
-	        resStatus.setStatus(StatusType.failed.name());
-            response.setResponseCode(ResponseCode.SERVER_ERROR);
-            response.setParams(resStatus);
-            response.put("healthy", false);
-            return getResponseEntity(response, apiId, null);
+			resStatus.setErrmsg(e.getMessage());
+			resStatus.setStatus(StatusType.failed.name());
+			response.setResponseCode(ResponseCode.SERVER_ERROR);
+			response.setParams(resStatus);
+			response.put("healthy", false);
+			return getResponseEntity(response, apiId, null);
 		}
 	}
 }
