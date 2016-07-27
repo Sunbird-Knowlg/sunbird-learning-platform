52ad28c66ee409024859b8a924adc483a65cb52b
Mohammad Azharuddin <azhar@ilimi.in>
2016-07-27 17:20:44 +0530

    Code Restructuring -- Moved Common Code to Base Initializer & Finalizer From BasePipeline.

--- platform-modules/manager/src/main/java/com/ilimi/taxonomy/content/operation/finalizer/PublishFinalizer.java
+++ platform-modules/manager/src/main/java/com/ilimi/taxonomy/content/operation/finalizer/PublishFinalizer.java
@@ -1,25 +1,17 @@
 package com.ilimi.taxonomy.content.operation.finalizer;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.ekstep.common.optimizr.ThumbnailGenerator;
 import org.ekstep.common.slugs.Slug;
-import org.ekstep.common.util.HttpDownloadUtility;
-import org.ekstep.common.util.ZipUtility;
-
 import com.ilimi.common.dto.Response;
 import com.ilimi.common.exception.ClientException;
-import com.ilimi.common.exception.ServerException;
 import com.ilimi.graph.dac.model.Node;
 import com.ilimi.taxonomy.content.common.ContentConfigurationConstants;
 import com.ilimi.taxonomy.content.common.ContentErrorMessageConstants;
@@ -71,12 +63,12 @@ public class PublishFinalizer extends BaseFinalizer {
 			// Get Content String
 			String ecml = getECMLString(ecrf, ecmlType);
 			// Write ECML File
-			writeECMLFile(ecml, ecmlType);
+			writeECMLFile(basePath, ecml, ecmlType);
 			// Create 'ZIP' Package
 			String zipFileName = basePath + File.separator + System.currentTimeMillis() + "_" + contentId
 					+ ContentConfigurationConstants.FILENAME_EXTENSION_SEPERATOR
 					+ ContentConfigurationConstants.DEFAULT_ZIP_EXTENSION;
-			createZipPackage(zipFileName);
+			createZipPackage(basePath, zipFileName);
 			// Upload Package
 			File packageFile = new File(zipFileName);
 			if (packageFile.exists()) {
@@ -90,7 +82,7 @@ public class PublishFinalizer extends BaseFinalizer {
 			}
 		}
 		// Download App Icon and create thumbnail
-		createThumbnail(node);
+		createThumbnail(basePath, node);
 		
 		// Set Package Version
 		double version = 1.0;
@@ -138,79 +130,4 @@ public class PublishFinalizer extends BaseFinalizer {
 		return response;
 	}
 	
-	private void createZipPackage(String zipFileName) {
-		if (!StringUtils.isBlank(zipFileName)) {
-			LOGGER.info("Creating Zip File: " + zipFileName);
-			ZipUtility appZip = new ZipUtility(basePath, zipFileName);
-			appZip.generateFileList(new File(basePath));
-			appZip.zipIt(zipFileName);
-		}
-	}
-
-	private void writeECMLFile(String ecml, String ecmlType) {
-		try {
-			if (StringUtils.isBlank(ecml))
-				throw new ClientException(ContentErrorCodeConstants.EMPTY_ECML.name(),
-						ContentErrorMessageConstants.EMPTY_ECML_STRING + " | [Unable to write Empty ECML File.]");
-			if (StringUtils.isBlank(ecmlType))
-				throw new ClientException(ContentErrorCodeConstants.INVALID_ECML_TYPE.name(),
-						ContentErrorMessageConstants.INVALID_ECML_TYPE
-								+ " | [System is in a fix between (XML & JSON) ECML Type.]");
-			
-			LOGGER.info("ECML File Type: " + ecmlType);
-			File file = new File(basePath + File.separator + ContentConfigurationConstants.DEFAULT_ECML_FILE_NAME
-					+ ContentConfigurationConstants.FILENAME_EXTENSION_SEPERATOR + ecmlType);
-			LOGGER.info("Creating ECML File With Name: " + file.getAbsolutePath());
-			FileUtils.writeStringToFile(file, ecml);
-		} catch (IOException e) {
-			throw new ServerException(ContentErrorCodeConstants.ECML_FILE_WRITE.name(),
-					ContentErrorMessageConstants.ECML_FILE_WRITE_ERROR + " | [Unable to Write ECML File.]");
-		}
-	}
-
-	private void createThumbnail(Node node) {
-		try {
-			if (null != node) {
-				String appIcon = (String) node.getMetadata().get(ContentWorkflowPipelineParams.appIcon.name());
-				if (!StringUtils.isBlank(appIcon)) {
-					LOGGER.info("Content Id: " + node.getIdentifier() + " | App Icon: " + appIcon);
-					File appIconFile = HttpDownloadUtility.downloadFile(appIcon, basePath);
-					if (null != appIconFile && appIconFile.exists() && appIconFile.isFile()) {
-						boolean generated = ThumbnailGenerator.generate(appIconFile);
-						if (generated) {
-							String thumbnail = appIconFile.getParent() + File.separator 
-									+ FilenameUtils.getBaseName(appIconFile.getPath()) + ".thumb." 
-									+ FilenameUtils.getExtension(appIconFile.getPath());
-							File thumbFile = new File(thumbnail);
-							if (thumbFile.exists()) {
-								LOGGER.info("Thumbnail created for Content Id: " + node.getIdentifier());
-								String[] urlArray = uploadToAWS(thumbFile, getUploadFolderName());
-								if (null != urlArray && urlArray.length >= 2) {
-									String thumbUrl = urlArray[IDX_S3_URL];
-									node.getMetadata().put(ContentWorkflowPipelineParams.appIcon.name(), thumbUrl);
-									node.getMetadata().put(ContentWorkflowPipelineParams.posterImage.name(), appIcon);
-								}
-								try {
-									thumbFile.delete();
-									LOGGER.info("Deleted local Thumbnail file");
-								} catch (Exception e) {
-								}
-							}
-						}
-						try {
-							appIconFile.delete();
-							LOGGER.info("Deleted local AppIcon file");
-						} catch (Exception e) {
-						}
-					}
-				}
-			}
-		} catch (Exception e) {
-			throw new ServerException(ContentErrorCodeConstants.DOWNLOAD_ERROR.name(),
-					ContentErrorMessageConstants.APP_ICON_DOWNLOAD_ERROR
-							+ " | [Unable to Download App Icon for Content Id: '" + node.getIdentifier() + "' ]",
-					e);
-		}
-	}
-
 }
