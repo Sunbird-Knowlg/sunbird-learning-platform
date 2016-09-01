ba0b48484ff30f2f6f842de8d4b72d098f167ef3
Karthik <kkarthik4cs@gmail.com>
2016-09-01 17:26:55 +0530

    entity refacor

--- language-platform/module/common/src/main/java/org/ekstep/language/common/LanguageSourceTypeMap.java
+++ language-platform/module/common/src/main/java/org/ekstep/language/common/LanguageSourceTypeMap.java
@@ -3,8 +3,15 @@ package org.ekstep.language.common;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The Class LanguageSourceTypeMap is to load Language source types into map for
+ * reference
+ *
+ * @author rayulu
+ */
 public class LanguageSourceTypeMap {
 
+    /** The Language source type map. */
     private static Map<String, String> LanguageSourceTypeMap = new HashMap<String, String>();
 
     static {
@@ -13,10 +20,22 @@ public class LanguageSourceTypeMap {
     	LanguageSourceTypeMap.put("wn", "WordNet");
     }
 
+    /**
+     * Contains source type.
+     *
+     * @param sourceId the source id
+     * @return true, if successful
+     */
     public static boolean containsSourceType(String sourceId) {
         return LanguageSourceTypeMap.containsKey(sourceId);
     }
 
+    /**
+     * Gets the source type.
+     *
+     * @param sourceId the source id
+     * @return the source type
+     */
     public static String getSourceType(String sourceId) {
         return LanguageSourceTypeMap.get(sourceId);
     }
