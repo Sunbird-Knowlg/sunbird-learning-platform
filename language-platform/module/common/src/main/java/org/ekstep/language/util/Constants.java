1ecfad1e8ebee7d4a68f7b2bf2f3c50466f00d3a
Karthik <kkarthik4cs@gmail.com>
2016-09-01 11:24:18 +0530

    constant class refactor

--- language-platform/module/common/src/main/java/org/ekstep/language/util/Constants.java
+++ language-platform/module/common/src/main/java/org/ekstep/language/util/Constants.java
@@ -1,21 +1,61 @@
 package org.ekstep.language.util;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Constants. maintains language common constants
+ *
+ * @author rayulu and amarnath
+ */
 public class Constants {
-	public static final int TAG_INDEX_ROOT_WORD=0;
-	public static final int TAG_INDEX_CATEGORY=1;
-	public static final int TAG_INDEX_GENDER=2;
-	public static final int TAG_INDEX_NUMBER=3;
-	public static final int TAG_INDEX_PERS=4;
-	public static final int TAG_INDEX_CASE=5;
-	public static final int TAG_INDEX_INFLECTION=6;
-	public static final int TAG_INDEX_RTS=7;
-	public static final int WL_INDEX_LEMMA=1;
-	public static final int WL_INDEX_IDENTIFER=0;
-	public static final String CITATION_INDEX_COMMON_NAME="citation_index";
-	public static final String WORD_INDEX_COMMON_NAME="word_index";
-	public static final String WORD_INFO_INDEX_COMMON_NAME="word_info_index";
-	public static final String CITATION_INDEX_TYPE="citation";
-	public static final String WORD_INDEX_TYPE="wordIndexType";
+
+	/** The Constant TAG_INDEX_ROOT_WORD. */
+	public static final int TAG_INDEX_ROOT_WORD = 0;
+
+	/** The Constant TAG_INDEX_CATEGORY. */
+	public static final int TAG_INDEX_CATEGORY = 1;
+
+	/** The Constant TAG_INDEX_GENDER. */
+	public static final int TAG_INDEX_GENDER = 2;
+
+	/** The Constant TAG_INDEX_NUMBER. */
+	public static final int TAG_INDEX_NUMBER = 3;
+
+	/** The Constant TAG_INDEX_PERS. */
+	public static final int TAG_INDEX_PERS = 4;
+
+	/** The Constant TAG_INDEX_CASE. */
+	public static final int TAG_INDEX_CASE = 5;
+
+	/** The Constant TAG_INDEX_INFLECTION. */
+	public static final int TAG_INDEX_INFLECTION = 6;
+
+	/** The Constant TAG_INDEX_RTS. */
+	public static final int TAG_INDEX_RTS = 7;
+
+	/** The Constant WL_INDEX_LEMMA. */
+	public static final int WL_INDEX_LEMMA = 1;
+
+	/** The Constant WL_INDEX_IDENTIFER. */
+	public static final int WL_INDEX_IDENTIFER = 0;
+
+	/** The Constant CITATION_INDEX_COMMON_NAME. */
+	public static final String CITATION_INDEX_COMMON_NAME = "citation_index";
+
+	/** The Constant WORD_INDEX_COMMON_NAME. */
+	public static final String WORD_INDEX_COMMON_NAME = "word_index";
+
+	/** The Constant WORD_INFO_INDEX_COMMON_NAME. */
+	public static final String WORD_INFO_INDEX_COMMON_NAME = "word_info_index";
+
+	/** The Constant CITATION_INDEX_TYPE. */
+	public static final String CITATION_INDEX_TYPE = "citation";
+
+	/** The Constant WORD_INDEX_TYPE. */
+	public static final String WORD_INDEX_TYPE = "wordIndexType";
+
+	/** The Constant WORD_INFO_INDEX_TYPE. */
 	public static final String WORD_INFO_INDEX_TYPE = "wordInfoType";
+
+	/** The Constant CSV_SPLIT_BY. */
 	public static final String CSV_SPLIT_BY = ",";
 }
