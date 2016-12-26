7508c822ee0257973172d26db8cd61f4d4eb1d0b
Karthik <kkarthik4cs@gmail.com>
2016-12-26 19:43:30 +0530

    Bolt DataVersionKey validator implementation

--- platform-core/graph-engine/module/graph-dac-api/src/main/java/org/ekstep/graph/service/request/validaor/Neo4JBoltDataVersionKeyValidator.java
+++ platform-core/graph-engine/module/graph-dac-api/src/main/java/org/ekstep/graph/service/request/validaor/Neo4JBoltDataVersionKeyValidator.java
@@ -1,5 +1,141 @@
 package org.ekstep.graph.service.request.validaor;
 
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.ekstep.graph.service.common.DACErrorCodeConstants;
+import org.ekstep.graph.service.common.DACErrorMessageConstants;
+import org.ekstep.graph.service.common.NodeUpdateMode;
+import org.ekstep.graph.service.util.DefinitionNodeUtil;
+import org.ekstep.graph.service.util.DriverUtil;
+import org.neo4j.driver.internal.InternalNode;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Transaction;
+
+import com.ilimi.common.exception.ClientException;
+import com.ilimi.graph.common.DateUtils;
+import com.ilimi.graph.dac.enums.GraphDACParams;
+import com.ilimi.graph.dac.model.Node;
+
 public class Neo4JBoltDataVersionKeyValidator {
+
+	private static Logger LOGGER = LogManager.getLogger(Neo4JBoltDataVersionKeyValidator.class.getName());
+
+	public boolean validateUpdateOperation(String graphId, Node node) {
+		LOGGER.debug("Graph Engine Node: ", node);
+
+		boolean isValidUpdateOperation = false;
+
+		// Fetching Version Check Mode ('OFF', 'STRICT', 'LENIENT')
+		String versionCheckMode = DefinitionNodeUtil.getMetadataValue(graphId, node.getObjectType(),
+				GraphDACParams.versionCheckMode.name());
+		LOGGER.info("Version Check Mode in Definition Node: " + versionCheckMode + " for Object Type: "
+				+ node.getObjectType());
+
+		// Checking if the 'versionCheckMode' Property is not specified,
+		// then default Mode is OFF
+		if (StringUtils.isBlank(versionCheckMode))
+			versionCheckMode = NodeUpdateMode.OFF.name();
+
+		// Checking of Node Update Version Checking is either 'STRICT'
+		// or 'LENIENT'.
+		// If Number of Modes are increasing then the Condition should
+		// be checked for 'OFF' Mode Only.
+		if (StringUtils.equalsIgnoreCase(NodeUpdateMode.STRICT.name(), versionCheckMode)
+				|| StringUtils.equalsIgnoreCase(NodeUpdateMode.LENIENT.name(), versionCheckMode)) {
+			boolean isValidVersionKey = isValidVersionKey(graphId, node);
+			LOGGER.info("Is Valid Version Key ? " + isValidVersionKey);
+
+			if (!isValidVersionKey) {
+				// Checking for Strict Mode
+				LOGGER.info("Checking for Node Update Operation Mode is 'STRICT' for Node Id: " + node.getIdentifier());
+				if (StringUtils.equalsIgnoreCase(NodeUpdateMode.STRICT.name(), versionCheckMode))
+					throw new ClientException(DACErrorCodeConstants.INVALID_VERSION.name(),
+							DACErrorMessageConstants.INVALID_VERSION_KEY_ERROR + " | [Unable to Update the Data.]");
+
+				// Checking for Lenient Mode
+				LOGGER.info(
+						"Checking for Node Update Operation Mode is 'LENIENT' for Node Id: " + node.getIdentifier());
+				if (StringUtils.equalsIgnoreCase(NodeUpdateMode.LENIENT.name(), versionCheckMode))
+					node.getMetadata().put(GraphDACParams.NODE_UPDATE_STATUS.name(),
+							GraphDACParams.STALE_DATA_UPDATED.name());
+
+				// Update Operation is Valid
+				isValidUpdateOperation = true;
+				LOGGER.info("Update Operation is Valid for Node Id: " + node.getIdentifier());
+			}
+		}
+		LOGGER.info("Is Valid Update Operation ? " + isValidUpdateOperation);
+
+		return isValidUpdateOperation;
+	}
+
+	private boolean isValidVersionKey(String graphId, Node node) {
+		LOGGER.debug("Node: ", node);
+
+		boolean isValidVersionKey = false;
+
+		String versionKey = (String) node.getMetadata().get(GraphDACParams.versionKey.name());
+		LOGGER.info("Data Node Version Key Value: " + versionKey + " | [Node Id: '" + node.getIdentifier() + "']");
+
+		// Fetching Neo4J Node
+		Map<String, Object> neo4jNode = getNeo4jNodeProperty(graphId, node.getIdentifier());
+		LOGGER.info("Fetched the Neo4J Node Id: " + neo4jNode.get(GraphDACParams.identifier.name()) + " | [Node Id: '"
+				+ node.getIdentifier() + "']");
+
+		// Reading Last Updated On time stamp from Neo4J Node
+		String lastUpdateOn = (String) neo4jNode.get(GraphDACParams.lastUpdatedOn.name());
+		LOGGER.info("Fetched 'lastUpdatedOn' Property from the Neo4J Node Id: "
+				+ neo4jNode.get(GraphDACParams.identifier.name()) + " as 'lastUpdatedOn': " + lastUpdateOn
+				+ " | [Node Id: '" + node.getIdentifier() + "']");
+
+		if (StringUtils.isNotBlank(versionKey) && StringUtils.isNotBlank(lastUpdateOn)) {
+			// Converting versionKey to milli seconds of type Long
+			long versionKeyTS = Long.parseLong(versionKey);
+			LOGGER.info("'versionKey' Time Stamp: " + versionKeyTS + " | [Node Id: '" + node.getIdentifier() + "']");
+
+			// Converting 'lastUpdatedOn' to milli seconds of type Long
+			long lastUpdatedOnTS = DateUtils.parse(lastUpdateOn).getTime();
+			LOGGER.info(
+					"'lastUpdatedOn' Time Stamp: " + lastUpdatedOnTS + " | [Node Id: '" + node.getIdentifier() + "']");
+
+			// Compare both the Time Stamp
+			if (versionKeyTS == lastUpdatedOnTS)
+				isValidVersionKey = true;
+		}
+
+		return isValidVersionKey;
+	}
+
+	private Map<String, Object> getNeo4jNodeProperty(String graphId, String identifier) {
+		Map<String, Object> prop = null;
+		try (Driver driver = DriverUtil.getDriver(graphId)) {
+			LOGGER.info("Driver Initialised. | [Graph Id: " + graphId + "]");
+			try (Session session = driver.session()) {
+				try (Transaction tx = session.beginTransaction()) {
+					String query = "match (n:" + graphId + "{identifier:'" + identifier + "'}) return (n) as result";
+					StatementResult result = tx.run(query);
+					if (result.hasNext()) {
+						Record record = result.next();
+						InternalNode node = (InternalNode) record.values().get(0).asObject();
+						prop = node.asMap();
+					}
+					tx.success();
+					tx.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					session.close();
+				}
+			}
+		}
+		return prop;
+
+	}
 
 }
--- platform-core/graph-engine/module/graph-dac-api/src/main/java/org/ekstep/graph/service/util/DefinitionNodeUtil.java
+++ platform-core/graph-engine/module/graph-dac-api/src/main/java/org/ekstep/graph/service/util/DefinitionNodeUtil.java
@@ -9,6 +9,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.ekstep.graph.service.common.DACErrorCodeConstants;
 import org.ekstep.graph.service.common.DACErrorMessageConstants;
+import org.neo4j.driver.internal.InternalNode;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
@@ -33,30 +38,66 @@ public class DefinitionNodeUtil {
 	/**
 	 * Gets the metadata value.
 	 *
-	 * @param graphId the graph id
-	 * @param objectType the object type
-	 * @param key the key
-	 * @param request the request
+	 * @param graphId
+	 *            the graph id
+	 * @param objectType
+	 *            the object type
+	 * @param key
+	 *            the key
+	 * @param request
+	 *            the request
 	 * @return the metadata value
 	 */
 	public static String getMetadataValue(String graphId, String objectType, String key, Request request) {
 		LOGGER.debug(
 				"Reading Metadata Value for Graph Id: " + graphId + "Object Type: " + objectType + "For Key: " + key);
-		
+
 		// Initializing the value
 		String value = "";
-		
+
 		LOGGER.info("Fetching the Definition Node to Read Metadata Value. | [Graph Id: " + graphId + "]");
 		Node definitionNode = getDefinitionNode(graphId, objectType, request);
 		if (null == definitionNode)
 			throw new ResourceNotFoundException(DACErrorCodeConstants.MISSING_DEFINITION.name(),
-					DACErrorMessageConstants.MISSING_DEFINTION_ERROR + " | [Object Type: " + objectType + " and Graph Id "
-							+ graphId + "]");
-		
+					DACErrorMessageConstants.MISSING_DEFINTION_ERROR + " | [Object Type: " + objectType
+							+ " and Graph Id " + graphId + "]");
+
 		Map<String, Object> metadataMap = definitionNode.getMetadata();
 		if (null != metadataMap && null != metadataMap.get(key))
 			value = (String) metadataMap.get(key);
-		
+
+		LOGGER.info("Returning the Metadata Value - Key: " + key + " | value: " + value);
+		return value;
+	}
+
+	/**
+	 * Gets the metadata value.
+	 *
+	 * @param graphId
+	 *            the graph id
+	 * @param objectType
+	 *            the object type
+	 * @param key
+	 *            the key
+	 * @return the metadata value
+	 */
+	public static String getMetadataValue(String graphId, String objectType, String key) {
+		LOGGER.debug(
+				"Reading Metadata Value for Graph Id: " + graphId + "Object Type: " + objectType + "For Key: " + key);
+
+		// Initializing the value
+		String value = "";
+
+		LOGGER.info("Fetching the Definition Node to Read Metadata Value. | [Graph Id: " + graphId + "]");
+		Map<String, Object> metadataMap = getDefinitionNodeMetadata(graphId, objectType);
+		if (null == metadataMap)
+			throw new ResourceNotFoundException(DACErrorCodeConstants.MISSING_DEFINITION.name(),
+					DACErrorMessageConstants.MISSING_DEFINTION_ERROR + " | [Object Type: " + objectType
+							+ " and Graph Id " + graphId + "]");
+
+		if (null != metadataMap && null != metadataMap.get(key))
+			value = (String) metadataMap.get(key);
+
 		LOGGER.info("Returning the Metadata Value - Key: " + key + " | value: " + value);
 		return value;
 	}
@@ -64,9 +105,12 @@ public class DefinitionNodeUtil {
 	/**
 	 * Gets the definition node.
 	 *
-	 * @param graphId the graph id
-	 * @param objectType the object type
-	 * @param request the request
+	 * @param graphId
+	 *            the graph id
+	 * @param objectType
+	 *            the object type
+	 * @param request
+	 *            the request
 	 * @return the definition node
 	 */
 	private static Node getDefinitionNode(String graphId, String objectType, Request request) {
@@ -83,7 +127,8 @@ public class DefinitionNodeUtil {
 				while (nodes.hasNext()) {
 					org.neo4j.graphdb.Node neo4jNode = nodes.next();
 					if (StringUtils.equalsIgnoreCase(
-							((String) neo4jNode.getProperty(SystemProperties.IL_FUNC_OBJECT_TYPE.name())), objectType)) {
+							((String) neo4jNode.getProperty(SystemProperties.IL_FUNC_OBJECT_TYPE.name())),
+							objectType)) {
 						node = new Node(graphId, neo4jNode);
 						LOGGER.info("Found the Definition Node for Object type " + objectType + " | Graph Id: "
 								+ graphId + " | Node Id:  " + node.getIdentifier());
@@ -101,4 +146,40 @@ public class DefinitionNodeUtil {
 		}
 	}
 
+	/**
+	 * Gets the definition node.
+	 *
+	 * @param graphId
+	 *            the graph id
+	 * @param objectType
+	 *            the object type
+	 * @return the definition node
+	 */
+	private static Map<String, Object> getDefinitionNodeMetadata(String graphId, String objectType) {
+		LOGGER.debug("Fetching Definition Node for Object Id: " + objectType + "of Graph Id: " + graphId + ".");
+		Map metadataMap = null;
+		try (Driver driver = DriverUtil.getDriver(graphId)) {
+			LOGGER.info("Driver Initialised. | [Graph Id: " + graphId + "]");
+			try (Session session = driver.session()) {
+				try (org.neo4j.driver.v1.Transaction tx = session.beginTransaction()) {
+					String query = "match (n:" + graphId + "{" + SystemProperties.IL_SYS_NODE_TYPE.name() + ":'"
+							+ SystemNodeTypes.DEFINITION_NODE.name() + "',"
+							+ SystemProperties.IL_FUNC_OBJECT_TYPE.name() + ":'"+objectType+"'}) return (n) as result";
+					StatementResult result = tx.run(query);
+					if (result.hasNext()) {
+						Record record = result.next();
+						InternalNode node = (InternalNode) record.values().get(0).asObject();
+						metadataMap = node.asMap();
+					}
+					tx.success();
+					tx.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					session.close();
+				}
+			}
+		}
+		return metadataMap;
+	}
 }
