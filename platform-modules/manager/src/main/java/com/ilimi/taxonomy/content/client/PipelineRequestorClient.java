9fa614928ec8c7110d65d5cf8474f3413f0b4b73
Mohammad Azharuddin <azhar@ilimi.in>
2017-01-24 16:11:54 +0530

    Issue #723 feat : created a new pipeline as validate

--- platform-modules/manager/src/main/java/com/ilimi/taxonomy/content/client/PipelineRequestorClient.java
+++ platform-modules/manager/src/main/java/com/ilimi/taxonomy/content/client/PipelineRequestorClient.java
@@ -1,5 +1,8 @@
 package com.ilimi.taxonomy.content.client;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.ilimi.taxonomy.content.concrete.processor.AssetsValidatorProcessor;
 import com.ilimi.taxonomy.content.concrete.processor.EmbedControllerProcessor;
 import com.ilimi.taxonomy.content.concrete.processor.GlobalizeAssetProcessor;
@@ -10,6 +13,8 @@ import com.ilimi.taxonomy.content.processor.AbstractProcessor;
 import com.ilimi.taxonomy.content.processor.ContentPipelineProcessor;
 
 public class PipelineRequestorClient {
+	
+	private static Logger LOGGER = LogManager.getLogger(PipelineRequestorClient.class.getName());
 	
 	public static AbstractProcessor getPipeline(String operation, String basePath, String contentId) {
 		ContentPipelineProcessor contentPipeline = new ContentPipelineProcessor();
@@ -23,17 +28,25 @@ public class PipelineRequestorClient {
 		
 		switch (operation) {
 		case "compress":
+			LOGGER.info("Registering the Processors for 'COMPRESS' Operation.");
 			contentPipeline.registerProcessor(localizeAssetProcessor);
-			//contentPipeline.registerProcessor(embedControllerProcessor);
 			contentPipeline.registerProcessor(missingAssetValidatorProcessor);
 			break;
 			
 		case "extract":
+			LOGGER.info("Registering the Processors for 'EXTRACT' Operation.");
 			contentPipeline.registerProcessor(missingAssetValidatorProcessor);
 			contentPipeline.registerProcessor(assetsValidatorProcessor);
 			contentPipeline.registerProcessor(missingCtrlValidatorProcessor);
 			contentPipeline.registerProcessor(globalizeAssetProcessor);
 			contentPipeline.registerProcessor(embedControllerProcessor);
+			break;
+			
+		case "validate":
+			LOGGER.info("Registering the Processors for 'VALIDATE' Operation.");
+			contentPipeline.registerProcessor(missingAssetValidatorProcessor);
+			contentPipeline.registerProcessor(assetsValidatorProcessor);
+			contentPipeline.registerProcessor(missingCtrlValidatorProcessor);
 			break;
 
 		default:
@@ -49,22 +62,36 @@ public class PipelineRequestorClient {
 		AbstractProcessor localizeAssetProcessor = new LocalizeAssetProcessor(basePath, contentId);
 		AbstractProcessor embedControllerProcessor = new EmbedControllerProcessor(basePath, contentId);
 		AbstractProcessor missingAssetValidatorProcessor = new MissingAssetValidatorProcessor(basePath, contentId);
+		AbstractProcessor missingCtrlValidatorProcessor = new MissingControllerValidatorProcessor(basePath, contentId);
+		AbstractProcessor assetsValidatorProcessor = new AssetsValidatorProcessor(basePath, contentId);
 		AbstractProcessor globalizeAssetProcessor = new GlobalizeAssetProcessor(basePath, contentId);
 		
 		switch (operation) {
 		case "compress":
+			LOGGER.info("Initialising the Processor's Chain for 'COMPRESS' Operation.");
 			localizeAssetProcessor.setNextProcessor(embedControllerProcessor);
 			embedControllerProcessor.setNextProcessor(missingAssetValidatorProcessor);
 			head = localizeAssetProcessor;
 			break;
 			
 		case "extract":
-			missingAssetValidatorProcessor.setNextProcessor(globalizeAssetProcessor);
+			LOGGER.info("Initialising the Processor's Chain for 'EXTRACT' Operation.");
+			missingAssetValidatorProcessor.setNextProcessor(assetsValidatorProcessor);
+			assetsValidatorProcessor.setNextProcessor(missingCtrlValidatorProcessor);
+			missingCtrlValidatorProcessor.setNextProcessor(globalizeAssetProcessor);
 			globalizeAssetProcessor.setNextProcessor(embedControllerProcessor);
+			head = missingAssetValidatorProcessor;
+			break;
+			
+		case "validate":
+			LOGGER.info("Initialising the Processor's Chain for 'VALIDATE' Operation.");
+			missingAssetValidatorProcessor.setNextProcessor(assetsValidatorProcessor);
+			assetsValidatorProcessor.setNextProcessor(missingCtrlValidatorProcessor);
 			head = missingAssetValidatorProcessor;
 			break;
 
 		default:
+			LOGGER.warn("Invalid Pipeline Operation.");
 			break;
 		}
 		
