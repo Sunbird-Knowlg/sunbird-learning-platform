a6ab20f325b8448c3bca23859a4425c5fe9072cb
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-01-31 00:18:22 +0530

    Issue #747 chore: utility to update S3 URLs

---
+++ platform-tools/spikes/s3-url.updater/src/main/java/org/ekstep/s3/util/UrlProperties.java
@@ -0,0 +1,21 @@
+package org.ekstep.s3.util;
+
+public enum UrlProperties {
+
+	thumbnail, 
+	posterImage,
+	appIcon,
+	grayScaleAppIcon,
+	downloadUrl,
+	artifactUrl,
+	launchUrl,
+	body,
+	editorState,
+	media,
+	audio,
+	pictures,
+	image,
+	wordImages,
+	variants,
+	pronunciations;
+}
