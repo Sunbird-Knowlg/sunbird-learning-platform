4f3054132f8bac2469809f51aed435bd418fa112
Mohammad Azharuddin <azhar@ilimi.in>
2017-02-13 16:33:36 +0530

    Issue #723 Feat : Asset MimeType Validation.

--- platform-modules/manager/src/main/java/com/ilimi/taxonomy/mgr/impl/AssetsMimeTypeMgrImpl.java
+++ platform-modules/manager/src/main/java/com/ilimi/taxonomy/mgr/impl/AssetsMimeTypeMgrImpl.java
@@ -1,11 +1,15 @@
 package com.ilimi.taxonomy.mgr.impl;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.tika.Tika;
+import org.apache.tika.mime.MimeTypes;
 import org.ekstep.common.optimizr.FileType;
 import org.ekstep.common.optimizr.FileUtils;
 import org.ekstep.learning.common.enums.ContentAPIParams;
@@ -15,6 +19,8 @@ import org.springframework.stereotype.Component;
 
 import com.ilimi.common.dto.Request;
 import com.ilimi.common.dto.Response;
+import com.ilimi.common.exception.ClientException;
+import com.ilimi.common.exception.ServerException;
 import com.ilimi.graph.dac.model.Node;
 import com.ilimi.taxonomy.content.pipeline.initializer.InitializePipeline;
 import com.ilimi.taxonomy.mgr.IMimeTypeManager;
@@ -50,30 +56,54 @@ public class AssetsMimeTypeMgrImpl extends BaseMimeTypeManager implements IMimeT
 		LOGGER.debug("Node: ", node);
 		LOGGER.debug("Uploaded File: " + uploadFile.getName());
 
-		LOGGER.info("Calling Upload Content Node For Node ID: " + node.getIdentifier());
-		String[] urlArray = uploadArtifactToAWS(uploadFile, node.getIdentifier());
+		Response response = new Response();
+		try {
+			LOGGER.info("Verifying the MimeTypes.");
+			Tika tika = new Tika(new MimeTypes());
+			String mimeType = tika.detect(uploadFile);
+			String nodeMimeType = (String) node.getMetadata().get(ContentAPIParams.mimeType.name());
+			LOGGER.debug("Uploaded Asset MimeType: " + mimeType);
+			if (!StringUtils.equalsIgnoreCase(mimeType, nodeMimeType))
+				LOGGER.warn("Uploaded File MimeType is not same as Node (Object) MimeType. [Uploaded MimeType: "
+						+ mimeType + " | Node (Object) MimeType: " + nodeMimeType + "]");
 
-		LOGGER.info("Updating the Content Node for Node ID: " + node.getIdentifier());
-		node.getMetadata().put(ContentAPIParams.s3Key.name(), urlArray[0]);
-		node.getMetadata().put(ContentAPIParams.artifactUrl.name(), urlArray[1]);
-		node.getMetadata().put(ContentAPIParams.downloadUrl.name(), urlArray[1]);
-		node.getMetadata().put(ContentAPIParams.size.name(), getS3FileSize(urlArray[0]));
-		node.getMetadata().put(ContentAPIParams.status.name(), "Live");
-		Map<String, String> variantsMap = new HashMap<String, String>();
-		node.getMetadata().put(ContentAPIParams.variants.name(), variantsMap);
-		
-		LOGGER.info("Calling 'updateContentNode' for Node ID: " + node.getIdentifier());
-		Response response = updateContentNode(node, urlArray[1]);
-		
-		FileType type = FileUtils.getFileType(uploadFile);
-		// Call async image optimiser for configured resolutions if asset type is image
-		if(type == FileType.Image){
-			//make async request to image optimiser actor
-			Request request = getLearningRequest(LearningActorNames.OPTIMIZER_ACTOR.name(), LearningOperations.optimizeImage.name());
-			request.put(ContentAPIParams.content_id.name(), node.getIdentifier());
-			makeAsyncLearningRequest(request, LOGGER);
+			LOGGER.info("Calling Upload Content Node For Node ID: " + node.getIdentifier());
+			String[] urlArray = uploadArtifactToAWS(uploadFile, node.getIdentifier());
+
+			LOGGER.info("Updating the Content Node for Node ID: " + node.getIdentifier());
+			node.getMetadata().put(ContentAPIParams.s3Key.name(), urlArray[0]);
+			node.getMetadata().put(ContentAPIParams.artifactUrl.name(), urlArray[1]);
+			node.getMetadata().put(ContentAPIParams.downloadUrl.name(), urlArray[1]);
+			node.getMetadata().put(ContentAPIParams.size.name(), getS3FileSize(urlArray[0]));
+			node.getMetadata().put(ContentAPIParams.status.name(), "Live");
+			Map<String, String> variantsMap = new HashMap<String, String>();
+			node.getMetadata().put(ContentAPIParams.variants.name(), variantsMap);
+
+			LOGGER.info("Calling 'updateContentNode' for Node ID: " + node.getIdentifier());
+			response = updateContentNode(node, urlArray[1]);
+
+			FileType type = FileUtils.getFileType(uploadFile);
+			// Call async image optimiser for configured resolutions if asset
+			// type is image
+			if (type == FileType.Image) {
+				// make async request to image optimiser actor
+				Request request = getLearningRequest(LearningActorNames.OPTIMIZER_ACTOR.name(),
+						LearningOperations.optimizeImage.name());
+				request.put(ContentAPIParams.content_id.name(), node.getIdentifier());
+				makeAsyncLearningRequest(request, LOGGER);
+			}
+		} catch (IOException e) {
+			throw new ServerException(ContentAPIParams.FILE_ERROR.name(),
+					"Error! While Reading the MimeType of Uploaded File. | [Node Id: " + node.getIdentifier() + "]");
+		} catch (ClientException e) {
+			throw e;
+		} catch (ServerException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new ServerException(ContentAPIParams.SERVER_ERROR.name(),
+					"Error! Something went Wrong While Uploading an Asset. | [Node Id: " + node.getIdentifier() + "]");
 		}
-		
+
 		return response;
 	}
 
@@ -95,7 +125,7 @@ public class AssetsMimeTypeMgrImpl extends BaseMimeTypeManager implements IMimeT
 		LOGGER.info("Calling 'updateContentNode' for Node ID: " + node.getIdentifier());
 		return updateContentNode(node, null);
 	}
-	
+
 	@Override
 	public Response review(Node node) {
 		LOGGER.debug("Node: ", node);
