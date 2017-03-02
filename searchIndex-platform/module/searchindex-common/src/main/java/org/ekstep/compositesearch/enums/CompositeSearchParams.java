6f92249e173b85bd447d1e1ffaa71eba0fb612f7
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-03-02 23:31:07 +0530

    Issue #723 feat: set default compatibilityLevel for content search requests

--- searchIndex-platform/module/searchindex-common/src/main/java/org/ekstep/compositesearch/enums/CompositeSearchParams.java
+++ searchIndex-platform/module/searchindex-common/src/main/java/org/ekstep/compositesearch/enums/CompositeSearchParams.java
@@ -1,5 +1,5 @@
 package org.ekstep.compositesearch.enums;
 
 public enum CompositeSearchParams {
-	operationType, CREATE, DELETE, UPDATE, nodeGraphId, nodeUniqueId, objectType, nodeType, transactionData, syncMessage, properties, addedProperties, propertyName, value, removedProperties, addedTags, removedTags, addedRelations, graphId, identifier, key, newValue, status, adedTags, RETIRED, DEFINITION_NODE, graphSyncStatus, success, query, fields, operation, values, limit, offset, filters, exists, not_exists, facets, sort_by, graph_id
+	operationType, CREATE, DELETE, UPDATE, nodeGraphId, nodeUniqueId, objectType, nodeType, compatibilityLevel, Content, transactionData, syncMessage, properties, addedProperties, propertyName, value, removedProperties, addedTags, removedTags, addedRelations, graphId, identifier, key, newValue, status, adedTags, RETIRED, DEFINITION_NODE, graphSyncStatus, success, query, fields, operation, values, limit, offset, filters, exists, not_exists, facets, sort_by, graph_id
 }
