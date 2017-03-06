8b5f3388043471c116b0605cd7a69e6eec75796c
vijethas <vijetha.nayak@riflexions.com>
2017-03-06 19:18:38 +0530

    Issue #782 feat: API Instrumentation changes to request for multipart

--- platform-config/service/src/main/java/org/ekstep/interceptor/ResponseFilter.java
+++ platform-config/service/src/main/java/org/ekstep/interceptor/ResponseFilter.java
@@ -32,17 +32,28 @@ public class ResponseFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		String requestId = getUUID();
-		RequestWrapper requestWrapper = new RequestWrapper((HttpServletRequest) request);
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		ExecutionContext.setRequestId(requestId);
-		LOGGER.info("Path: " + requestWrapper.getServletPath() + " | Remote Address: " + request.getRemoteAddr()
-				+ " | Params: " + request.getParameterMap());
+		boolean isMultipart = (httpRequest.getHeader("content-type") != null
+				&& httpRequest.getHeader("content-type").indexOf("multipart/form-data") != -1);
+		if (!isMultipart) {
+			RequestWrapper requestWrapper = new RequestWrapper(httpRequest);
+			LOGGER.info("Path: " + requestWrapper.getServletPath() + " | Remote Address: " + request.getRemoteAddr()
+			+ " | Params: " + request.getParameterMap());
+			
+			ResponseWrapper responseWrapper = new ResponseWrapper((HttpServletResponse) response);
+			requestWrapper.setAttribute("startTime", System.currentTimeMillis());
 
-		ResponseWrapper responseWrapper = new ResponseWrapper((HttpServletResponse) response);
-		requestWrapper.setAttribute("startTime", System.currentTimeMillis());
+			chain.doFilter(requestWrapper, responseWrapper);
+			
+			TelemetryAccessEventUtil.writeTelemetryEventLog(requestWrapper, responseWrapper);
+			response.getOutputStream().write(responseWrapper.getData());
+		} else {
+			LOGGER.info("Path: " + httpRequest.getServletPath() + " | Remote Address: " + request.getRemoteAddr()
+			+ " | Params: " + request.getParameterMap());
+			chain.doFilter(request, response);
+		}
 
-		chain.doFilter(requestWrapper, responseWrapper);
-		TelemetryAccessEventUtil.writeTelemetryEventLog(requestWrapper, responseWrapper);
-		response.getOutputStream().write(responseWrapper.getData());
 	}
 
 	@Override
