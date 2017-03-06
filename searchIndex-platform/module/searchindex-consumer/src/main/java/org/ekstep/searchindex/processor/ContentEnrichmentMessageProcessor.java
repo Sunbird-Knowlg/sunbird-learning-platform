9c8587eaa765b0a756d09ef09fa536ae01638454
rashmi93 <rashminrocks93@gmail.com>
2017-03-06 17:25:55 +0530

    Issue #774 feat : updated content language enrichment consumers logic to filter kafka message

--- searchIndex-platform/module/searchindex-consumer/src/main/java/org/ekstep/searchindex/processor/ContentEnrichmentMessageProcessor.java
+++ searchIndex-platform/module/searchindex-consumer/src/main/java/org/ekstep/searchindex/processor/ContentEnrichmentMessageProcessor.java
@@ -18,6 +18,7 @@ import com.ilimi.graph.dac.enums.RelationTypes;
 import com.ilimi.graph.dac.model.Node;
 import com.ilimi.graph.dac.model.Relation;
 import com.ilimi.graph.enums.CollectionTypes;
+import com.ilimi.taxonomy.content.enums.ContentWorkflowPipelineParams;
 
 /**
  * The Class ContentEnrichmentMessageProcessor is a kafka consumer which
@@ -29,7 +30,7 @@ import com.ilimi.graph.enums.CollectionTypes;
  * 
  * @see IMessageProcessor
  */
-public class ContentEnrichmentMessageProcessor extends BaseProcessor implements IMessageProcessor {
+public class ContentEnrichmentMessageProcessor implements IMessageProcessor {
 
 	/** The logger. */
 	private static Logger LOGGER = LogManager.getLogger(ContentEnrichmentMessageProcessor.class.getName());
@@ -130,8 +131,10 @@ public class ContentEnrichmentMessageProcessor extends BaseProcessor implements 
 		LOGGER.info("calling getItemsMap method to get items from item sets");
 		List<String> items = getItemsMap(node, graphId, contentId);
 
-		LOGGER.info("calling getConceptsFromItems method to get concepts from items" + items);
-		getConceptsFromItems(graphId, contentId, items, node, conceptIds, conceptGrades);
+		if(null != items && !items.isEmpty()){
+			LOGGER.info("calling getConceptsFromItems method to get concepts from items" + items);
+			getConceptsFromItems(graphId, contentId, items, node, conceptIds, conceptGrades);
+		}
 
 	}
 
@@ -468,5 +471,109 @@ public class ContentEnrichmentMessageProcessor extends BaseProcessor implements 
 			}
 		}
 		return node;
+	}
+	/**
+	 * This method holds logic to filter the kafka message and return the nodeId
+	 * 
+	 * @param message
+	 *            The kafka message
+	 * 
+	 * @return The content node
+	 * 
+	 * @throws Exception
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Node filterMessage(Map<String, Object> message) throws Exception {
+		Map<String, Object> edata = new HashMap<String, Object>();
+		Map<String, Object> eks = new HashMap<String, Object>();
+		Node node = null;
+
+		LOGGER.info("checking if kafka message contains edata");
+		if (null != message.get("edata")) {
+			LOGGER.debug("checking if kafka message contains edata" + edata);
+			edata = (Map) message.get("edata");
+
+			LOGGER.info("checking if edata contains eks");
+			if (null != edata.get("eks")) {
+				LOGGER.debug("checking if edata contains eks" + eks);
+				eks = (Map) edata.get("eks");
+
+				LOGGER.info("checking if the content is a live content");
+				if (null != eks.get("state") && StringUtils.equalsIgnoreCase("Live", eks.get("state").toString())) {
+
+					LOGGER.info("checking if eks contains cid/nodeId");
+					if (null != eks.get("cid")) {
+
+						LOGGER.debug("checking if eks contains cid" + eks);
+						node = util.getNode("domain", eks.get("cid").toString());
+						LOGGER.info("node data fetched from cid" + node);
+						return node;
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * This method holds logic to process outRelations and fetch concept nodes
+	 * grades from outRelations
+	 * 
+	 * @param outRelations
+	 *            outRelationsMap
+	 * @return result map
+	 */
+	protected Map<String, Object> getOutRelationsMap(List<Relation> outRelations) {
+
+		Set<String> nodeIds = new HashSet<String>();
+		List<String> conceptGrades = new ArrayList<String>();
+		Map<String, Object> result_map = new HashMap<String, Object>();
+
+		LOGGER.info("outRelations fetched from each item" + outRelations);
+		if (null != outRelations && !outRelations.isEmpty()) {
+
+			LOGGER.info("Iterating through outrelations");
+			for (Relation rel : outRelations) {
+
+				LOGGER.info("checking if endNodeType is objectType");
+				if (null != rel.getEndNodeObjectType()
+						&& StringUtils.equalsIgnoreCase("Concept", rel.getEndNodeObjectType())) {
+
+					String status = null;
+					LOGGER.info("checking relation contains status");
+					if (null != rel.getEndNodeMetadata().get("status")) {
+						LOGGER.info("getting status from node");
+						status = (String) rel.getEndNodeMetadata().get(ContentWorkflowPipelineParams.status.name());
+					}
+
+					LOGGER.info("checking if status is LIVE and fetching nodeIds from it" + status);
+					if (StringUtils.isNotBlank(status)
+							&& StringUtils.equalsIgnoreCase(ContentWorkflowPipelineParams.Live.name(), status)) {
+						LOGGER.info("nodeIds fetched form LIVE nodes" + nodeIds);
+						nodeIds.add(rel.getEndNodeId());
+					}
+
+					LOGGER.info("checking if concept contains gradeLevel");
+					if (null != rel.getEndNodeMetadata().get("gradeLevel")) {
+						String[] grade_array =  (String[])(rel.getEndNodeMetadata().get("gradeLevel"));
+						for(String garde : grade_array){
+							conceptGrades.add(garde);
+						}
+					}
+				}
+			}
+
+			LOGGER.info("Adding nodeIds to map" + nodeIds);
+			if (null != nodeIds && !nodeIds.isEmpty()) {
+				result_map.put("conceptIds", nodeIds);
+			}
+
+			LOGGER.info("Adding conceptGrades to map" + conceptGrades);
+			if (null != conceptGrades && !conceptGrades.isEmpty()) {
+				result_map.put("conceptGrades", conceptGrades);
+			}
+		}
+		LOGGER.info("Map of conceptGrades and nodeIds" + result_map);
+		return result_map;
 	}
 }
