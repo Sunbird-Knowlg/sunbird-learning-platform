bcdadc02358f482b1962ae973c8daf30f91275dd
Mohammad Azharuddin <azhar@ilimi.in>
2017-04-03 20:59:08 +0530

    Issue #848 Feat : isImageObject added as Taxonomy Params (Task: #850)

--- platform-modules/manager/src/main/java/com/ilimi/taxonomy/enums/TaxonomyAPIParams.java
+++ platform-modules/manager/src/main/java/com/ilimi/taxonomy/enums/TaxonomyAPIParams.java
@@ -2,5 +2,5 @@ package com.ilimi.taxonomy.enums;
 
 public enum TaxonomyAPIParams {
 
-	taxonomy, taxonomy_hierarchy, concepts, search_criteria, property_keys, unique_constraint, status, Flagged, Live, Draft;
+	taxonomy, taxonomy_hierarchy, concepts, search_criteria, property_keys, unique_constraint, status, Flagged, Live, Draft, isImageObject;
 }
