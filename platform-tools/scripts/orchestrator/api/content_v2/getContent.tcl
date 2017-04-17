7f7b98ac2f91e89fff452f17bf1a4cb137c1e8ce
rashmi93 <rashminrocks93@gmail.com>
2017-04-17 18:13:59 +0530

    Issue #848 feat: fixed issue with getContent TCL script for edit mode

--- platform-tools/scripts/orchestrator/api/content_v2/getContent.tcl
+++ platform-tools/scripts/orchestrator/api/content_v2/getContent.tcl
@@ -73,7 +73,7 @@ set graph_id "domain"
 set content_image_id ${content_id}.img
 set is_mode_null [java::isnull $mode]
 set resp_get_node [java::null]]
-if {($is_mode_null != 0) && ($mode == "edit")} {
+if {($is_mode_null == 0) && ([$mode toString] == "edit")} {
 	set resp_get_node [getDataNode $graph_id $content_image_id]
 	set check_error [check_response_error $resp_get_node]
 	if {$check_error} {
