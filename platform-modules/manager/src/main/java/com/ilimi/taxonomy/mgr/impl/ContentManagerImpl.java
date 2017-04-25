23bd5f0697c13b4f0f2a16f7241a84e2a02ebe27
rashmi93 <rashminrocks93@gmail.com>
2017-04-25 13:40:08 +0530

    Issue #835 fix : fixed issues with contentforking upload workflow

--- platform-modules/manager/src/main/java/com/ilimi/taxonomy/mgr/impl/ContentManagerImpl.java
+++ platform-modules/manager/src/main/java/com/ilimi/taxonomy/mgr/impl/ContentManagerImpl.java
@@ -164,6 +164,12 @@ public class ContentManagerImpl extends BaseManager implements IContentManager {
 		}
 
 		LOGGER.info("Returning Response.");
+		if(StringUtils.endsWith(res.getResult().get("identifier").toString(), ".img")){
+			 String identifier = (String)res.getResult().get("identifier");
+			 String new_identifier = identifier.replace(".img", "");
+			 LOGGER.info("replacing image id with content id in response" + identifier + new_identifier);
+			 res.getResult().replace("identifier", identifier, new_identifier);
+		}
 		return res;
 	}
 
