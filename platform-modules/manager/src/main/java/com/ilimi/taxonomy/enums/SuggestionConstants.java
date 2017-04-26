19f966fde77d495936041bafa618f6a246eb658a
rashmi93 <rashminrocks93@gmail.com>
2017-04-26 12:48:33 +0530

    Issue #900 feat: curation workflow implemenation

---
+++ platform-modules/manager/src/main/java/com/ilimi/taxonomy/enums/SuggestionConstants.java
@@ -0,0 +1,8 @@
+package com.ilimi.taxonomy.enums;
+
+public class SuggestionConstants {
+	 public static final String SUGGESTION_INDEX= "lp_suggestion_index";
+	 public static final String SUGGESTION_INDEX_TYPE = "sg";
+	 public static final String GRAPH_ID = "domain";
+			 
+}
