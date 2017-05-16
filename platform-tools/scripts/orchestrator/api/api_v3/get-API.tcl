4b399a91dac496dc9e82cdffb75e8f064c78394b
rashmi93 <rashminrocks93@gmail.com>
2017-05-16 13:32:58 +0530

    Issue #940 feat: Enchance Developer Workflow - API definition and API tcl scripts

---
+++ platform-tools/scripts/orchestrator/api/api_v3/get-API.tcl
@@ -0,0 +1,16 @@
+package require java
+java::import -package java.util ArrayList List
+java::import -package java.util HashMap Map
+java::import -package com.ilimi.graph.dac.model Node
+
+set object_type "api"
+set graph_id "domain"
+
+set get_node_response [getObject $graph_id $object_type $api_id]
+set api_object [get_resp_value $get_node_response "object"]
+
+set result_map [java::new HashMap]
+$result_map put "api" $api_object
+set api_response [create_response $result_map]
+return $api_response
+
---
+++ platform-tools/scripts/orchestrator/api/api_v3/list-API.tcl
@@ -0,0 +1,15 @@
+package require java
+java::import -package java.util ArrayList List
+java::import -package java.util HashMap Map
+java::import -package com.ilimi.graph.dac.model Node
+
+set object_type "api"
+set graph_id "domain"
+
+set get_node_response [getAllObjects $graph_id $object_type]
+set api_object [get_resp_value $get_node_response "object_list"]
+
+set result_map [java::new HashMap]
+$result_map put "apis" $api_object
+set api_response [create_response $result_map]
+return $api_response
