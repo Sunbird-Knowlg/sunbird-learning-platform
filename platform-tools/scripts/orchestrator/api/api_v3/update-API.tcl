0dc9a4885a6769e80702c87379367b0574d98943
rashmi93 <rashminrocks93@gmail.com>
2017-05-16 13:44:08 +0530

    Issue #940 feat: Enchance Developer Workflow - API definition and API tcl scripts

---
+++ platform-tools/scripts/orchestrator/api/api_v3/update-API.tcl
@@ -0,0 +1,11 @@
+package require java
+java::import -package java.util ArrayList List
+java::import -package java.util HashMap Map
+java::import -package com.ilimi.graph.dac.model Node
+
+set object_type "api"
+set graph_id "domain"
+
+set update_response [updateObject $graph_id $api $object_type $api_id]
+return $update_response
+
