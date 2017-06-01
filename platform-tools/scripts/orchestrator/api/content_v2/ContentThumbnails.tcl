f3c46400e8a66bedd3e3d4bbf1d8d424addd1c0d
rashmi93 <rashminrocks93@gmail.com>
2017-06-01 15:14:15 +0530

    Issue #962 feat: Script to Migrate existing screenshots to thumbnails

---
+++ platform-tools/scripts/orchestrator/api/content_v2/ContentThumbnails.tcl
@@ -0,0 +1,104 @@
+package require java
+java::import -package java.util HashMap Map Date
+java::import -package java.util ArrayList List
+java::import -package java.util HashSet Set
+java::import -package java.util Arrays
+java::import -package com.ilimi.graph.dac.model Node
+java::import -package com.ilimi.graph.common DateUtils
+java::import -package com.ilimi.graph.dac.model Relation
+java::import -package java.util Arrays
+
+set graph_id "domain"
+set object_type "Content"
+set image_object_type "ContentImage" 
+
+proc isNotEmpty {list} {
+	set exist false
+	set isListNull [java::isnull $list]
+	if {$isListNull == 0} {
+		set listSize [$list size]
+		if {$listSize > 0} {
+			set exist true
+		}
+	}
+	return $exist
+}
+
+proc proc_isNotNull {value} {
+	set exist false
+	java::try {
+		set hasValue [java::isnull $value]
+		if {$hasValue == 0} {
+			set exist true
+		}
+	} catch {Exception err} {
+    	set exist false
+	}
+	return $exist
+}
+
+proc proc_getScreenshots {graph_node} {
+        set urlSet [java::new HashSet]
+	set result_map [java::new Relation]
+        set node_metadata [java::prop $graph_node "metadata"]
+	set outRelations [java::prop $graph_node "outRelations"]
+	java::for {Relation object} $outRelations {
+	set resultMap $object
+	set relationType [java::prop $resultMap "relationType"]
+		if {$relationType == "associatedTo"} {
+			set endNodeObjectType [java::prop $resultMap "endNodeObjectType"]
+			if {$endNodeObjectType == "Content"} {
+		 		set endNodeMetadata [java::prop $resultMap "endNodeMetadata"]
+				set endNodeContentType [$endNodeMetadata get "contentType"]
+				set contentType [$endNodeContentType toString]
+				if {$contentType == "Asset"} {
+				     set artifactUrl [$endNodeMetadata get "artifactUrl"]
+				     set url [java::cast String $artifactUrl]
+				     set urlNotNull [proc_isNotNull $url]
+	 			     if {$urlNotNull} { 
+					$urlSet add $url
+		                     }
+			       }
+		        } 
+                 }
+        }
+        set screenshots [$node_metadata get "screenshots"]
+        set arr_instance [java::instanceof $screenshots {String[]}]
+	if {$arr_instance == 1} {
+		set existingAsset [java::cast {String[]} $screenshots]
+		set existingData [java::call Arrays asList $existingAsset]
+                        
+        }
+        set screenShotNotNull [proc_isNotNull $existingData]
+        if {$screenShotNotNull} {
+   	 	$urlSet addAll $existingData
+        }
+	set urls [java::new ArrayList $urlSet]
+        $node_metadata put "screenshots" $urls
+        return $graph_node
+}
+
+set resp_get_node [getDataNode $graph_id $content_id]
+set check_error [check_response_error $resp_get_node]
+if {$check_error} {
+	return $resp_get_node;
+} else {
+        	
+		set graph_node [get_resp_value $resp_get_node "node"]
+		set node_object_type [java::prop $graph_node "objectType"]
+        	
+		if {$node_object_type == $object_type} {
+                	proc_getScreenshots $graph_node
+                        set create_response [updateDataNode $graph_id $content_id $graph_node]
+		}
+		if {$node_object_type == $image_object_type} {
+                        set content_id ${content_id}.img
+                        set resp_get_node [getDataNode $graph_id $content_id]
+                        set image_node [get_resp_value $resp_get_node "node"]
+                        proc_getScreenshots $$image_node
+                        set create_image_response [updateDataNode $graph_id $content_id $image_node]
+           
+                 }	            
+                 return $create_response
+	}
+    
