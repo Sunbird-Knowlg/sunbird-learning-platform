69a49c9cf45a82c9391d1a457aa700b0ead6da3e
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-07-05 23:50:36 +0530

    Issue #1039 feat : Add Course and CourseUnit content types

--- platform-modules/content-manager/src/main/java/org/ekstep/content/operation/finalizer/PublishFinalizer.java
+++ platform-modules/content-manager/src/main/java/org/ekstep/content/operation/finalizer/PublishFinalizer.java
@@ -180,6 +180,15 @@ public class PublishFinalizer extends BaseFinalizer {
 				|| StringUtils.containsIgnoreCase((String) node.getMetadata().get(ContentWorkflowPipelineParams.mimeType.name()), ContentWorkflowPipelineParams.msword.name()))
 			node.getMetadata().put(ContentWorkflowPipelineParams.compatibilityLevel.name(), 4);
 		
+		LOGGER.info("setting compatability level for course and coure unit");
+		if (StringUtils.equalsIgnoreCase(
+				(String) node.getMetadata().get(ContentWorkflowPipelineParams.contentType.name()),
+				ContentWorkflowPipelineParams.Course.name())
+				|| StringUtils.equalsIgnoreCase(
+						(String) node.getMetadata().get(ContentWorkflowPipelineParams.contentType.name()),
+						ContentWorkflowPipelineParams.CourseUnit.name()))
+			node.getMetadata().put(ContentWorkflowPipelineParams.compatibilityLevel.name(), 4);
+		
 		LOGGER.info("checking is the contentType is Asset");
 		if (BooleanUtils.isFalse(isAssetTypeContent)) {
 			// Create ECAR Bundle
