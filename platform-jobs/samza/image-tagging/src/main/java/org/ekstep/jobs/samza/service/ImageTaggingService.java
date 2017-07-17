9060df91520ee2a7fc40522e105dd1675e4a504d
rashmi93 <rashminrocks93@gmail.com>
2017-07-18 00:43:11 +0530

    Issue #956 feat : samza job - imageTagging | Google Vision API

--- platform-jobs/samza/image-tagging/src/main/java/org/ekstep/jobs/samza/service/ImageTaggingService.java
+++ platform-jobs/samza/image-tagging/src/main/java/org/ekstep/jobs/samza/service/ImageTaggingService.java
@@ -17,11 +17,10 @@ import org.apache.samza.config.Config;
 import org.apache.samza.task.MessageCollector;
 import org.ekstep.common.util.HttpDownloadUtility;
 import org.ekstep.jobs.samza.service.task.JobMetrics;
-import org.ekstep.jobs.samza.util.ConsumerWorkflowEnums;
+import org.ekstep.jobs.samza.util.ImageWorkflowEnums;
 import org.ekstep.jobs.samza.util.JobLogger;
 import org.ekstep.jobs.samza.util.OptimizerUtil;
 import org.ekstep.jobs.samza.util.VisionApi;
-import org.ekstep.learning.common.enums.ContentAPIParams;
 import org.ekstep.learning.router.LearningRequestRouterPool;
 import org.ekstep.learning.util.ControllerUtil;
 
@@ -56,29 +55,29 @@ public class ImageTaggingService implements ISamzaService {
 		Map<String, Object> edata = new HashMap<String, Object>();
 		Map<String, Object> eks = new HashMap<String, Object>();
 		try{
-		if (null != message.get(ContentAPIParams.edata.name())) {
-			edata = (Map) message.get(ContentAPIParams.edata.name());
-			if (null != edata.get(ContentAPIParams.eks.name())) {
-				eks = (Map) edata.get(ContentAPIParams.eks.name());
+		if (null != message.get(ImageWorkflowEnums.edata.name())) {
+			edata = (Map) message.get(ImageWorkflowEnums.edata.name());
+			if (null != edata.get(ImageWorkflowEnums.eks.name())) {
+				eks = (Map) edata.get(ImageWorkflowEnums.eks.name());
 				if (null != eks) {
-					if (null != eks.get(ContentAPIParams.type.name()) && null != eks.get(ContentAPIParams.subtype.name())) {
-						if ((StringUtils.equalsIgnoreCase(eks.get(ContentAPIParams.type.name()).toString(), ContentAPIParams.Asset.name()))
-								&& (StringUtils.equalsIgnoreCase(eks.get(ContentAPIParams.subtype.name()).toString(), ContentAPIParams.image.name()))) {
+					if (null != eks.get(ImageWorkflowEnums.type.name()) && null != eks.get(ImageWorkflowEnums.subtype.name())) {
+						if ((StringUtils.equalsIgnoreCase(eks.get(ImageWorkflowEnums.type.name()).toString(), ImageWorkflowEnums.Asset.name()))
+								&& (StringUtils.equalsIgnoreCase(eks.get(ImageWorkflowEnums.subtype.name()).toString(), ImageWorkflowEnums.image.name()))) {
 							Map<String, String> variantsMap;
 							try {
-								variantsMap = OptimizerUtil.optimiseImage(eks.get(ContentAPIParams.id.name()).toString());
-								String nodeId = (String) eks.get(ContentAPIParams.id.name());
-								Node node = util.getNode(ConsumerWorkflowEnums.domain.name(), nodeId);
+								variantsMap = OptimizerUtil.optimiseImage(eks.get(ImageWorkflowEnums.id.name()).toString());
+								String nodeId = (String) eks.get(ImageWorkflowEnums.id.name());
+								Node node = util.getNode(ImageWorkflowEnums.domain.name(), nodeId);
 								if (null == variantsMap)
 									variantsMap = new HashMap<String, String>();
-								if (StringUtils.isBlank(variantsMap.get(ContentAPIParams.medium.name()))) {
+								if (StringUtils.isBlank(variantsMap.get(ImageWorkflowEnums.medium.name()))) {
 									String downloadUrl = (String) node.getMetadata()
-											.get(ContentAPIParams.downloadUrl.name());
+											.get(ImageWorkflowEnums.downloadUrl.name());
 									if (StringUtils.isNotBlank(downloadUrl)) {
-										variantsMap.put(ContentAPIParams.medium.name(), downloadUrl);
+										variantsMap.put(ImageWorkflowEnums.medium.name(), downloadUrl);
 									}
 								}
-								String image_url = variantsMap.get(ContentAPIParams.medium.name());
+								String image_url = variantsMap.get(ImageWorkflowEnums.medium.name());
 								processImage(image_url, variantsMap, node);
 							} catch (Exception e) {
 								e.printStackTrace();
@@ -99,11 +98,11 @@ public class ImageTaggingService implements ISamzaService {
 			LOGGER.info("Fetching google.vision property from config" + key);
 			if ("true".equalsIgnoreCase(key)) {
 				Node data = callVisionService(image_url, node, variantsMap);
-				data.getMetadata().put(ContentAPIParams.variants.name(), variantsMap);
+				data.getMetadata().put(ImageWorkflowEnums.variants.name(), variantsMap);
 				OptimizerUtil.controllerUtil.updateNode(data);
 			} else {
-				node.getMetadata().put(ContentAPIParams.status.name(), ContentAPIParams.Live.name());
-				node.getMetadata().put(ContentAPIParams.variants.name(), variantsMap);
+				node.getMetadata().put(ImageWorkflowEnums.status.name(), ImageWorkflowEnums.Live.name());
+				node.getMetadata().put(ImageWorkflowEnums.variants.name(), variantsMap);
 				OptimizerUtil.controllerUtil.updateNode(node);
 			}
 		} catch (Exception e) {
@@ -137,38 +136,38 @@ public class ImageTaggingService implements ISamzaService {
 		}
 		try {
 			List<String> node_keywords = new ArrayList<String>();
-			if (null != node.getMetadata().get(ContentAPIParams.keywords.name())) {
-				Object object = node.getMetadata().get(ContentAPIParams.keywords.name());
+			if (null != node.getMetadata().get(ImageWorkflowEnums.keywords.name())) {
+				Object object = node.getMetadata().get(ImageWorkflowEnums.keywords.name());
 
 				if (object instanceof String[]) {
-					String[] stringArray = (String[]) node.getMetadata().get(ContentAPIParams.keywords.name());
+					String[] stringArray = (String[]) node.getMetadata().get(ImageWorkflowEnums.keywords.name());
 					List keywords = Arrays.asList(stringArray);
 					node_keywords = setKeywords(keywords, labels);
 				}
 
 				if (object instanceof String) {
-					String keyword = (String) node.getMetadata().get(ContentAPIParams.keywords.name());
+					String keyword = (String) node.getMetadata().get(ImageWorkflowEnums.keywords.name());
 					node_keywords.add(keyword);
 					node_keywords = setKeywords(node_keywords, labels);
 				}
 			}
 
 			if (!node_keywords.isEmpty()) {
-				node.getMetadata().put(ContentAPIParams.keywords.name(), node_keywords);
+				node.getMetadata().put(ImageWorkflowEnums.keywords.name(), node_keywords);
 			}
-			node.getMetadata().put(ContentAPIParams.status.name(), ContentAPIParams.Live.name());
+			node.getMetadata().put(ImageWorkflowEnums.status.name(), ImageWorkflowEnums.Live.name());
 			List<String> flaggedByList = new ArrayList<>();
-			if (null != node.getMetadata().get(ContentAPIParams.flaggedBy.name())) {
-				flaggedByList.addAll((Collection<? extends String>) node.getMetadata().get(ContentAPIParams.flaggedBy.name()));
+			if (null != node.getMetadata().get(ImageWorkflowEnums.flaggedBy.name())) {
+				flaggedByList.addAll((Collection<? extends String>) node.getMetadata().get(ImageWorkflowEnums.flaggedBy.name()));
 			}
 
 			if (null != flags && (!flags.isEmpty())) {
-				node.getMetadata().put(ContentAPIParams.flags.name(), flags);
-				flaggedByList.add(ContentAPIParams.Ekstep.name());
-				node.getMetadata().put(ContentAPIParams.flaggedBy.name(), flaggedByList);
-				node.getMetadata().put(ContentAPIParams.versionKey.name(), node.getMetadata().get(ContentAPIParams.versionKey.name()));
-				node.getMetadata().put(ContentAPIParams.status.name(), ContentAPIParams.Flagged.name());
-				node.getMetadata().put(ContentAPIParams.lastFlaggedOn.name(), new Date().toString());
+				node.getMetadata().put(ImageWorkflowEnums.flags.name(), flags);
+				flaggedByList.add(ImageWorkflowEnums.Ekstep.name());
+				node.getMetadata().put(ImageWorkflowEnums.flaggedBy.name(), flaggedByList);
+				node.getMetadata().put(ImageWorkflowEnums.versionKey.name(), node.getMetadata().get(ImageWorkflowEnums.versionKey.name()));
+				node.getMetadata().put(ImageWorkflowEnums.status.name(), ImageWorkflowEnums.Flagged.name());
+				node.getMetadata().put(ImageWorkflowEnums.lastFlaggedOn.name(), new Date().toString());
 			}
 
 		} catch (Exception e) {
---
+++ platform-jobs/samza/image-tagging/src/main/java/org/ekstep/jobs/samza/util/ImageWorkflowEnums.java
@@ -0,0 +1,7 @@
+package org.ekstep.jobs.samza.util;
+
+public enum ImageWorkflowEnums {
+Live, state, Image,subtype, versionKey, downloadUrl, medium, id, edata, eks,image, variants,
+flaggedBy,keywords, flags, Flagged, lastFlaggedOn, type, contentType, mediaType, 
+Asset, status, domain, Ekstep;
+}
