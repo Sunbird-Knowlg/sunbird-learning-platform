c661d781f11abc56021172dda0798499629d44fd
rashmi93 <rashminrocks93@gmail.com>
2017-07-17 22:37:48 +0530

    Issue #956 feat : publish-pipeline samza job

--- platform-jobs/samza/publish-pipeline/src/main/java/org/ekstep/jobs/samza/service/PublishPipelineService.java
+++ platform-jobs/samza/publish-pipeline/src/main/java/org/ekstep/jobs/samza/service/PublishPipelineService.java
@@ -16,7 +16,6 @@ import org.ekstep.jobs.samza.util.PublishPipelineParams;
 import org.ekstep.learning.common.enums.ContentAPIParams;
 import org.ekstep.learning.router.LearningRequestRouterPool;
 import org.ekstep.learning.util.ControllerUtil;
-import com.ilimi.common.logger.PlatformLogger;
 import com.ilimi.graph.common.mgr.Configuration;
 import com.ilimi.graph.dac.enums.SystemNodeTypes;
 import com.ilimi.graph.dac.model.Node;
@@ -52,41 +51,50 @@ public class PublishPipelineService implements ISamzaService {
 	public void processMessage(Map<String, Object> message, JobMetrics metrics, MessageCollector collector)
 			throws Exception {
 		try {
-			Node node = getNode(message);
-			String contentId = (String) message.get(PublishPipelineParams.nodeUniqueId.name());
+			Map<String,Object> eksMap = getMap(message);
+			Node node = getNode(eksMap);
+			String contentId = (String) eksMap.get(PublishPipelineParams.id.name());
 			Map<String, Object> parameterMap = new HashMap<String, Object>();
 			parameterMap.put(ContentAPIParams.node.name(), node);
 			String mimeType = (String) node.getMetadata().get("mimeType");
+			LOGGER.info("Checking if node mimeType is ecml" + mimeType);
 			if (StringUtils.equalsIgnoreCase(mimeType, "application/vnd.ekstep.ecml-archive")) {
 				parameterMap.put(PublishPipelineParams.ecmlType.name(), true);
 			} else {
 				parameterMap.put(PublishPipelineParams.ecmlType.name(), false);
 			}
 			parameterMap.put(PublishPipelineParams.node.name(), node);
+			LOGGER.info("Start Publish Pipeline | Initiatizing Publish Pipeline");
 			InitializePipeline pipeline = new InitializePipeline(getBasePath(contentId), contentId);
 			pipeline.init(ContentWorkflowPipelineParams.publish.name(), parameterMap);
 			LOGGER.info("Node fetched from graph");
 		} catch (Exception e) {
+			LOGGER.info("Something went wrong while publishing the content" + e.getMessage());
 			e.printStackTrace();
 		}
 	}
 
 	private Node getNode(Map<String, Object> message) throws Exception {
-		String nodeId = (String) message.get(PublishPipelineParams.nodeUniqueId.name());
-		Map<String, Object> statusMap = getStateChangeEvent(message);
-		String prevstate = (String) statusMap.get("ov");
-		String state = (String) statusMap.get("nv");
-		if (StringUtils.equalsIgnoreCase("Draft", prevstate) && StringUtils.equalsIgnoreCase("Processing", state)) {
+		String nodeId = (String) message.get(PublishPipelineParams.id.name());
+		String state = (String) message.get(PublishPipelineParams.State.name());
+		LOGGER.info("Checking if node status is Processing" + state);
+		if (StringUtils.equalsIgnoreCase(PublishPipelineParams.Processing.name(), state)) {
+			LOGGER.info("Fetching Node required for Publish Operation" + nodeId);
 			return getNodeForOperation(PublishPipelineParams.domain.name(), nodeId);
 		}
 		return null;
 	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private Map<String,Object> getMap(Map<String, Object> message){
+		Map<String,Object> edata = (Map)message.get("edata"); 
+		Map<String,Object> eks = (Map)edata.get("eks");
+		return eks;
+	}
+	
 	private Node getNodeForOperation(String taxonomyId, String contentId) {
 		Node node = new Node();
-
 		String contentImageId = getContentImageIdentifier(contentId);
-		LOGGER.info("Fetching the Content Node. | [Content ID: " + contentId + "]");
 
 		LOGGER.info("Fetching the Content Image Node for Content Id: " + contentId);
 		Node imageNode = util.getNode(taxonomyId, contentImageId);
@@ -95,7 +103,7 @@ public class PublishPipelineService implements ISamzaService {
 			LOGGER.info("Trying to Fetch Content Node (Not Image Node) for Content Id: " + contentId);
 			node = util.getNode(taxonomyId, contentId);
 			if (null != node) {
-				PlatformLogger.log("Fetched Content Node: ", node);
+				LOGGER.info("Fetched Content Node: "+ node);
 				String status = (String) node.getMetadata().get(PublishPipelineParams.status.name());
 				if (StringUtils.isNotBlank(status)
 						&& (StringUtils.equalsIgnoreCase(PublishPipelineParams.Live.name(), status)
@@ -118,6 +126,7 @@ public class PublishPipelineService implements ISamzaService {
 		imageNode.setOutRelations(node.getOutRelations());
 		imageNode.setTags(node.getTags());
 		imageNode.getMetadata().put(PublishPipelineParams.status.name(), PublishPipelineParams.Draft.name());
+		LOGGER.info("Creating Image Node");
 		util.createDataNode(imageNode);
 		Node nodeData = util.getNode(taxonomyId, contentImageId);
 		return nodeData;
@@ -129,28 +138,6 @@ public class PublishPipelineService implements ISamzaService {
 			path = tempFileLocation + File.separator + System.currentTimeMillis()
 					+ ContentWorkflowPipelineParams._temp.name() + File.separator + contentId;
 		return path;
-	}
-
-	@SuppressWarnings("unchecked")
-	private Map<String, Object> getStateChangeEvent(Map<String, Object> message) {
-
-		if (!message.containsKey(PublishPipelineParams.nodeUniqueId.name()))
-			return null;
-		if (!message.containsKey(PublishPipelineParams.transactionData.name()))
-			return null;
-		Map<String, Object> transactionMap = (Map<String, Object>) message
-				.get(PublishPipelineParams.transactionData.name());
-
-		if (!transactionMap.containsKey(PublishPipelineParams.properties.name()))
-			return null;
-		Map<String, Object> propertiesMap = (Map<String, Object>) transactionMap
-				.get(PublishPipelineParams.properties.name());
-
-		if (propertiesMap.containsKey(PublishPipelineParams.status.name())) {
-			return (Map<String, Object>) propertiesMap.get(PublishPipelineParams.status.name());
-		}
-
-		return null;
 	}
 
 	private String getContentImageIdentifier(String contentId) {
