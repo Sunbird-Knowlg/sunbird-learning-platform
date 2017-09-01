921374ab5ffe98ea65efc30cdd900f18a7089ee4
Mahesh <mahesh@canopusconsulting.com>
2017-09-01 16:32:11 +0530

    Issue #000 fix: tags backward compatibility in get api response.

--- platform-modules/common/src/main/java/com/ilimi/common/mgr/ConvertGraphNode.java
+++ platform-modules/common/src/main/java/com/ilimi/common/mgr/ConvertGraphNode.java
@@ -88,7 +88,8 @@ public class ConvertGraphNode {
                 }
                 updateReturnMap(map, outRelMap, outRelDefMap);
             }
-
+            if (null != node.getTags() && !node.getTags().isEmpty())
+            		map.put("tags", node.getTags());
             map.put("identifier", node.getIdentifier());
         }
         return map;
