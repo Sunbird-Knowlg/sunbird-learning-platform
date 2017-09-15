2b6ba527b8da3ecdc2a5683eeb8b1b0d92b490df
rashmi93 <rashminrocks93@gmail.com>
2017-09-15 15:03:28 +0530

    Issue #956 chore : remove inMemory usage from set and sequence manager

--- platform-core/graph-engine/module/cache-manager/src/main/java/com/ilimi/graph/cache/mgr/impl/SetCacheManager.java
+++ platform-core/graph-engine/module/cache-manager/src/main/java/com/ilimi/graph/cache/mgr/impl/SetCacheManager.java
@@ -4,14 +4,11 @@ import static com.ilimi.graph.cache.factory.JedisFactory.getRedisConncetion;
 import static com.ilimi.graph.cache.factory.JedisFactory.returnConnection;
 import java.util.ArrayList;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 
-import com.ilimi.common.Platform;
 import com.ilimi.common.exception.ClientException;
 import com.ilimi.common.exception.ServerException;
 import com.ilimi.graph.cache.exception.GraphCacheErrorCodes;
@@ -20,12 +17,9 @@ import redis.clients.jedis.Jedis;
 
 public class SetCacheManager {
 
-	private static Map<String, Object> setCache = new HashMap<String, Object>();
-
 	public static void createSet(String graphId, String setId, List<String> members) {
 		validateRequired(graphId, setId, members, GraphCacheErrorCodes.ERR_CACHE_CREATE_SET_ERROR.name());
 		String key = CacheKeyGenerator.getSetMembersKey(graphId, setId);
-		if ("redis".equalsIgnoreCase(cacheType())) {
 			Jedis jedis = getRedisConncetion();
 			try {
 				String[] tempMembers = new String[members.size()];
@@ -34,38 +28,23 @@ public class SetCacheManager {
 			} finally {
 				returnConnection(jedis);
 			}
-		} else {
-			setCache.put(key, members);
-		}
 	}
 
-	@SuppressWarnings("unchecked")
 	public static void addSetMember(String graphId, String setId, String memberId) {
 		validateRequired(graphId, setId, memberId, GraphCacheErrorCodes.ERR_CACHE_ADD_SET_MEMBER.name());
 		String key = CacheKeyGenerator.getSetMembersKey(graphId, setId);
 		Jedis jedis = getRedisConncetion();
-		if ("redis".equalsIgnoreCase(cacheType())) {
 			try {
 				jedis.sadd(key, memberId);
 			} finally {
 				returnConnection(jedis);
 			}
-		} else {
-			List<String> existingMembers = (List<String>) setCache.get(key);
-			if (null == existingMembers)
-				existingMembers = new ArrayList<String>();
-			if (!existingMembers.contains(memberId))
-				existingMembers.add(memberId);
-			setCache.put(key, existingMembers);
-		}
 	}
 
-	@SuppressWarnings("unchecked")
 	public static void addSetMembers(String graphId, String setId, List<String> memberIds) {
 		validateRequired(graphId, setId, memberIds, GraphCacheErrorCodes.ERR_CACHE_ADD_SET_MEMBER.name());
 		Jedis jedis = getRedisConncetion();
 		String key = CacheKeyGenerator.getSetMembersKey(graphId, setId);
-		if ("redis".equalsIgnoreCase(cacheType())) {
 			try {
 				String[] members = new String[memberIds.size()];
 				for (int i = 0; i < memberIds.size(); i++) {
@@ -77,24 +56,12 @@ public class SetCacheManager {
 			} finally {
 				returnConnection(jedis);
 			}
-		} else {
-			List<String> existingMembers = (List<String>) setCache.get(key);
-			if (null == existingMembers)
-				existingMembers = new ArrayList<String>();
-			for (String id : memberIds) {
-				if (!existingMembers.contains(id))
-					existingMembers.add(id);
-			}
-			setCache.put(key, existingMembers);
-		}
 	}
 
-	@SuppressWarnings("unchecked")
 	public static void removeSetMember(String graphId, String setId, String memberId) {
 		validateRequired(graphId, setId, memberId, GraphCacheErrorCodes.ERR_CACHE_REMOVE_SET_MEMBER.name());
 		Jedis jedis = getRedisConncetion();
 		String key = CacheKeyGenerator.getSetMembersKey(graphId, setId);
-		if ("redis".equalsIgnoreCase(cacheType())) {
 			try {
 				jedis.srem(key, memberId);
 			} catch (Exception e) {
@@ -102,20 +69,12 @@ public class SetCacheManager {
 			} finally {
 				returnConnection(jedis);
 			}
-		} else {
-			List<String> existingMembers = (List<String>) setCache.get(key);
-			if (null == existingMembers)
-				existingMembers = new ArrayList<String>();
-			if (existingMembers.contains(memberId))
-				setCache.remove(memberId);
-		}
 	}
 
 	public static void dropSet(String graphId, String setId) {
 		validateRequired(graphId, setId, GraphCacheErrorCodes.ERR_CACHE_DROP_SET.name());
 		Jedis jedis = getRedisConncetion();
 		String key = CacheKeyGenerator.getSetMembersKey(graphId, setId);
-		if ("redis".equalsIgnoreCase(cacheType())) {
 			try {
 				jedis.del(key);
 			} catch (Exception e) {
@@ -123,18 +82,12 @@ public class SetCacheManager {
 			} finally {
 				returnConnection(jedis);
 			}
-		} else {
-			if (setCache.containsKey(key))
-				setCache.remove(key);
-		}
 	}
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static List<String> getSetMembers(String graphId, String setId) {
 		validateRequired(graphId, setId, GraphCacheErrorCodes.ERR_CACHE_SET_GET_MEMBERS.name());
 		String key = CacheKeyGenerator.getSetMembersKey(graphId, setId);
 		List<String> members = new ArrayList<String>();
-		if ("redis".equalsIgnoreCase(cacheType())) {
 			Jedis jedis = getRedisConncetion();
 			try {
 				Set<String> memberIds = jedis.smembers(key);
@@ -143,22 +96,14 @@ public class SetCacheManager {
 				}
 			} finally {
 				returnConnection(jedis);
-			}
-		} else {
-			members = (List) setCache.get(key);
-			if (null != members && members.size() > 0) {
-				return members;
-			}
 		}
 		return members;
 	}
 
-	@SuppressWarnings({ "unchecked" })
 	public static Long getSetCardinality(String graphId, String setId) {
 		validateRequired(graphId, setId, GraphCacheErrorCodes.ERR_CACHE_SET_GET_MEMBERS.name());
 		Jedis jedis = getRedisConncetion();
 		String key = CacheKeyGenerator.getSetMembersKey(graphId, setId);
-		if ("redis".equalsIgnoreCase(cacheType())) {
 			try {
 				Long cardinality = jedis.scard(key);
 				System.out.println(cardinality);
@@ -168,22 +113,13 @@ public class SetCacheManager {
 			} finally {
 				returnConnection(jedis);
 			}
-		} else {
-			List<String> members = (List<String>) setCache.get(key);
-        	if (null != members) {
-        		return (long) members.size();
-        	} else 
-			return 0l;
-		}
 	}
 
-	@SuppressWarnings("unchecked")
 	public static Boolean isSetMember(String graphId, String setId, String member) {
 		validateRequired(graphId, setId, member, GraphCacheErrorCodes.ERR_CACHE_SET_GET_MEMBERS.name());
 		Jedis jedis = getRedisConncetion();
 		String key = CacheKeyGenerator.getSetMembersKey(graphId, setId);
 		Boolean isMember;
-		if ("redis".equalsIgnoreCase(cacheType())) {
 			try {
 				isMember = jedis.sismember(key, member);
 				return isMember;
@@ -192,24 +128,6 @@ public class SetCacheManager {
 			} finally {
 				returnConnection(jedis);
 			}
-		} else {
-			isMember = false;
-			List<String> memberIds = (List<String>) setCache.get(key);
-			for (String id : memberIds) {
-				if (member.equals(id))
-					isMember = true;
-				else
-					isMember = false;
-			}
-		}
-		return isMember;
-	}
-
-	private static String cacheType() {
-		if (Platform.config.hasPath("cache.type")) 
-			return Platform.config.getString("cache.type");
-		else 
-			return "redis";
 	}
 	
 	private static void validateRequired(String graphId, String id, Object members, String errCode) {
