295666734a19c61536fb9b68fbc3c8c7658eeaa7
rashmi93 <rashminrocks93@gmail.com>
2017-09-18 15:43:58 +0530

    Issue #1160 feat : generate Lifecycle event on deletion of node

--- platform-jobs/samza/object-lifecycle/src/main/java/org/ekstep/jobs/samza/util/ConsumerWorkflowEnums.java
+++
@@ -1,7 +0,0 @@
-package org.ekstep.jobs.samza.util;
-
-public enum ConsumerWorkflowEnums {
-result, content, domain, transactionData, properties, status, nodeUniqueId, ItemSet, SET, objectType, nodeType, channel,
-ContentImage, Draft, Flagged, prevstate, FlagDraft, Live, state, Image, identifier, name, code, node, parentid, parenttype,
-subtype, Dimension, isParentOf, Concept, Domain, hasMember, type, contentType, mediaType, category, Plugin, Asset , Content;
-}
