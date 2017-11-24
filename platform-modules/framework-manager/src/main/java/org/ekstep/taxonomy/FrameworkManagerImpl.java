1c118493c26f13ed32e739969879236a7d5b7614
Pradyumna Nagendra <pradyumnan@ilimi.in>
2017-11-24 12:21:17 +0530

    Issue #000 feat : Created new module framework-manager under platform-modules

---
+++ platform-modules/framework-manager/src/main/java/org/ekstep/taxonomy/FrameworkManagerImpl.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.ekstep.taxonomy;
+
+/**
+ * @author pradyumna
+ * 
+ * 
+ *         Empty class created for further Implementation of Framework related
+ *         changes
+ *
+ */
+public class FrameworkManagerImpl {
+
+}
