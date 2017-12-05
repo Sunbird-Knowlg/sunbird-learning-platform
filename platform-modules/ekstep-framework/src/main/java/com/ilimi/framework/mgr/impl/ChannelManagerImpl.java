e904979cb80302fb310c92cb680144be0517f4a8
Rashmi <rashminrocks93@gmail.com>
2017-12-05 13:14:30 +0530

    Issue #1308 feat: category Instance API implemenation

--- platform-modules/ekstep-framework/src/main/java/com/ilimi/framework/mgr/impl/ChannelManagerImpl.java
+++ platform-modules/ekstep-framework/src/main/java/com/ilimi/framework/mgr/impl/ChannelManagerImpl.java
@@ -2,10 +2,13 @@ package com.ilimi.framework.mgr.impl;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import com.ilimi.common.dto.Response;
 import com.ilimi.common.exception.ResponseCode;
+import com.ilimi.common.exception.ServerException;
+import com.ilimi.framework.enums.CategoryEnum;
 import com.ilimi.framework.enums.ChannelEnum;
 import com.ilimi.framework.mgr.IChannelManager;
 
@@ -19,6 +22,9 @@ public class ChannelManagerImpl extends BaseFrameworkManager implements IChannel
 	public Response createChannel(Map<String, Object> request) {
 		if (null == request)
 			return ERROR("ERR_INVALID_CHANNEL_OBJECT", "Invalid Request", ResponseCode.CLIENT_ERROR);
+		if (null == request.get("code") || StringUtils.isBlank((String)request.get("code")))
+			return ERROR("ERR_CHANNEL_CODE_REQUIRED", "Unique code is mandatory for Channel", ResponseCode.CLIENT_ERROR);
+		request.put("identifier", (String)request.get("code"));
 		return create(request, CHANNEL_OBJECT_TYPE, null);
 	}
 
