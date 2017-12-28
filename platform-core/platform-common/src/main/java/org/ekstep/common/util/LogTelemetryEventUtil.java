2c38a0c40af0b303c3ac22144ffff3316572b9fc
Mahesh Kumar Gangula <mahesh@ilimi.in>
2017-12-28 07:51:42 +0530

    Issue #1336 feat: removed BE_CONTENT_LIFECYCLE event generation.

--- platform-core/platform-common/src/main/java/org/ekstep/common/util/LogTelemetryEventUtil.java
+++ platform-core/platform-common/src/main/java/org/ekstep/common/util/LogTelemetryEventUtil.java
@@ -50,43 +50,6 @@ public class LogTelemetryEventUtil {
 		}
 		return jsonMessage;
 	}
-	
-	public static String logContentLifecycleEvent(String contentId, Map<String, Object> metadata) {
-		TelemetryBEEvent te = new TelemetryBEEvent();
-		long unixTime = System.currentTimeMillis();
-		Map<String,Object> data = new HashMap<String,Object>();
-		te.setEid("BE_CONTENT_LIFECYCLE");
-		te.setVer("2.0");
-		te.setMid(mid);
-		te.setEts(unixTime);
-		te.setPdata("org.ekstep.content.platform", "", "1.0", "");
-		data.put("cid", contentId);
-		data.put("size", metadata.get("size"));
-		data.put("organization", metadata.get("organization"));
-		data.put("createdFor", metadata.get("createdFor"));
-		data.put("creator", metadata.get("creator"));
-		data.put("udpdater", metadata.get("udpdater"));
-		data.put("reviewer", metadata.get("reviewer"));
-		data.put("pkgVersion", metadata.get("pkgVersion"));
-		data.put("concepts", metadata.get("concepts"));
-		data.put("state", metadata.get("status"));
-		data.put("prevstate", metadata.get("prevState"));
-		data.put("downloadUrl", metadata.get("downloadUrl"));
-		data.put("contentType", metadata.get("contentType"));
-		data.put("mediaType", metadata.get("mediaType"));
-		data.put("flags",metadata.get("flags"));
-		te.setEdata(data);
-		
-		String jsonMessage = null;
-		try {
-//			jsonMessage = mapper.writeValueAsString(te);
-//			if (StringUtils.isNotBlank(jsonMessage))
-////				telemetryEventLogger.info(jsonMessage);
-		} catch (Exception e) {
-			PlatformLogger.log("Error logging BE_CONTENT_LIFECYCLE event", e.getMessage(), e);
-		}
-		return jsonMessage;
-	}
 
 	public static String logContentSearchEvent(String query, Object filters, Object sort, String correlationId, int size, Request req) {
 		TelemetryBEEvent te = new TelemetryBEEvent();
