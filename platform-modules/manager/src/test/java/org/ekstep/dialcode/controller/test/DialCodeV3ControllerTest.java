b4eceb498021f2bde87ff3506f8c03ea11e251b9
krgauraw <er.krgauraw@gmail.com>
2018-01-02 13:44:47 +0530

    Issue #1338 feat: Updated test cases and config

--- platform-modules/manager/src/test/java/org/ekstep/dialcode/controller/test/DialCodeV3ControllerTest.java
+++ platform-modules/manager/src/test/java/org/ekstep/dialcode/controller/test/DialCodeV3ControllerTest.java
@@ -1,5 +1,10 @@
 package org.ekstep.dialcode.controller.test;
 
+import java.util.Collection;
+import java.util.Map;
+
+import org.ekstep.common.dto.Response;
+import org.ekstep.dialcode.mgr.IDialCodeManager;
 import org.ekstep.dialcode.mgr.impl.DialCodeManagerImpl;
 import org.ekstep.dialcode.test.common.TestSetupUtil;
 import org.junit.AfterClass;
@@ -22,6 +27,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -40,29 +46,32 @@ public class DialCodeV3ControllerTest extends TestSetupUtil{
 
 	@Autowired
 	private WebApplicationContext context;
-
+	
 	MockMvc mockMvc;
 
 	private ResultActions actions;
 	private static final String basePath = "/v3/dialcode";
 	private static ObjectMapper mapper = new ObjectMapper();
 	private static DialCodeManagerImpl dialCodeMgr = new DialCodeManagerImpl();
+	
+	private static String dialCode;
 
-	private static String cassandraScript_1="CREATE KEYSPACE IF NOT EXISTS dialcode_store WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};";
-	private static String cassandraScript_2="CREATE TABLE IF NOT EXISTS dialcode_store.dial_code (identifier text,dialcode_index double,publisher text,channel text,batchCode text,metadata text,count int,status text,generated_on text,published_on text, primary key(identifier));";
+	private static String cassandraScript_1="CREATE KEYSPACE IF NOT EXISTS dialcode_store_test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};";
+	private static String cassandraScript_2="CREATE TABLE IF NOT EXISTS dialcode_store_test.dial_code_test (identifier text,dialcode_index double,publisher text,channel text,batchCode text,metadata text,count int,status text,generated_on text,published_on text, primary key(identifier));";
 	
 	
-	private static  String generateDialCodeReq_1 = "{\"request\": {\"dialcodes\": {\"count\":5,\"publisher\": \"testPublisher\",\"batchCode\":\"ka_math_std1\"}}}";
+	private static  String generateDialCodeReq_1 = "{\"request\": {\"dialcodes\": {\"count\":2,\"publisher\": \"testPublisher\",\"batchCode\":\"ka_math_std1\"}}}";
 	private static  String generateDialCodeReq_2 = "{\"request\": {\"dialcodes\": {\"count\":200,\"publisher\": \"testPub11\",\"batchCode\":\"ap_match_std2\"}}}";
 	
 	private static  String generateDialCodeReqInvalidPublisher = "{\"request\": {\"dialcodes\": {\"count\":2,\"publisher\": \"\",\"batchCode\":\"ap_match_std2\"}}}";
-	private static  String generateDialCodeReqInvalidCount = "{\"request\": {\"dialcodes\": {\"count\":\"\",\"publisher\": \"testPub1isher\",\"batchCode\":\"ka_math_std2\"}}}";
+	private static  String generateDialCodeReqInvalidCount = "{\"request\": {\"dialcodes\": {\"count\":\"\",\"publisher\": \"testPub1isherInvalidCount\",\"batchCode\":\"ka_math_std2\"}}}";
 
 	
 	@BeforeClass
 	public static void setup() throws Exception {
 		//loadDefinition("definitions/content_definition.json");
 		setupEmbeddedCassandra(cassandraScript_1,cassandraScript_2);
+		generateDIALCode();
 	}
 	
 	@AfterClass
@@ -73,6 +82,18 @@ public class DialCodeV3ControllerTest extends TestSetupUtil{
 	@Before
 	public void init() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+	
+	private static void generateDIALCode() throws Exception{
+		String dialCodeGenReq="{\"count\":1,\"publisher\": \"testPublisher\",\"batchCode\":\"test_math_std1\"}";
+		String channelId="channelTest";
+		Map<String, Object> requestMap = mapper.readValue(dialCodeGenReq,
+				new TypeReference<Map<String, Object>>() {});
+		Response resp=dialCodeMgr.generateDialCode(requestMap, channelId);
+		 Collection<String> obj= (Collection)resp.getResult().get("dialcodes");
+		 for (String s:obj) {
+			 dialCode=s;
+		}
 	}
 	
 	/*
@@ -88,7 +109,6 @@ public class DialCodeV3ControllerTest extends TestSetupUtil{
 		String path = basePath + "/generate";
 		actions = mockMvc.perform(MockMvcRequestBuilders.post(path).contentType(MediaType.APPLICATION_JSON).header("X-Channel-Id", "channelTest")
 				.content(generateDialCodeReq_1));
-		System.out.println("Response:::::::::"+actions.andReturn().getResponse().getContentAsString());
 		Assert.assertEquals(200, actions.andReturn().getResponse().getStatus());
 	}
 	
@@ -97,7 +117,7 @@ public class DialCodeV3ControllerTest extends TestSetupUtil{
 	 * 
 	 * Given: Invalid url and valid request body.
 	 * When: Generate DIAL Code API hits.
-	 * Then: 404 - 
+	 * Then: 404 - Invalid request path
 	 * 
 	 */
 	@Test
@@ -105,16 +125,15 @@ public class DialCodeV3ControllerTest extends TestSetupUtil{
 		String path = basePath + "/generat";
 		actions = mockMvc.perform(MockMvcRequestBuilders.post(path).contentType(MediaType.APPLICATION_JSON).header("X-Channel-Id", "channelTest")
 				.content(generateDialCodeReq_1));
-		System.out.println("Response:::::::::"+actions.andReturn().getResponse().getContentAsString());
 		Assert.assertEquals(404, actions.andReturn().getResponse().getStatus());
 	}
 	
 	/*
-	 * Scenario 3 : Generate Dial Code with valid url, valid request body, Invalid Publisher.
+	 * Scenario 3 : Generate Dial Code with valid url, valid request body, Invalid Publisher (No Publisher).
 	 * 
-	 * Given: Valid url,valid request body and Invalid Publisher.
+	 * Given: Valid url,valid request body and Invalid Publisher (No Publisher).
 	 * When: Generate DIAL Code API hits.
-	 * Then:  - Client Error
+	 * Then:  400 - Client Error . Error Message : "Publisher is Manadatory"
 	 * 
 	 */
 	@Test
@@ -122,7 +141,6 @@ public class DialCodeV3ControllerTest extends TestSetupUtil{
 		String path = basePath + "/generate";
 		actions = mockMvc.perform(MockMvcRequestBuilders.post(path).contentType(MediaType.APPLICATION_JSON).header("X-Channel-Id", "channelTest")
 				.content(generateDialCodeReqInvalidPublisher));
-		System.out.println("Response:::::::::"+actions.andReturn().getResponse().getContentAsString());
 		Assert.assertEquals(400, actions.andReturn().getResponse().getStatus());
 	}
 	
@@ -131,7 +149,7 @@ public class DialCodeV3ControllerTest extends TestSetupUtil{
 	 * 
 	 * Given: Valid url and valid request body.
 	 * When: Generate DIAL Code API hits.
-	 * Then:  - Client Error
+	 * Then:  400 - Client Error. Invalid Count
 	 * 
 	 */
 	@Test
@@ -139,7 +157,6 @@ public class DialCodeV3ControllerTest extends TestSetupUtil{
 		String path = basePath + "/generate";
 		actions = mockMvc.perform(MockMvcRequestBuilders.post(path).contentType(MediaType.APPLICATION_JSON).header("X-Channel-Id", "channelTest")
 				.content(generateDialCodeReqInvalidCount));
-		System.out.println("Response:::::::::"+actions.andReturn().getResponse().getContentAsString());
 		Assert.assertEquals(400, actions.andReturn().getResponse().getStatus());
 	}
 }
--- platform-modules/manager/src/test/java/org/ekstep/dialcode/test/common/TestSetupUtil.java
+++ platform-modules/manager/src/test/java/org/ekstep/dialcode/test/common/TestSetupUtil.java
@@ -1,9 +1,5 @@
 package org.ekstep.dialcode.test.common;
 
-import java.io.IOException;
-
-import org.apache.cassandra.exceptions.ConfigurationException;
-import org.apache.thrift.transport.TTransportException;
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -19,7 +15,7 @@ public class TestSetupUtil{
 
 	@AfterClass
 	public static void afterTest() throws Exception {
-		tearEmbeddedCassandraSetup();
+		//tearEmbeddedCassandraSetup();
 	}
 
 	@BeforeClass
@@ -30,33 +26,25 @@ public class TestSetupUtil{
 	
 	@SuppressWarnings("unused")
 	protected static void setupEmbeddedCassandra(String...querys ) {
-		
-		try {
-			 EmbeddedCassandraServerHelper.startEmbeddedCassandra("/cassandra-unit.yaml", 100000L);
-			 System.out.println("Embedded Cassandra Started..");
-			 
-			 System.out.println("Creating Schema..");
-			 //Session session =  CassandraConnector.getSession();
-			 
-			 Session session = EmbeddedCassandraServerHelper.getSession();
-			 
-			 for(String query:querys){
-				 System.out.println("query::::::::::::"+query);
-				 session.execute(query);
-				 System.out.println("Completed...");
-			 }
-			 System.out.println("Schema created..");
-			
+		 Session session =  CassandraConnector.getSession();
+		 for(String query:querys){
+			 session.execute(query);
+		 }
+		/*try {
+			 	EmbeddedCassandraServerHelper.startEmbeddedCassandra("/cassandra-unit.yaml", 100000L);
+				 Session session =  CassandraConnector.getSession();
+		 		for(String query:querys){
+			 		session.execute(query);
+		 		}
 		} catch (TTransportException | IOException | ConfigurationException e) {
 			System.out.println("Exception Occured while setting up Embedded Cassandra : "+e.getMessage());
 			e.printStackTrace();
-		}
+		}*/
 	}
 	
 	private static void tearEmbeddedCassandraSetup() {
 		EmbeddedCassandraServerHelper.getCluster().close();
 		EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
-		System.out.println("Cassandra Cleaned!!!!!!!!!!!!!!!!!");
 	}
 	
 }
