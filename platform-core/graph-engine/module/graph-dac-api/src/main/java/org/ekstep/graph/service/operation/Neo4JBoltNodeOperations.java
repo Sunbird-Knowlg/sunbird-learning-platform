378d5c20c6159ef70f50b7b4a3092b6178f05dc1
Pradyumna Nagendra <pradyumnan@ilimi.in>
2018-01-03 14:33:52 +0530

    Issue #1280 fix: Resoled response issue for already existing node

--- platform-core/graph-engine/module/graph-dac-api/src/main/java/org/ekstep/graph/service/operation/Neo4JBoltNodeOperations.java
+++ platform-core/graph-engine/module/graph-dac-api/src/main/java/org/ekstep/graph/service/operation/Neo4JBoltNodeOperations.java
@@ -284,6 +284,8 @@ public class Neo4JBoltNodeOperations {
 				PlatformLogger.log("Error! While writing data to Neo4J Node.", null, e);
 				throw new ServerException(DACErrorCodeConstants.CONNECTION_PROBLEM.name(),
 						DACErrorMessageConstants.CONNECTION_PROBLEM + " | " + e.getMessage());
+			} else {
+				throw e;
 			}
 		}
 		return node;
