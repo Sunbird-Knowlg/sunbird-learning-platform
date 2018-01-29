96b1fa950c0acab03bebfe3a3858f83d310e2ff7
Mahesh Kumar Gangula <mahesh@ilimi.in>
2018-01-29 12:19:21 +0530

    Issue #000 fix: review changes for content linking with framework terms.

--- platform-core/graph-engine/module/graph-core/src/main/java/org/ekstep/graph/model/cache/CategoryCache.java
+++ platform-core/graph-engine/module/graph-core/src/main/java/org/ekstep/graph/model/cache/CategoryCache.java
@@ -1,11 +1,11 @@
 package org.ekstep.graph.model.cache;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
+import org.ekstep.graph.cache.util.RedisStoreUtil;
 import org.ekstep.telemetry.logger.TelemetryManager;
 
 /**
@@ -16,11 +16,9 @@ import org.ekstep.telemetry.logger.TelemetryManager;
 
 public class CategoryCache {
 
-	private static Map<String, List<Object>> categories = new HashMap<>();
-
 	public static List<Object> getTerms(String framework, String category) {
 		String key = getKey(framework, category);
-		return categories.get(key);
+		return RedisStoreUtil.getList(key);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -36,7 +34,7 @@ public class CategoryCache {
 	}
 	
 	private static String getKey(String framework, String category) {
-		return framework + category;
+		return "cat_" + framework + category;
 	}
 
 	private static void setFramework(String framework, List<Map<String, Object>> categories) {
@@ -46,12 +44,7 @@ public class CategoryCache {
 				List<Object> terms = getTerms(category, "terms");
 				if (!terms.isEmpty()) {
 					String key = getKey(framework, catName);
-					CategoryCache.categories.put(key, terms);
-					Map<String, Object> params = new HashMap<>();
-					params.put("framework", framework);
-					params.put("category", catName);
-					params.put("terms", terms);
-					TelemetryManager.info("Update - category cache.", params);
+					RedisStoreUtil.saveList(key, terms);
 				}
 			}
 		}
