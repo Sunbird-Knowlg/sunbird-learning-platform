4f85dd2572bcbff263cb9e1fe0a691108b9cf5b5
Mahesh Kumar Gangula <mahesh@ilimi.in>
2018-01-30 00:21:02 +0530

    Issue #000 fix: review changes for content linking with framework terms.

--- platform-modules/ekstep-framework/src/main/java/org/ekstep/framework/mgr/impl/ChannelManagerImpl.java
+++ platform-modules/ekstep-framework/src/main/java/org/ekstep/framework/mgr/impl/ChannelManagerImpl.java
@@ -44,13 +44,21 @@ public class ChannelManagerImpl extends BaseFrameworkManager implements IChannel
 				for (Object entry : frameworks) {
 					Map<String, Object> framework = (Map<String, Object>) entry;
 					String id = (String) framework.get("identifier");
-					Map<String, Object> hierarchy = getHierarchy(id, 0, false);
-					TelemetryManager.info("Loading framework to category cache with framework id: "+ id);
-					CategoryCache.setFramework(id, hierarchy);
+					loadFrameworkCategoryCache(id);
 				}
 			}
 		} catch (Exception e) {
-			TelemetryManager.error("Error while loading all frameworks for category cache.", e);
+			TelemetryManager.error("Error while fetching all frameworks for category cache.", e);
+		}
+	}
+	
+	private void loadFrameworkCategoryCache(String id) {
+		try {
+			Map<String, Object> hierarchy = getHierarchy(id, 0, false);
+			CategoryCache.setFramework(id, hierarchy);
+			TelemetryManager.info("Loaded framework to category cache with framework id: "+ id);
+		} catch (Exception e) {
+			TelemetryManager.error("Loading framework to category cache failed for framework id: "+ id, e);
 		}
 	}
 	
