1269022f25de7316e5bab932f2d2affd400b6786
Mahesh Kumar Gangula <mahesh@ilimi.in>
2018-02-19 22:40:47 +0530

    Issue ekstep/Learning-Platform#1417 feat: form apis.

--- platform-modules/manager/src/main/java/org/ekstep/learning/controller/FormController.java
+++ platform-modules/manager/src/main/java/org/ekstep/learning/controller/FormController.java
@@ -2,6 +2,7 @@ package org.ekstep.learning.controller;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.ekstep.common.controller.BaseController;
 import org.ekstep.common.dto.Request;
 import org.ekstep.common.dto.Response;
@@ -24,14 +25,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/v3/form")
-public class FormController  extends BaseController {
+public class FormController extends BaseController {
 
 	@Autowired
 	private IFormManager formManager;
-	
+
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<Response> create(@RequestBody Map<String, Object> requestMap) {
+	public ResponseEntity<Response> create(@RequestHeader(value = "X-Channel-ID", required = true) String channel,
+			@RequestBody Map<String, Object> requestMap) {
 		String apiId = "ekstep.learning.form.create";
 		Request request = getRequest(requestMap);
 		try {
@@ -45,7 +47,7 @@ public class FormController  extends BaseController {
 	}
 
 	@SuppressWarnings("unchecked")
-	@RequestMapping(value = "/update/{id:.+}", method = RequestMethod.PATCH)
+	@RequestMapping(value = "/update/{type:.+}", method = RequestMethod.PATCH)
 	@ResponseBody
 	public ResponseEntity<Response> update(@PathVariable(value = "type") String type,
 			@RequestParam(value = "operation", required = true) String operation,
@@ -54,8 +56,9 @@ public class FormController  extends BaseController {
 		String apiId = "ekstep.learning.form.update";
 		Request request = getRequest(requestMap);
 		try {
-			String identifier = formManager.getIdentifier(type, operation, channel); 
+			String identifier = formManager.getIdentifier(type, operation, channel);
 			Map<String, Object> map = (Map<String, Object>) request.get(formManager.getObjectType());
+			
 			Response response = formManager.update(identifier, map);
 			return getResponseEntity(response, apiId, null);
 		} catch (Exception e) {
@@ -63,7 +66,7 @@ public class FormController  extends BaseController {
 			return getExceptionResponseEntity(e, apiId, null);
 		}
 	}
-	
+
 	@RequestMapping(value = "/read/{type:.+}", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<Response> read(@PathVariable(value = "type") String type,
@@ -72,11 +75,27 @@ public class FormController  extends BaseController {
 		String apiId = "ekstep.learning.form.info";
 		Response response;
 		try {
-			// TODO: 
 			String identifier = formManager.getIdentifier(type, operation, channel);
 			response = formManager.read(identifier, type, operation);
 			return getResponseEntity(response, apiId, null);
 		} catch (Exception e) {
+			return getExceptionResponseEntity(e, apiId, null);
+		}
+	}
+
+	@RequestMapping(value = "/retire/{type:.+}", method = RequestMethod.DELETE)
+	@ResponseBody
+	public ResponseEntity<Response> retire(@PathVariable(value = "type") String type,
+			@RequestParam(value = "operation", required = true) String operation,
+			@RequestHeader(value = "X-Channel-ID", required = true) String channel) {
+		String apiId = "ekstep.learning.form.retire";
+		Response response;
+		try {
+			String identifier = formManager.getIdentifier(type, operation, channel);
+			response = formManager.read(identifier, type, operation);
+			return getResponseEntity(response, apiId, null);
+		} catch (Exception e) {
+			TelemetryManager.error("retire category | Exception: " + e.getMessage(), e);
 			return getExceptionResponseEntity(e, apiId, null);
 		}
 	}
