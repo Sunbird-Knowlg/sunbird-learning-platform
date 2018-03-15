df12663f9974ecccca2cf35090091031ab7f939f
amitpriyadarshi <darshi.1983@gmail.com>
2018-03-15 11:59:02 +0530

    Issue #1462 feat: Neo4j to Elastic Search sync - Creating project structure

---
+++ platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/shell/SyncCommand.java
@@ -0,0 +1,17 @@
+package org.ekstep.sync.tool.shell;
+
+import org.springframework.shell.core.CommandMarker;
+import org.springframework.shell.core.annotation.CliCommand;
+import org.springframework.shell.core.annotation.CliOption;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SyncCommand implements CommandMarker{
+
+	@CliCommand(value = "syncbyid", help = "Sync data from Neo4j to Elastic Search by Id")
+	public void syncById(@CliOption(key = { "id" }, mandatory = true, help = "Unique Id of node object") final String id,
+			@CliOption(key = { "type" }, mandatory = true, help = "Object type of node object") final String type) {
+		System.out.println("Id: " + id);
+		System.out.println("Type: " + type);
+	}
+}
