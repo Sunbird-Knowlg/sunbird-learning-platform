e415109eb82989879f6c01a24ac2748e8f5d5a40
krgauraw <er.krgauraw@gmail.com>
2018-03-27 16:29:12 +0530

    Issue #1487 feat: Reverting code for telemetry audit of Publisher

--- platform-modules/manager/src/main/java/org/ekstep/dialcode/store/PublisherStore.java
+++ platform-modules/manager/src/main/java/org/ekstep/dialcode/store/PublisherStore.java
@@ -5,9 +5,11 @@ package org.ekstep.dialcode.store;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.ekstep.cassandra.store.CassandraStore;
 import org.ekstep.common.Platform;
+import org.ekstep.telemetry.logger.TelemetryManager;
 import org.springframework.stereotype.Component;
 
 import com.datastax.driver.core.Row;
@@ -30,6 +32,9 @@ public class PublisherStore extends CassandraStore {
 
 	public void create(String id, Map<String, Object> props) {
 		insert(id, props);
+		List<String> keys = props.keySet().stream().collect(Collectors.toList());
+		keys.add("identifier");
+		TelemetryManager.audit(id, getObjectType(), keys, null, null);
 	}
 	
 	public List<Row> get(String id, Object value) {
@@ -38,5 +43,7 @@ public class PublisherStore extends CassandraStore {
 	
 	public void modify(String key, String idValue, Map<String, Object> props) {
 		update(key, idValue, props);
+		List<String> keys = props.keySet().stream().collect(Collectors.toList());
+		TelemetryManager.audit(idValue, getObjectType(), keys, null, null);
 	}
 }
