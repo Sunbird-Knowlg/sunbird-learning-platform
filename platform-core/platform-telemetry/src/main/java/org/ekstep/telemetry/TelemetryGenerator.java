9120f70b58780d9e472752e1495e80a244d40ddc
Pradyumna Nagendra <pradyumnan@ilimi.in>
2018-04-06 00:20:26 +0530

    Issue #1496 fix: Fixed telemetry audit log context.pdata.id incorrect value

--- platform-core/platform-telemetry/src/main/java/org/ekstep/telemetry/TelemetryGenerator.java
+++ platform-core/platform-telemetry/src/main/java/org/ekstep/telemetry/TelemetryGenerator.java
@@ -25,7 +25,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class TelemetryGenerator {
 
 	private static ObjectMapper mapper = new ObjectMapper();
-	private static String environment = Platform.config.getString("environment");
+	private static String environment = Platform.config.getString("env");
 	private static Producer producer = new Producer(environment + ".ekstep.learning.platform", "1.0");
 
 	
