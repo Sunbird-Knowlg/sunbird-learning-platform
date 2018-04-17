bcfc1e71e45914795861238065754d1e83b8dad9
Karthik Kumarasamy <kkarthik4cs@gmail.com>
2018-04-17 20:12:14 +0530

    Issue #1462 feat: sync-tool Testcases update

--- platform-tools/spikes/sync-tool/src/test/java/org/ekstep/sync/tool/shell/SyncCommandTest.java
+++ platform-tools/spikes/sync-tool/src/test/java/org/ekstep/sync/tool/shell/SyncCommandTest.java
@@ -1,8 +1,17 @@
 package org.ekstep.sync.tool.shell;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.ekstep.common.dto.Request;
 import org.ekstep.common.dto.Response;
 import org.ekstep.graph.common.enums.GraphEngineParams;
@@ -69,23 +78,69 @@ public class SyncCommandTest extends SpringShellTest{
 	}
 
 	@Test
-	public void testNodeById() {
+	public void testNodeById() throws Exception {
 		CommandResult cr = new CommandResult(false);
 		//Execute command
 		cr = getShell().executeCommand("syncbyids --ids do_112178562079178752118");
 		Assert.assertNull(cr.getException());
 		Assert.assertTrue( cr.isSuccess() );
+		String doc = elasticSearchUtil.getDocumentAsStringById(CompositeSearchConstants.COMPOSITE_SEARCH_INDEX,
+				CompositeSearchConstants.COMPOSITE_SEARCH_INDEX_TYPE, "do_112178562079178752118");
+		assertTrue(StringUtils.contains(doc, "do_112178562079178752118"));
 		//Execute command
 		cr = getShell().executeCommand("syncbyids --ids domain_48763,domain_14658,do_112178562079178752118");
 		Assert.assertNull(cr.getException());
 		Assert.assertTrue( cr.isSuccess() );
+		List<String> resultDocs = elasticSearchUtil.getMultiDocumentAsStringByIdList(
+				CompositeSearchConstants.COMPOSITE_SEARCH_INDEX, CompositeSearchConstants.COMPOSITE_SEARCH_INDEX_TYPE,
+				Arrays.asList("domain_48763","domain_14658","do_112178562079178752118"));
+
+		assertNotNull(resultDocs);
+		assertEquals(3, resultDocs.size());
 	}
 	
 	@Test
-	public void testNodeByObjectType() {
+	public void testNodeByObjectType() throws Exception {
 		CommandResult cr = new CommandResult(false);
 		//Execute command
 		cr = getShell().executeCommand("syncbyobjecttype --objectType AssessmentItem");
+		Assert.assertNull(cr.getException());
+		Assert.assertTrue( cr.isSuccess() );
+		List<String> ids = Arrays.asList("ek.n.q_QAFTB_88","do_10096483","do_112178562079178752118","test.ftb_010","do_11225380345346457614","do_11225380550993510416","do_11225380647183155217","do_11225381096194867211","do_11225562396351692815","do_112255733460467712120","do_112255733481611264121","do_112257049614336000119","do_112257049883033600120","do_112257050142777344121","do_112257050369056768122","do_112257683409166336123","do_112257683430334464124","do_112257852917121024153","do_112258573914611712145","do_112260519610449920119");
+		List<String> resultDocs = elasticSearchUtil.getMultiDocumentAsStringByIdList(
+				CompositeSearchConstants.COMPOSITE_SEARCH_INDEX, CompositeSearchConstants.COMPOSITE_SEARCH_INDEX_TYPE,
+				ids);
+
+		assertNotNull(resultDocs);
+		assertEquals(20, resultDocs.size());
+
+	}
+	
+	@Test
+	public void testNodeByDateRangeIncorrectFormat() throws Exception {
+		CommandResult cr = new CommandResult(false);
+
+		//Execute command
+		PrintStream err = System.err;
+		System.setErr(null);	
+		try {
+			cr = getShell().executeCommand("syncbydaterange --objectType Content --startDate 2017-05-20 --endDate 2017-05-31");
+		}catch (Exception e) {
+			Assert.assertEquals("ERR_DATE_RANGE_INCORRECT: End Date should be more than or equal to Start Date", cr.getException());			
+		}
+		Assert.assertFalse( cr.isSuccess() );
+		System.setErr(err);
+
+	}
+	
+	@Test
+	public void testNodeByDateRange() throws Exception {
+		CommandResult cr = new CommandResult(false);
+		//Execute command
+		Date date = new Date();
+		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd");
+		String today = inputFormat.format(date);
+		cr = getShell().executeCommand("syncbydaterange --objectType Content --startDate "+today+" --endDate "+today);
 		Assert.assertNull(cr.getException());
 		Assert.assertTrue( cr.isSuccess() );
 	}
