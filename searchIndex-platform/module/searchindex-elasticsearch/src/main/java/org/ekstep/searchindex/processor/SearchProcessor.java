c2d013810c18a6c7a2f3339d6c2ee7b81a9d87d3
Pradyumna Nagendra <pradyumnan@ilimi.in>
2018-04-28 00:23:57 +0530

    Issue #1503 feat: ElasticSearch Upgrade from 2.x to 6.x version changes

--- searchIndex-platform/module/searchindex-elasticsearch/src/main/java/org/ekstep/searchindex/processor/SearchProcessor.java
+++ searchIndex-platform/module/searchindex-elasticsearch/src/main/java/org/ekstep/searchindex/processor/SearchProcessor.java
@@ -378,6 +378,7 @@ public class SearchProcessor {
 		if (null != softConstraints && !softConstraints.isEmpty()) {
 			boolQuery.should(getSoftConstraintQuery(softConstraints));
 			searchDTO.setSortBy(null);
+			relevanceSort = true;
 		}
 		return boolQuery;
 	}
