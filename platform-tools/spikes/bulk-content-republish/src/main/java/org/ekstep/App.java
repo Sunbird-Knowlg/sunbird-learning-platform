8363b6307b65f907cc8170787effa7bde39bf636
Karthik Kumarasamy <kkarthik4cs@gmail.com>
2018-05-12 10:45:56 +0530

    Issue #1553 feat: spike for bulk-content-republish job

--- platform-tools/spikes/bulk-content-republish/src/main/java/org/ekstep/App.java
+++ platform-tools/spikes/bulk-content-republish/src/main/java/org/ekstep/App.java
@@ -1,59 +1,120 @@
 package org.ekstep;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.ekstep.common.Platform;
 import org.ekstep.graph.dac.model.Node;
+import org.ekstep.graph.dac.model.SearchCriteria;
 import org.ekstep.graph.engine.router.ActorBootstrap;
-import org.ekstep.GraphUtil;
-import org.ekstep.InstructionEventFeeder;
 
 public class App {
 
-	private static final int batchSize = 500;
 	private static GraphUtil util = new GraphUtil();
 	private static InstructionEventFeeder feeder = new InstructionEventFeeder();
 	private static int totalSize = 0;
-	
-	public static void main(String[] args) {
-		if(Platform.config.hasPath("total.size")) {
-			totalSize = Platform.config.getInt("total.size");
+	private final static int defaultBatchSize = 500;
+	public static SearchCriteria sc = null;
+
+	public static void main(String[] args) throws Exception {
+
+		int startPostition = 0;
+		int resultSize = 0;
+
+		if (args.length>0&&StringUtils.isNotBlank(args[0]))
+			try {
+				startPostition = Integer.parseInt(args[0]);
+			} catch (Exception e) {
+				new Exception("args[0] should be startPoisitoin and it should be Integer value");
+			}
+
+		if (args.length>1&&StringUtils.isNotBlank(args[1]))
+			try {
+				resultSize = Integer.parseInt(args[1]);
+			} catch (Exception e) {
+				new Exception("args[1] should be resultSize and it should be Integer value");
+			}
+
+		if (Platform.config.hasPath("search_criteria")) {
+			Map<String, Object> search_criteria = (Map<String, Object>) Platform.config.getAnyRef("search_criteria");
+			ObjectMapper mapper = new ObjectMapper();
+			sc = mapper.convertValue(search_criteria, SearchCriteria.class);
+
+			if (startPostition != 0)
+				sc.setStartPosition(startPostition);
+
+			if (resultSize != 0)
+				sc.setResultSize(resultSize);
+
+			System.out.println("Search Criteria="+mapper.convertValue(sc, Map.class));
+
+		} else {
+			throw new Exception("missing search_criteria in application.conf");
 		}
 		process();
 		ActorBootstrap.getActorSystem().shutdown();
-		
 	}
-	
+
 	private static void process() {
 		boolean found = true;
-		int start = 0;
+		List<String> processed_ids = new ArrayList<String>();
+		int start = sc.getStartPosition();
 		int count = 0;
+		//check and change result size to set it to minimum batches
+		if (sc.getResultSize() == 0)
+			sc.setResultSize(defaultBatchSize);
+		else {
+			totalSize = sc.getResultSize();
+			if(sc.getResultSize()>defaultBatchSize)
+				sc.setResultSize(defaultBatchSize);
+		}
+
 		while (found) {
-			List<Node> nodes = util.getLiveContentNodes( start, batchSize);
+			if (totalSize != 0 && totalSize == count) {
+				System.out.println("Skipping remaining as it reaches result size(" + totalSize + ") to process");
+				found = false;
+				break;
+			}
+			List<Node> nodes = null;
+			try {
+				if (start != 0 && sc.getStartPosition() != start)
+					sc.setStartPosition(start);
+				nodes = util.getLiveContentNodes(sc);
+			} catch (Exception e) {
+				System.out.println("error while fetching neo4j records for Live content offset=" + start + ",batchSize="
+						+ sc.getResultSize());
+				start += sc.getResultSize();
+				continue;
+			}
+
 			if (null != nodes && !nodes.isEmpty()) {
-				System.out.println(batchSize + " objects are getting synced");
-				start += batchSize;
-				for(Node node:nodes) {
+				System.out.println(nodes.size() + " objects are getting republished, offset=" + sc.getStartPosition());
+				start += sc.getResultSize();
+				for (Node node : nodes) {
+					count++;
+					
 					try {
 						String imgNodeId = node.getIdentifier() + ".img";
 						Node imgNode = util.getNode("domain", imgNodeId);
-						if(imgNode==null) {
-							System.out.println("pushing publish message for id="+node.getIdentifier());
-							feeder.push(node, "Public");							
-							count++;
+						if (imgNode == null) {
+							processed_ids.add(node.getIdentifier());
+							feeder.push(node, "Public");
 						}
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
-					if(totalSize!=0&&totalSize==count) {
-						found = false;
-						break;						
-					}
+
 				}
 			} else {
 				found = false;
 				break;
 			}
 		}
+		
+		System.out.println(processed_ids.size()+" content events got pushed for republishing");
+		System.out.println("processed_ids="+processed_ids);
 	}
 }
--- platform-tools/spikes/bulk-content-republish/src/main/java/org/ekstep/GraphUtil.java
+++ platform-tools/spikes/bulk-content-republish/src/main/java/org/ekstep/GraphUtil.java
@@ -1,33 +1,20 @@
 package org.ekstep;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.ekstep.common.dto.Request;
 import org.ekstep.common.dto.Response;
 import org.ekstep.common.exception.ResourceNotFoundException;
 import org.ekstep.graph.dac.enums.GraphDACParams;
-import org.ekstep.graph.dac.model.Filter;
-import org.ekstep.graph.dac.model.MetadataCriterion;
 import org.ekstep.graph.dac.model.Node;
-import org.ekstep.graph.dac.model.SearchConditions;
 import org.ekstep.graph.dac.model.SearchCriteria;
 import org.ekstep.graph.engine.router.GraphEngineManagers;
 import org.ekstep.learning.util.ControllerUtil;
 
+
 public class GraphUtil extends ControllerUtil{
 
-	public List<Node> getLiveContentNodes(int startPosition, int batchSize) {
-		SearchCriteria sc = new SearchCriteria();
-		sc.setObjectType("Content");
-		Filter statusFilter = new Filter(GraphDACParams.status.name(), SearchConditions.OP_IN, Arrays.asList("Live","Unlisted"));
-		Filter contentTypeFilter = new Filter("resourceType", SearchConditions.OP_IN, Arrays.asList("Lesson Plan","Book","Worksheet","Collection","Study material","Course"));
-		//Filter contentTypeFilter = new Filter("contentType", SearchConditions.OP_IN, Arrays.asList("Resource","Collection","TextBook","Course","LessonPlan"));
-		//Filter contentTypeFilter = new Filter("contentType", SearchConditions.OP_IN, Arrays.asList("Asset","ContentTemplate","ItemTemplate","Template","Plugin","TextBookUnit","LessonPlanUnit"));
-		MetadataCriterion mc = MetadataCriterion.create(Arrays.asList(statusFilter, contentTypeFilter));
-		sc.addMetadata(mc);
-		sc.setResultSize(batchSize);
-		sc.setStartPosition(startPosition);
+	public List<Node> getLiveContentNodes(SearchCriteria sc) {
 		Request req = getRequest("domain", GraphEngineManagers.SEARCH_MANAGER, "searchNodes",
 				GraphDACParams.search_criteria.name(), sc);
 		req.put(GraphDACParams.get_tags.name(), true);
@@ -39,4 +26,5 @@ public class GraphUtil extends ControllerUtil{
 			return nodes;
 		}
 	}
+	
 }
