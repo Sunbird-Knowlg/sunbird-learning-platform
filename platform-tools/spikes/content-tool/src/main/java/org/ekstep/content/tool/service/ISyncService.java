4d3ca9ff2d8aae18849749dc8dd72938abaa737b
Pradyumna Nagendra <pradyumnan@ilimi.in>
2018-09-25 12:41:41 +0530

    Issue #SB-6944 feat: Content tool | Sync implementation - added force update implementation

--- platform-tools/spikes/content-tool/src/main/java/org/ekstep/content/tool/service/ISyncService.java
+++ platform-tools/spikes/content-tool/src/main/java/org/ekstep/content/tool/service/ISyncService.java
@@ -5,7 +5,7 @@ public interface ISyncService {
     void dryRun();
 
 
-    void ownerMigration(String createdBy, String channel, String[] createdFor, String[] organisation, String creator, String filter, String dryRun);
+    void ownerMigration(String createdBy, String channel, String[] createdFor, String[] organisation, String creator, String filter, String dryRun, String forceUpdate);
 
-    void sync(String filter, String dryRun);
+    void sync(String filter, String dryRun, String forceUpdate);
 }
