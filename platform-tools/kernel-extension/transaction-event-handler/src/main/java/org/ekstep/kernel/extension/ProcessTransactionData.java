7e6e84e266ef04e57df9c3549a23ee0746ad699b
Shubham Varshney <>
2018-10-16 16:15:14 +0530

    Issue #SB-8141: feat: Added missed import.

--- platform-tools/kernel-extension/transaction-event-handler/src/main/java/org/ekstep/kernel/extension/ProcessTransactionData.java
+++ platform-tools/kernel-extension/transaction-event-handler/src/main/java/org/ekstep/kernel/extension/ProcessTransactionData.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 import org.ekstep.common.dto.ExecutionContext;
