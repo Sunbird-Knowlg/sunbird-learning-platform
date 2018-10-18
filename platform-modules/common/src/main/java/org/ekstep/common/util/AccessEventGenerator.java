ce385dbe6277154ad68445dbb7911fc23c7ca106
Pradyumna Nagendra <pradyumnan@ilimi.in>
2018-10-18 18:40:24 +0530

    Issue #SB-8438 fix: Ignored external properties part of request in access event

--- platform-modules/common/src/main/java/org/ekstep/common/util/AccessEventGenerator.java
+++ platform-modules/common/src/main/java/org/ekstep/common/util/AccessEventGenerator.java
@@ -1,9 +1,13 @@
 package org.ekstep.common.util;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.ekstep.common.Platform;
 import org.ekstep.common.dto.Request;
 import org.ekstep.common.dto.Response;
 import org.ekstep.common.util.RequestWrapper;
@@ -21,6 +25,12 @@ public class AccessEventGenerator extends TelemetryAccessEventUtil {
 			String body = requestWrapper.getBody();
 			if (body.contains("request") && body.length() > 0 && !body.contains("SCRIPT")) {
 				request = mapper.readValue(body, Request.class);
+				if(null != request.getRequest().get("content")) {
+					Map<String, Object> content = (Map<String, Object>) request.getRequest().get("content");
+					List<String> ignoredProperties = Platform.config.hasPath("learning.telemetry_ignored_props")?Platform.config.getStringList("learning.telemetry_ignored_props"): Arrays.asList("body","screenshots","stageIcons","editorState");
+					content.keySet().removeAll(ignoredProperties);
+					request.getRequest().put("content",content);
+				}
 			} else if (null != body) {
 				int index = body.indexOf("filename=");
 				String fileName = "";
