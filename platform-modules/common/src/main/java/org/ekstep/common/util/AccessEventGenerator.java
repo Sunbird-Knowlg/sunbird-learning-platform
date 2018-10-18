7c83e81728895ebad85862b79da448c4ce3a4863
Pradyumna Nagendra <pradyumnan@ilimi.in>
2018-10-18 23:14:59 +0530

    Issue #SB-8438 fix: Trimmed request string in api access to 100 chars

--- platform-modules/common/src/main/java/org/ekstep/common/util/AccessEventGenerator.java
+++ platform-modules/common/src/main/java/org/ekstep/common/util/AccessEventGenerator.java
@@ -1,18 +1,12 @@
 package org.ekstep.common.util;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.ekstep.common.Platform;
 import org.ekstep.common.dto.Request;
 import org.ekstep.common.dto.Response;
-import org.ekstep.common.util.RequestWrapper;
-import org.ekstep.common.util.ResponseWrapper;
 import org.ekstep.telemetry.util.TelemetryAccessEventUtil;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AccessEventGenerator extends TelemetryAccessEventUtil {
 	
@@ -25,12 +19,6 @@ public class AccessEventGenerator extends TelemetryAccessEventUtil {
 			String body = requestWrapper.getBody();
 			if (body.contains("request") && body.length() > 0 && !body.contains("SCRIPT")) {
 				request = mapper.readValue(body, Request.class);
-				if(null != request.getRequest().get("content")) {
-					Map<String, Object> content = (Map<String, Object>) request.getRequest().get("content");
-					List<String> ignoredProperties = Platform.config.hasPath("learning.telemetry_ignored_props")?Platform.config.getStringList("learning.telemetry_ignored_props"): Arrays.asList("body","screenshots","stageIcons","editorState");
-					content.keySet().removeAll(ignoredProperties);
-					request.getRequest().put("content",content);
-				}
 			} else if (null != body) {
 				int index = body.indexOf("filename=");
 				String fileName = "";
