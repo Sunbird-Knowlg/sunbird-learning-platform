001a3d4062463c54a8c00c0c15ad69cd89828d4a
Shubham Varshney <>
2018-10-24 18:37:38 +0530

    Issue #SB-8464: feat: License Validation API for Youtube

---
+++ platform-modules/manager/src/main/java/org/ekstep/taxonomy/mgr/impl/AssetManagerImpl.java
@@ -0,0 +1,90 @@
+package org.ekstep.taxonomy.mgr.impl;
+
+import org.apache.commons.lang3.StringUtils;
+import org.ekstep.common.Platform;
+import org.ekstep.common.dto.Response;
+import org.ekstep.common.exception.ClientException;
+import org.ekstep.common.exception.ResponseCode;
+import org.ekstep.taxonomy.enums.AssetParams;
+import org.ekstep.taxonomy.mgr.IAssetManager;
+import org.ekstep.taxonomy.util.YouTubeDataAPIV3Service;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The Class <code>AssetManagerImpl</code> is the implementation of
+ *  <code>IAssetManager</code> for all asset related operations.
+ *
+ * @see IAssetManager
+ */
+@Service
+public class AssetManagerImpl implements IAssetManager {
+
+    private List<String> validLicenses;
+
+    @PostConstruct
+    public void init() {
+        validLicenses = Platform.config.getStringList("learning.valid-license");
+    }
+
+    private String getProvider(Map<String, Object> asset) throws Exception {
+        String provider = (String) asset.get(AssetParams.provider.name());
+        if(null == provider || StringUtils.isBlank(provider))
+            throw new ClientException(ResponseCode.CLIENT_ERROR.name(), "Provider is blank.");
+        return provider;
+    }
+
+    private String getUrl(Map<String, Object> asset) throws Exception {
+        String url = (String) asset.get(AssetParams.url.name());
+        if(null == url || StringUtils.isBlank(url))
+            throw new ClientException(ResponseCode.CLIENT_ERROR.name(), "Invalid Url.");
+        return url;
+    }
+
+    private String getLicenseType(Map<String, Object> asset) throws Exception {
+        String licenseType;
+        switch (StringUtils.lowerCase(getProvider(asset))) {
+            case "youtube": licenseType = YouTubeDataAPIV3Service.getLicense(getUrl(asset));
+                break;
+            default       : throw new ClientException(ResponseCode.CLIENT_ERROR.name(), "Invalid Provider");
+        }
+        return licenseType;
+    }
+
+    /**
+     * Validate License
+     *
+     * @param asset
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public Response licenseValidate(Map<String, Object> asset) throws Exception {
+        String licenseType = getLicenseType(asset);
+        boolean validLicense = validLicenses.contains(licenseType);
+        Response response = new Response();
+        response.getResult().put(AssetParams.validLicense.name(), validLicense);
+        return response;
+    }
+
+    /**
+     * Read Url Metadata.
+     *
+     * @param asset
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public Response metadataRead(Map<String, Object> asset) throws Exception {
+        String licenseType = getLicenseType(asset);
+        Map<String, Object> metadata = new HashMap<>();
+        metadata.put("license", licenseType);
+        Response response = new Response();
+        response.getResult().put("metadata", metadata);
+        return response;
+    }
+}
