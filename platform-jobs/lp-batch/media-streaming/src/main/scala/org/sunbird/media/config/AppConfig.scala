392e7460d65ed3e3d813ac303db2156d4311f9c3
Kumar Gauraw <gaurawk@ilimi.in>
2018-11-08 18:29:34 +0530

    Issue #SB-8781 feat: Video Streaming configuration changes

--- platform-jobs/lp-batch/media-streaming/src/main/scala/org/sunbird/media/config/AppConfig.scala
+++ platform-jobs/lp-batch/media-streaming/src/main/scala/org/sunbird/media/config/AppConfig.scala
@@ -9,14 +9,21 @@ import org.sunbird.media.exception.MediaServiceException
   */
 object AppConfig {
 
-  lazy val defaultConfig = ConfigFactory.load();
-  lazy val envConfig = ConfigFactory.systemEnvironment();
-  lazy val config = envConfig.withFallback(defaultConfig);
+  val defaultConfig = ConfigFactory.load();
+  val envConfig = ConfigFactory.systemEnvironment();
+  val config = envConfig.withFallback(defaultConfig);
 
   def getConfig(key: String): String = {
     if (config.hasPath(key))
       config.getString(key);
     else throw new MediaServiceException("CONFIG_NOT_FOUND", "Configuration for key [" + key + "] Not Found.")
+  }
+
+  def getSystemConfig(key: String): String = {
+    val sysKey=key.replaceAll("\\.","_")
+    if (config.hasPath(sysKey))
+      config.getString(sysKey);
+    else throw new MediaServiceException("CONFIG_NOT_FOUND", "Configuration for key [" + sysKey + "] Not Found.")
   }
 
   def getConfig(): Config = {
