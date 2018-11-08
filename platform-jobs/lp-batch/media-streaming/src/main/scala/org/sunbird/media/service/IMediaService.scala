2628ed724a1b4f1243758a00e6fb9ee19cdff100
Mahesh Kumar Gangula <mahesh@ilimi.in>
2018-11-08 14:52:55 +0530

    Issue #SB-8781 fix: refactor MediaService Interface

--- platform-jobs/lp-batch/media-streaming/src/main/scala/org/sunbird/media/service/IMediaService.scala
+++ platform-jobs/lp-batch/media-streaming/src/main/scala/org/sunbird/media/service/IMediaService.scala
@@ -8,37 +8,16 @@ import org.sunbird.media.common.{MediaRequest, MediaResponse}
   */
 trait IMediaService {
 
-  //Job
-  def createJob(createJobRequest: MediaRequest): MediaResponse
+  def getToken(request: MediaRequest): MediaResponse
+
+  def submitJob(request: MediaRequest): MediaResponse
 
   def getJob(jobId: String): MediaResponse
+
+  def getStreamingPaths(jobId: String): MediaResponse
 
   def listJobs(listJobsRequest: MediaRequest): MediaResponse
 
   def cancelJob(cancelJobRequest: MediaRequest): MediaResponse
-
-  def getStreamingUrl(jobId: String): MediaResponse
-
-  // Preset
-  def createPreset(createPresetRequest: MediaRequest): MediaResponse
-
-  def getPreset(getPresetRequest: MediaRequest): MediaResponse
-
-  def listPresets(listPresetRequest: MediaRequest): MediaResponse
-
-  def updatePreset(updatePresetRequest: MediaRequest): MediaResponse
-
-  def deletePreset(deletePresetRequest: MediaRequest): MediaResponse
-
-  //Template
-  def createJobTemplate(createTemplateRequest: MediaRequest): MediaResponse
-
-  def getJobTemplate(templateId: String): MediaResponse
-
-  def listJobTemplate(listTemplateRequest: MediaRequest): MediaResponse
-
-  def updateJobTemplate(updateTemplateRequest: MediaRequest): MediaResponse
-
-  def deleteJobTemplate(deleteTemplateRequest: MediaRequest): MediaResponse
 
 }
