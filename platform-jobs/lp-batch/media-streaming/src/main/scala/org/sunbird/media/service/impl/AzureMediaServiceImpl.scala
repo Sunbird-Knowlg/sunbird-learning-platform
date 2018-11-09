48b051afb16edf3032d4df5e22711b68aa7320d0
Kumar Gauraw <gaurawk@ilimi.in>
2018-11-09 08:28:00 +0530

    Issue #SB-8781 feat: Prepare Azure API URL

--- platform-jobs/lp-batch/media-streaming/src/main/scala/org/sunbird/media/service/impl/AzureMediaServiceImpl.scala
+++ platform-jobs/lp-batch/media-streaming/src/main/scala/org/sunbird/media/service/impl/AzureMediaServiceImpl.scala
@@ -1,6 +1,10 @@
 package org.sunbird.media.service.impl
 
-import org.sunbird.media.common.{MediaRequest, MediaResponse}
+import java.util.UUID
+
+import org.sunbird.media.common.{MediaRequest, MediaResponse, ResponseCode}
+import org.sunbird.media.config.AppConfig
+import org.sunbird.media.exception.MediaServiceException
 import org.sunbird.media.service.IMediaService
 
 /**
@@ -31,5 +35,30 @@ object AzureMediaServiceImpl extends IMediaService {
 
   override def cancelJob(cancelJobRequest: MediaRequest): MediaResponse = {
     null
+  }
+
+  def prepareApiUrl(apiName: String): String = {
+
+    val subscriptionId: String = AppConfig.getSystemConfig("azure.subscription_id")
+    val resourceGroupName: String = AppConfig.getSystemConfig("azure.resource_group_name")
+    val accountName: String = AppConfig.getSystemConfig("azure.account_name")
+    val apiVersion: String = AppConfig.getConfig("azure.api.version")
+    val transformName: String = AppConfig.getConfig("azure.transform.default")
+
+    val baseUrl: String = new StringBuilder().append("https://management.azure.com/subscriptions/")
+      .append(subscriptionId)
+      .append("/resourceGroups/")
+      .append(resourceGroupName)
+      .append("/providers/Microsoft.Media/mediaServices/")
+      .append(accountName).mkString
+
+
+    apiName.toLowerCase() match {
+      case "asset" => baseUrl + "/assets/:assetId?api-version=" + apiVersion
+      case "job" => baseUrl + "/transforms/" + transformName + "/jobs/jobId?api-version=" + apiVersion
+      case "create_stream_locator" => baseUrl + "/streamingLocators/streamingLocatorName?api-version=" + apiVersion
+      case "list_paths" => baseUrl + "s/treamingLocators/streamingLocatorName/listPaths?api-version=" + apiVersion
+      case _ => throw new MediaServiceException("ERR_INVALID_API_NAME", "Please Provide Valid Media Service API Name")
+    }
   }
 }
