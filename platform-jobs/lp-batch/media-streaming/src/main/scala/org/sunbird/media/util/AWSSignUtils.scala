e1847ab96e56d11f729bf34497065dcc0d0b9184
Pradyumna Nagendra <pradyumnan@ilimi.in>
2018-11-09 19:16:21 +0530

    Issue #SB-8781 feat: Updated AWS authorization generation util

--- platform-jobs/lp-batch/media-streaming/src/main/scala/org/sunbird/media/util/AWSSignUtils.scala
+++ platform-jobs/lp-batch/media-streaming/src/main/scala/org/sunbird/media/util/AWSSignUtils.scala
@@ -1,13 +1,13 @@
 package org.sunbird.media.util
 
-import java.math.BigInteger
-import java.security.MessageDigest
 import java.text.SimpleDateFormat
 import java.util.Date
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
+import javax.xml.bind.DatatypeConverter
 
 import org.apache.commons.codec.binary.Hex
+import org.apache.commons.codec.digest.DigestUtils
 import org.apache.commons.lang3.StringUtils
 import org.sunbird.media.config.AppConfig
 
@@ -15,15 +15,23 @@ import scala.util.parsing.json.JSONObject
 
 object AWSSignUtils {
 
+  val accessKey = AppConfig.getSystemConfig("aws.token.access_key")
   val secret = AppConfig.getSystemConfig("aws.token.access_secret")
   val region = AppConfig.getConfig("aws.region")
   val service = AppConfig.getConfig("aws.service.name")
+  val version = AppConfig.getConfig("aws.api.version")
 
 
-  def getSiginingkey() : String = {
+  def getSiginingkey() : Array[Byte] = {
     val dateFormat = new SimpleDateFormat("yyyyMMdd")
     val date = dateFormat.format(new Date()).getBytes("UTF8")
-    HmacSHA256(HmacSHA256(HmacSHA256(HmacSHA256("AWS4" + secret, date), region.getBytes("UTF8")), service.getBytes("UTF8")), "aws4_request".getBytes("UTF8"))
+    val kSecret = ("AWS4" + secret).getBytes("UTF8");
+    val kDate = HmacSHA256(date, kSecret);
+    val kRegion = HmacSHA256(region.getBytes("UTF8"), kDate);
+    val kService = HmacSHA256(service.getBytes("UTF8"), kRegion);
+    val kSigning = HmacSHA256("aws4_request".getBytes("UTF8"), kService);
+
+    kSigning
   }
 
 
@@ -34,27 +42,29 @@ object AWSSignUtils {
     val canonicalHeaders = getCanonicalHeaders(headers, hashedPayload)
     val signedHeaders = getSignedHeaders(headers.keySet)
 
-
     val canonicalRequest = httpMethod + "\n" + canonicalUri + "\n" + canonicalQueryString +"\n" + canonicalHeaders + "\n" + signedHeaders + "\n" + hashedPayload
 
-    val timeStampISO8601Format = new SimpleDateFormat("yyyyMMddThhmmssZ").format(new Date())
+    val timeStampISO8601Format = headers.get("x-amz-date").get
     val scope = new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + region + "/" + service + "/aws4_request"
 
-    "AWS4-HMAC-SHA256" + "\n" + timeStampISO8601Format + "\n" + scope + "\n" + Hex.encodeHex(sha256Hash(canonicalRequest).getBytes("UTF8")).toString
+    val stringToSign = "AWS4-HMAC-SHA256" + "\n" + timeStampISO8601Format + "\n" + scope + "\n" + sha256Hash(canonicalRequest)
+
+    stringToSign
   }
 
-  def generateToken(httpMethod: String, url: String, headers:Map[String, String], payload : Map[String, AnyRef]) : String = {
-    val signature: String = Hex.encodeHex(HmacSHA256(getStringToSign(httpMethod, url, headers, payload), getSiginingkey().getBytes("UTF8")).getBytes("UTF8"), true).toString
-    "AWS4-HMAC-SHA256 Credential=" + "/" + new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + region + "/" + service + "/aws4_request,SignedHeaders=" + getSignedHeaders(headers.keySet) + ",Signature=" +signature
+  def generateToken(httpMethod: String, url: String, headers: Map[String, String], payload : Map[String, AnyRef]) : String = {
+    val signature = new String(Hex.encodeHex(HmacSHA256(getStringToSign(httpMethod, url, headers, payload).getBytes("UTF-8"), getSiginingkey())))
+
+    "AWS4-HMAC-SHA256 Credential=" + accessKey +  "/" + new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + region + "/" + service + "/aws4_request,SignedHeaders=" + getSignedHeaders(headers.keySet) + ",Signature=" + signature
   }
 
 
   @throws[Exception]
-  def HmacSHA256(data: String, key: Array[Byte]) = {
-    val algorithm : String = "HmacSHA256"
+  def HmacSHA256(data: Array[Byte], key: Array[Byte]) = {
+    val algorithm : String = "HMacSha256"
     val mac :Mac = Mac.getInstance(algorithm)
     mac.init(new SecretKeySpec(key, algorithm))
-    mac.doFinal(data.getBytes("UTF8")).toString
+    mac.doFinal(data)
   }
 
 
@@ -76,28 +86,33 @@ object AWSSignUtils {
   }
 
   def sha256Hash(input: String): String = {
-    String.format("%032x", new BigInteger(1, MessageDigest.getInstance("SHA-256").digest(input.getBytes("UTF-8"))))
+
+    DigestUtils.sha256Hex(input)
   }
 
 
   def getCanonicalUri(url: String): String = {
-    val uri = url.split(AppConfig.getConfig("aws.version"))(1)
-    if(StringUtils.isBlank(uri)) "" else uri
+    var uri = url.split(version)(1)
+    if(StringUtils.isBlank(uri)) "" else{uri = "/" + version + uri; uri}
   }
 
   def getCanonicalQueryString(url: String): String = {
-    val queryString: String = url.split("\\?")(1)
     var result:String = ""
-    if(StringUtils.isNotBlank(queryString)){
-      for(param <- queryString.split("\\&")){
-        if(param.split("=").length == 2)
-          result += uriEncode(param.split("=")(0).toCharArray, false) + "=" +  uriEncode(param.split("=")(1).toCharArray, false) + "&"
-        else
-          result += uriEncode(param.split("=")(0).toCharArray, false) + "&"
+    if(url.split("\\?").length > 1){
+      val queryString: String = url.split("\\?")(1)
+      if(StringUtils.isNotBlank(queryString)){
+        for(param <- queryString.split("\\&")){
+          if(param.split("=").length == 2)
+            result += uriEncode(param.split("=")(0).toCharArray, false) + "=" +  uriEncode(param.split("=")(1).toCharArray, false) + "&"
+          else
+            result += uriEncode(param.split("=")(0).toCharArray, false) + "&"
+        }
+        result = result.substring(0, result.length - 2)
       }
-      result = result.substring(0, result.length - 2)
+      result
+    }else {
+      result
     }
-    result
   }
 
   def getCanonicalHeaders(headers: Map[String, String], hashedPayload: String): String = {
@@ -106,26 +121,32 @@ object AWSSignUtils {
       result += header._1.toLowerCase + ":" + header._2.trim + "\n"
     })
 
-    result += "x-amz-content-sha256" + ":" + hashedPayload.trim
+    //result += "x-amz-content-sha256" + ":" //+ hashedPayload.trim
     result
   }
 
-  def getSignedHeaders(keySet: Set[String]) :String = {
+  def getSignedHeaders(keySet: scala.collection.Set[String]) :String = {
     var result :String = ""
     keySet.foreach(key => {
-      result += key + ";"
+      result += key.toLowerCase + ";"
     })
 
-    result += "x-amz-content-sha256"
-    result
+    //result += "x-amz-content-sha256"
+    result.substring(0, result.length - 1)
   }
 
 
   def getHashedPayload(payload: Map[String, AnyRef]) : String = {
     if(null != payload && !payload.isEmpty) {
-      Hex.encodeHex(sha256Hash(JSONObject(payload).toString()).getBytes("UTF8")).toString
+      sha256Hash(JSONObject(payload).toString())
     }else {
-      Hex.encodeHex(sha256Hash("").getBytes("UTF8")).toString
+      sha256Hash("")
     }
   }
+
+
+  def stringtoHex(str: String) : String = {
+    DatatypeConverter.printHexBinary(str.getBytes("UTF8"))
+  }
+
 }
