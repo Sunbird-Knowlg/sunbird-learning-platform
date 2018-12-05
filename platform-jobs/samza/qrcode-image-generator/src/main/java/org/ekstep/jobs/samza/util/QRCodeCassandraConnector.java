8214954703c8bec8de5ae0e895184611b689c25b
Jaikumar Soundara Rajan <>
2018-12-05 19:14:56 +0530

    Issue #000 fix: QR Code Samza Job

--- platform-jobs/samza/qrcode-image-generator/src/main/java/org/ekstep/jobs/samza/util/QRCodeCassandraConnector.java
+++ platform-jobs/samza/qrcode-image-generator/src/main/java/org/ekstep/jobs/samza/util/QRCodeCassandraConnector.java
@@ -6,12 +6,12 @@ import org.ekstep.cassandra.connector.util.CassandraConnector;
 public class QRCodeCassandraConnector {
 
     public static void updateDownloadUrl(String id, String downloadUrl) {
-        String query = "update dialcodes.dialcode_images set url='"+downloadUrl+"' where filename='"+id+"'";
+        String query = "update dialcodes.dialcode_images set status=2, url='"+downloadUrl+"' where filename='"+id+"'";
         executeQuery(query);
     }
 
     public static void updateDownloadZIPUrl(String id, String downloadZIPUrl) {
-        String query = "update dialcodes.dialcode_batch set url='"+downloadZIPUrl+"' where processid="+id;
+        String query = "update dialcodes.dialcode_batch set status=2, url='"+downloadZIPUrl+"' where processid="+id;
         executeQuery(query);
     }
 
