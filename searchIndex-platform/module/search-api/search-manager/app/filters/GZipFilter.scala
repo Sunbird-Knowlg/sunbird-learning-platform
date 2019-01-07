acb336e2e6b2e99d802eb158ebabe50011c3fb2d
Pradyumna Nagendra <pradyumnan@ilimi.in>
2019-01-07 17:27:10 +0530

    Issue #SC-835 feat: Gzip response based on request header

--- searchIndex-platform/module/search-api/search-manager/app/filters/GZipFilter.scala
+++ searchIndex-platform/module/search-api/search-manager/app/filters/GZipFilter.scala
@@ -2,10 +2,13 @@ package filters
 
 import javax.inject.Inject
 
+import org.apache.commons.lang3.StringUtils
 import play.api.http.HttpFilters
 import play.filters.gzip.GzipFilter
 
 class GZipFilter @Inject() extends HttpFilters {
-  def filters = Seq(new GzipFilter(shouldGzip = (request, response) =>
-    request.headers.get("Accept-Encoding").exists(_.equalsIgnoreCase("gzip"))))
+  def filters = Seq(new GzipFilter(shouldGzip = (request, response) => {
+    println("Accepted Types :" + request.acceptedTypes)
+    request.headers.get("Accept-Encoding").exists(StringUtils.containsIgnoreCase(_, "gzip"))
+  }))
 }
