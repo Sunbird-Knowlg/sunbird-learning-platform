56eec688d9a001db04c83a7c67b51498b1e53b8e
Kumar Gauraw <gaurawk@ilimi.in>
2019-03-18 18:38:38 +0530

    Issue #SB-0000 fix: updated definition and pom version

--- platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/util/SyncMessageGenerator.java
+++ platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/util/SyncMessageGenerator.java
@@ -2,6 +2,7 @@ package org.ekstep.sync.tool.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
