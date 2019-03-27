0c606e87b8c87398818c083c746574f06d6de624
G33tha <geetha.p@optit.co>
2019-03-27 11:55:50 +0530

    Issue #000 feat: Adding vm set up script
    DO-306

---
+++ vmsetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install redis-server -y
+curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.2.deb
+sudo dpkg -i elasticsearch-6.3.2.deb
+sudo service elasticsearch start
+sudo service elasticsearch status
