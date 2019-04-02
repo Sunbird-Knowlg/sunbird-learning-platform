81955023fbd69f7d632ba95fd81c9f1caadae2f1
Pradyumna Nagendra <pradyumnan@ilimi.in>
2019-04-02 16:59:09 +0530

    Issue #SC-918 feat: Get Hierarchy Refactoring

--- platform-modules/manager/src/main/java/org/ekstep/content/mgr/impl/operation/hierarchy/GetHierarchyOperation.java
+++ platform-modules/manager/src/main/java/org/ekstep/content/mgr/impl/operation/hierarchy/GetHierarchyOperation.java
@@ -7,6 +7,8 @@ import org.ekstep.common.dto.Request;
 import org.ekstep.common.dto.Response;
 import org.ekstep.common.exception.ClientException;
 import org.ekstep.common.exception.ResourceNotFoundException;
+import org.ekstep.common.mgr.ConvertGraphNode;
+import org.ekstep.common.mgr.ConvertToGraphNode;
 import org.ekstep.graph.cache.util.RedisStoreUtil;
 import org.ekstep.graph.dac.model.Node;
 import org.ekstep.graph.model.node.DefinitionDTO;
@@ -17,6 +19,8 @@ import org.ekstep.learning.contentstore.ContentStoreParams;
 import org.ekstep.taxonomy.mgr.impl.BaseContentManager;
 import org.ekstep.telemetry.logger.TelemetryManager;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -24,47 +28,6 @@ import java.util.stream.Collectors;
 public class GetHierarchyOperation extends BaseContentManager {
 
     private static final String COLLECTION_MIME_TYPE = "application/vnd.ekstep.content-collection";
-
-    public Response getHierarchy(String contentId, String mode) {
-        if(StringUtils.equalsIgnoreCase("edit", mode)){
-            Node node = getContentNode(TAXONOMY_ID, contentId, mode);
-
-            boolean fetchAll = true;
-            String nodeStatus = (String) node.getMetadata().get("status");
-            validateIsNodeRetired(node.getMetadata());
-
-            if(!(StringUtils.equalsIgnoreCase(mode, "edit")) && (StringUtils.equalsIgnoreCase(nodeStatus, "Live") || StringUtils.equalsIgnoreCase(nodeStatus, "Unlisted"))) {
-                fetchAll = false;
-            }
-
-            TelemetryManager.log("Collecting Hierarchical Data For Content Id: " + node.getIdentifier());
-            DefinitionDTO definition = getDefinition(TAXONOMY_ID, node.getObjectType());
-            Map<String, Object> map = util.getContentHierarchyRecursive(TAXONOMY_ID, node, definition, mode, true);
-            Map<String, Object> dataMap = contentCleanUp(map);
-            Response response = new Response();
-            response.put("content", dataMap);
-            response.setParams(getSucessStatus());
-            return response;
-        } else{
-            Response hierarchyResponse = getCollectionHierarchy(contentId);
-            Response response = new Response();
-            if(!checkError(hierarchyResponse) && (null != hierarchyResponse.getResult().get("hierarchy"))){
-                String cachedStatus = RedisStoreUtil.getNodeProperty(TAXONOMY_ID, contentId, "status");
-                Map<String, Object> hierarchy = (Map<String, Object>) hierarchyResponse.getResult().get("hierarchy");
-                if(StringUtils.isNotBlank(cachedStatus)){
-                    hierarchy.put("status", cachedStatus);
-                } else{
-                    hierarchy.put("status", getStatus(contentId, mode));
-                }
-
-                response.put("content", hierarchy);
-                response.setParams(getSucessStatus());
-            } else {
-                response = hierarchyResponse;
-            }
-            return response;
-        }
-    }
 
     private Response getCollectionHierarchy(String contentId) {
         Request request = new Request();
@@ -81,28 +44,54 @@ public class GetHierarchyOperation extends BaseContentManager {
         return (String) node.getMetadata().get("status");
     }
 
-    public Response getContentHierarchy(String contentId, String bookMarkId, String mode, List<String> fields) {
+    /**
+     *
+     * @param contentId
+     * @param bookMarkId
+     * @param mode
+     * @param fields
+     * @return
+     */
+    public Response getContentHierarchy(String contentId, String bookMarkId, String mode, List<String> fields)  {
+        // Check bookMarkId is same as contentId
         String id = getNodeIdToBeFetched(contentId, bookMarkId);
+
+        // For unpublished contents
         if(StringUtils.equalsIgnoreCase("edit", mode)){
-            Node node = getContentNode(TAXONOMY_ID, id, mode);
-            String nodeStatus = (String) node.getMetadata().get("status");
+            // Fetch collection root node from neo4j
+            Node node = getContentNode(TAXONOMY_ID, contentId, mode);
 
-            if(StringUtils.equalsIgnoreCase(nodeStatus, "Retired")) {
-                throw new ResourceNotFoundException(ContentErrorCodes.ERR_CONTENT_NOT_FOUND.name(),
-                        "Content not found with id: " + id);
-            }
-
+            // Check for Collection mimetypes
             if(!StringUtils.equalsIgnoreCase(COLLECTION_MIME_TYPE, (String) node.getMetadata().get("mimeType"))) {
                 throw new ClientException(ContentErrorCodes.ERR_INVALID_INPUT.name(),
                         "Requested ID is not of collection mimeType : " + id);
             }
 
+            // Check for Retired Status
+            if(StringUtils.equalsIgnoreCase("Retired", (String) node.getMetadata().get("status"))) {
+                throw new ResourceNotFoundException(ContentErrorCodes.ERR_CONTENT_NOT_FOUND.name(),
+                        "Content not found with id: " + id);
+            }
+            // get Content definition
             DefinitionDTO definition = getDefinition(TAXONOMY_ID, node.getObjectType());
-            long startTime = System.currentTimeMillis();
-            Map<String,Object> dataMap = util.getHierarchyMap(TAXONOMY_ID, node.getIdentifier(), definition, mode,
-                    fields);
-            System.out.println("Time to fetchNodes and construct hierarchy: " + (System.currentTimeMillis() - startTime));
+            Map<String,Object> dataMap = ConvertGraphNode.convertGraphNode(node, TAXONOMY_ID, definition, fields);
 
+            String hierarchyResponse = getContentBody(node.getIdentifier().concat(".img"));
+
+            long startTime = System.currentTimeMillis();
+            if(StringUtils.isNotBlank(hierarchyResponse)){
+                try {
+                    dataMap.put("children", objectMapper.readValue(hierarchyResponse, List.class));
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+            }else {
+                dataMap = util.getHierarchyMap(TAXONOMY_ID, node.getIdentifier(), definition, mode,
+                        fields);
+                createCollectionHierarchy(node.getIdentifier());
+            }
+            TelemetryManager.log("Time to fetchNodes and construct hierarchy: " + (System.currentTimeMillis() -
+                    startTime));
             Response response = new Response();
             response.put("content", dataMap);
             response.setParams(getSucessStatus());
@@ -161,5 +150,12 @@ public class GetHierarchyOperation extends BaseContentManager {
         }
         return null;
 
+    }
+
+    /**
+     * Aysnc call to update cassandra and clean up neo4j for collection hierarchy
+     * @param identifier
+     */
+    private void createCollectionHierarchy(String identifier) {
     }
 }
