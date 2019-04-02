2c746af1fa9a0ad6159b7400d372bacbbdee7e4d
Mahesh Kumar Gangula <mahesh@ilimi.in>
2019-04-02 09:27:47 +0530

    Issue #000 fix: changes to circleci configuration.

--- vmsetup.sh
+++ vmsetup.sh
@@ -16,4 +16,3 @@ find ./ -type f -name "log4j2.xml" -print0 | xargs -0 sed -i -e 's/\/data\/logs/
 find ./ -type f -name "logback.xml" -print0 | xargs -0 sed -i -e 's/\/data\/logs/logs/g'
 find ./ -type f -name "application.conf" -print0 | xargs -0 sed -i -e 's/\/data\//~\//g'
 find ./ -type f -name "*.java" -print0 | xargs -0 sed -i -e 's/\/data\//~\//g'
-mvn clean install
