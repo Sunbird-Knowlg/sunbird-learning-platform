35bad15058e67571e05f3bb91b271a25ad9eb6c8
Kumar Gauraw <gaurawk@ilimi.in>
2019-05-31 14:07:00 +0530

    Issue #SC-0000 fix: updated routes

--- content-service/content-api/app/controllers/ContentV3Controller.scala
+++ content-service/content-api/app/controllers/ContentV3Controller.scala
@@ -7,6 +7,7 @@ import javax.inject.{Inject, Singleton}
 import org.ekstep.actor.ContentActor
 import org.ekstep.common.dto.Response
 import org.ekstep.commons.{APIIds, Request}
+import org.ekstep.util.JSONUtils
 import play.api.mvc.{Action, AnyContent}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -17,6 +18,13 @@ class ContentV3Controller @Inject()(system: ActorSystem) extends BaseController 
 
   val contentActor = system.actorOf(Props(ContentActor).withRouter(FromConfig), name = "contentActor")
 
+  def create() = Action.async {
+    implicit request =>
+      val body: String = JSONUtils.serialize(request.body.asJson.getOrElse(""))
+      val result = ask(contentActor, Request(APIIds.CREATE_CONTENT, Some(body), Some(Map())))
+        .mapTo[Response]
+      result.map(response => sendResponse(response))
+  }
 
   def read(identifier: String, mode: Option[String], fields: Option[List[String]]): Action[AnyContent] = Action.async {
     implicit request =>
