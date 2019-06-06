4024c8753a79c3cb711dbfa746e040c2a388a905
Kumar Gauraw <gaurawk@ilimi.in>
2019-06-06 17:41:03 +0530

    Issue #SC-0000 feat: dial link api

--- content-service/content-api-core/src/main/scala/org/ekstep/actor/ContentActor.scala
+++ content-service/content-api-core/src/main/scala/org/ekstep/actor/ContentActor.scala
@@ -1,33 +1,26 @@
 package org.ekstep.actor
 
-import org.ekstep.actor.core.BaseAPIActor
-import org.ekstep.commons.{APIIds, Request}
 import akka.dispatch.Futures
 import akka.pattern.Patterns
-import org.ekstep.managers.ContentManager
+import org.ekstep.actor.core.BaseAPIActor
+import org.ekstep.commons.{APIIds, Request}
+import org.ekstep.managers.{ContentDialCodeManagerImpl, ContentManager}
 import org.ekstep.mgr.impl.ContentManagerImpl
 
 object ContentActor extends BaseAPIActor {
 
   override def onReceive(request: Request) = {
-
     request.apiId match {
-      case APIIds.CREATE_CONTENT =>
-        createContent(request)
-
-      case APIIds.READ_CONTENT =>
-        readContent(request)
-
-      case APIIds.UPDATE_CONTENT =>
-        updateContent(request)
-
-      case APIIds.REVIEW_CONTENT =>
-        reviewContent(request)
-
-      case _ =>
-        invalidAPIResponseSerialized(request.apiId);
+      case APIIds.CREATE_CONTENT => createContent(request)
+      case APIIds.READ_CONTENT => readContent(request)
+      case APIIds.UPDATE_CONTENT => updateContent(request)
+      case APIIds.REVIEW_CONTENT => reviewContent(request)
+      case APIIds.DIALCODE_LINK => linkDialCode(request)
+      case APIIds.DIALCODE_COLLECTION_LINK => collectionLinkDialCode(request)
+      case APIIds.DIALCODE_RESERVE => reserveDialCode(request)
+      case APIIds.DIALCODE_RELEASE => releaseDialCode(request)
+      case _ => invalidAPIResponseSerialized(request.apiId);
     }
-
   }
 
   private def createContent(request: Request) = {
@@ -78,6 +71,77 @@ object ContentActor extends BaseAPIActor {
         sender().tell(getErrorResponse(e, request.apiId), self)
     }
 
+  }
+
+  /**
+    * Link DIAL Code to Neo4j Object
+    *
+    * @param request
+    * @return
+    */
+  private def linkDialCode(request: Request) = {
+    try {
+      println("contentActor:: start")
+      val result = ContentDialCodeManagerImpl.linkDialCode(request)
+      //TODO: Review Below Code to make a single method.
+      println("result :: "+result)
+      val response = setResponseEnvelope(result, request.apiId, null)
+      Patterns.pipe(Futures.successful(response), getContext().dispatcher).to(sender())
+    } catch {
+      case e: Exception =>
+        sender().tell(getErrorResponse(e, request.apiId), self)
+    }
+  }
+
+  /**
+    * Link DIAL Code to Collection Objects
+    *
+    * @param request
+    * @return
+    */
+  private def collectionLinkDialCode(request: Request) = {
+    try {
+      val result = ContentDialCodeManagerImpl.collectionLinkDialCode(request)
+      val response = setResponseEnvelope(result, request.apiId, null)
+      Patterns.pipe(Futures.successful(response), getContext().dispatcher).to(sender())
+    } catch {
+      case e: Exception =>
+        sender().tell(getErrorResponse(e, request.apiId), self)
+    }
+  }
+
+  /**
+    * Reserve DIAL Codes for Textbook
+    *
+    * @param request
+    * @return
+    */
+  private def reserveDialCode(request: Request) = {
+    try {
+      val result = ContentDialCodeManagerImpl.reserveDialCode(request)
+      val response = setResponseEnvelope(result, request.apiId, null)
+      Patterns.pipe(Futures.successful(response), getContext().dispatcher).to(sender())
+    } catch {
+      case e: Exception =>
+        sender().tell(getErrorResponse(e, request.apiId), self)
+    }
+  }
+
+  /**
+    * Release DIAL Codes for Textbook
+    *
+    * @param request
+    * @return
+    */
+  private def releaseDialCode(request: Request) = {
+    try {
+      val result = ContentDialCodeManagerImpl.releaseDialCode(request)
+      val response = setResponseEnvelope(result, request.apiId, null)
+      Patterns.pipe(Futures.successful(response), getContext().dispatcher).to(sender())
+    } catch {
+      case e: Exception =>
+        sender().tell(getErrorResponse(e, request.apiId), self)
+    }
   }
 
 
--- content-service/content-api-core/src/main/scala/org/ekstep/commons/Model.scala
+++ content-service/content-api-core/src/main/scala/org/ekstep/commons/Model.scala
@@ -91,5 +91,10 @@ object ContentMetadata {
     val gzip, identity = Value
   }
 
+  object DialCodeEnum extends Enumeration {
+    type String = Value
+    val ERR_DIALCODE_LINK, ERR_DIALCODE_LINK_REQUEST= Value
+  }
+
 }
 
