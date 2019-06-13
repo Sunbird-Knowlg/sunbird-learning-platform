749c2fa504693da0e8a130fc213f7bf80faded50
rheafernandes <rheafernandeshallu@gmail.com>
2019-06-13 17:05:37 +0530

    Issue #SC-0000: Discard-Api Changed delete to update hierarchy

--- platform-modules/manager/src/main/java/org/ekstep/content/mgr/impl/operation/content/DiscardOperation.java
+++ platform-modules/manager/src/main/java/org/ekstep/content/mgr/impl/operation/content/DiscardOperation.java
@@ -1,5 +1,6 @@
 package org.ekstep.content.mgr.impl.operation.content;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.ekstep.common.Platform;
 import org.ekstep.common.dto.Request;
@@ -17,7 +18,9 @@ import org.ekstep.taxonomy.mgr.impl.BaseContentManager;
 import org.ekstep.telemetry.logger.TelemetryManager;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Rhea Fernandes
@@ -26,6 +29,8 @@ public class DiscardOperation extends BaseContentManager {
     private Boolean isCollection = false;
     private static final List<String> CONTENT_DISCARD_STATUS = Platform.config.hasPath("content.discard.status") ?
             Platform.config.getStringList("content.discard.status") : Arrays.asList("Draft", "FlagDraft");
+    private static final List<String> REMOVE_PUBLISHED_DATA = Arrays.asList("compatibilityLevel", "lastPublishedOn", "pkgVersion", "leafNodesCount", "downloadUrl", "variants");
+
     /**
      * This API will allow to discard content
      * @param contentId
@@ -40,7 +45,7 @@ public class DiscardOperation extends BaseContentManager {
             if(StringUtils.equalsIgnoreCase(objectType, "ContentImage"))
                 response = discardNode(imageNode);
             else
-                throw new ClientException(ContentErrorCodes.ERR_OBJECT_TYPE_MISMATCH.name(), "Content Image Node objectType isn't matching");
+                throw new ResourceNotFoundException(ContentErrorCodes.ERR_INVALID_CONTENT_ID.name(), "Content Id is invalid");
         } else {
             Node node = getNode(contentId, false);
             if (node == null) {
@@ -70,13 +75,12 @@ public class DiscardOperation extends BaseContentManager {
         if (CONTENT_DISCARD_STATUS.contains(status)) {
             if (isCollection) {
                 Response responseCollection = discardCollection(contentId);
-                if (StringUtils.equalsIgnoreCase(ResponseCode.OK.name(), responseCollection.getResponseCode().name())) {
+                if (!checkError(responseCollection)) {
                     return discardContent(contentId);
                 }
                 return responseCollection;
             } else {
                 return discardContent(contentId);
-
             }
         } else {
             throw new ClientException(ContentErrorCodes.ERR_CONTENT_NOT_DRAFT.name(),
@@ -126,10 +130,14 @@ public class DiscardOperation extends BaseContentManager {
      */
     private Response discardCollection(String contentId) {
         String identifier = contentId;
-            if(!StringUtils.endsWithIgnoreCase(contentId, ".img"))
-                identifier = identifier + ".img";
-            Response resp = deleteHierarchy(Arrays.asList(identifier));
-            return resp;
+        Response resp = null;
+        if(StringUtils.endsWithIgnoreCase(contentId, ".img")){
+            resp = updateImageHierarchy(contentId);
+        }else {
+            identifier = identifier + ".img";
+            resp = deleteHierarchy(Arrays.asList(identifier));
+        }
+        return resp;
     }
 
     /**
@@ -156,6 +164,40 @@ public class DiscardOperation extends BaseContentManager {
         else
             response.getResult().put("message", "Draft version of the content with id : " + contentId + "is discarded");
         return response;
+    }
+
+    private Response updateImageHierarchy(String contentId) {
+        Response response = getCollectionHierarchy(contentId.replace(".img", ""));
+        if (!checkError(response)) {
+            Map<String, Object> liveHierarchy = (Map<String, Object>) response.getResult().get("hierarchy");
+            List<Map<String, Object>> children = (List<Map<String, Object>>) liveHierarchy.get("children");
+            Node node = getNode(contentId.replace(".img", ""), false);
+            updateHierarchyMetadata(children, node);
+            Map<String,Object> hierarchy = new HashMap<>();
+            hierarchy.put("identifier", node.getIdentifier());
+            hierarchy.put("children", children);
+            Response resp = updateCollectionHierarchy(node.getIdentifier()+".img", hierarchy);
+            return resp;
+        }
+        return response;
+    }
+
+    private void updateHierarchyMetadata(List<Map<String, Object>> children, Node node) {
+        if (CollectionUtils.isNotEmpty(children)) {
+            for (Map<String, Object> child : children) {
+                if (StringUtils.equalsIgnoreCase("Parent",
+                        (String) child.get("visibility"))) {
+                    populatePublishMetadata(child, node);
+                    updateHierarchyMetadata((List<Map<String, Object>>) child.get("children"), node);
+                }
+            }
+        }
+    }
+
+    private void populatePublishMetadata(Map<String, Object> content, Node node) {
+        REMOVE_PUBLISHED_DATA.forEach(property -> content.remove(property));
+        content.put("status", "Draft");
+        content.put("lastUpdatedOn", (String) node.getMetadata().get("lastUpdatedOn"));
     }
 }
 
