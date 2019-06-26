ca6020f41ba12f299149c9119fd1a4187725eb9e
Pradyumna Nagendra <pradyumnan@ilimi.in>
2019-06-26 12:13:36 +0530

    Issue #SB-13293 feat: Added more loggers to syncleafnodescount

--- platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/mgr/CassandraESSyncManager.java
+++ platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/mgr/CassandraESSyncManager.java
@@ -395,7 +395,11 @@ public class CassandraESSyncManager {
         domainObj.setObjectType("Content");
         domainObj.setIdentifier(id);
         Response response = util.updateNodeWithoutValidation(domainObj) ;
-        if(util.checkError(response))
+        if(util.checkError(response)){
+            System.out.println("Status :::::::" + response.getParams().getStatus());
+            System.out.println(mapper.writeValueAsString(response));
             throw new ServerException("Error while updating RootNode" , response.getParams().getErr() + " :: "+ response.getParams().getErrmsg() + " :: " + response.getResult());
+        }
+
     }
 }
