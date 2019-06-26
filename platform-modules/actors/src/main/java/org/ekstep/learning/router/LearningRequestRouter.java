27110ee7e28eefddb9abf78942da9bbda0e1045e
Pradyumna Nagendra <pradyumnan@ilimi.in>
2019-06-26 16:58:24 +0530

    Issue #000 fix: Removed Fw-dispatcher

--- platform-modules/actors/src/main/java/org/ekstep/learning/router/LearningRequestRouter.java
+++ platform-modules/actors/src/main/java/org/ekstep/learning/router/LearningRequestRouter.java
@@ -86,14 +86,15 @@ public class LearningRequestRouter extends UntypedActor {
 		int poolSize = 4;
 
 		Props contentStoreProps = Props.create(ContentStoreActor.class);
-		Props fwhierarchyProps = Props.create(FrameworkHierarchyActor.class).withDispatcher("fw-dispatcher");
+
 		Props localCacheUpdaterProps = Props.create(LocalCacheUpdateActor.class);
 		ActorRef contentStoreActor = system.actorOf(new SmallestMailboxPool(poolSize).props(contentStoreProps));
-		ActorRef fwHierarchyActor = system.actorOf(new SmallestMailboxPool(8).props(fwhierarchyProps));
 		ActorRef localCacheUpdaterActor = system.actorOf(new SmallestMailboxPool(poolSize).props(localCacheUpdaterProps));
 		LearningActorPool.addActorRefToPool(LearningActorNames.CONTENT_STORE_ACTOR.name(), contentStoreActor);
-		LearningActorPool.addActorRefToPool(LearningActorNames.FRAMEWORK_HIERARCHY_ACTOR.name(), fwHierarchyActor);
 		LearningActorPool.addActorRefToPool(LearningActorNames.CACHE_UPDATE_ACTOR.name(), localCacheUpdaterActor);
+		Props fwhierarchyProps = Props.create(FrameworkHierarchyActor.class);
+		ActorRef fwHierarchyActor = system.actorOf(new SmallestMailboxPool(8).props(fwhierarchyProps));
+		LearningActorPool.addActorRefToPool(LearningActorNames.FRAMEWORK_HIERARCHY_ACTOR.name(), fwHierarchyActor);
 	}
 
 	/**
