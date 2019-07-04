e6251bf833c175767d5a427ea904d8e9abc23032
Kumar Gauraw <gaurawk@ilimi.in>
2019-07-04 19:14:46 +0530

    Issue #SC-0000 fix: build fix

--- searchIndex-platform/module/searchindex-elasticsearch/src/main/java/org/ekstep/searchindex/elasticsearch/ElasticSearchUtil.java
+++ searchIndex-platform/module/searchindex-elasticsearch/src/main/java/org/ekstep/searchindex/elasticsearch/ElasticSearchUtil.java
@@ -5,11 +5,14 @@ package org.ekstep.searchindex.elasticsearch;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.util.EntityUtils;
