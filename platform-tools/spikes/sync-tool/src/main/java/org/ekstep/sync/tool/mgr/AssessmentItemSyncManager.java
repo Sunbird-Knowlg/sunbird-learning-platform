5cdd160c0536f97ee296ab24d4cc8d59667de939
amitpriyadarshi <darshi.1983@gmail.com>
2019-09-11 15:02:10 +0530

    Issue #0000 feat: Migration tool for migrating questions external property to cassandra

--- platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/mgr/AssessmentItemSyncManager.java
+++ platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/mgr/AssessmentItemSyncManager.java
@@ -9,8 +9,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.ekstep.assessment.store.AssessmentStore;
+import org.ekstep.common.dto.Response;
 import org.ekstep.common.exception.ClientException;
 import org.ekstep.common.exception.ResourceNotFoundException;
 import org.ekstep.graph.dac.model.Node;
@@ -26,7 +28,7 @@ public class AssessmentItemSyncManager {
 	private ControllerUtil util = new ControllerUtil();
 	private static int batchSize = 50;
 	
-	public void syncAssessmentExternalProperties(String graphId, String objectType, Integer delay) throws InterruptedException {
+	public void syncAssessmentExternalProperties(String graphId, String objectType, List<String> ids, Integer delay) throws InterruptedException {
 		if (StringUtils.isBlank(graphId))
 			throw new ClientException("BLANK_GRAPH_ID", "Graph Id is blank.");
 		
@@ -36,41 +38,59 @@ public class AssessmentItemSyncManager {
 			if (null != def) {
 				System.out.println("\nSyncing external data of " + objectType + " from neo4j to cassandta.\n");
 				
-				List<String> externalProperties = getExternalPropsList(def);
 				int start = 0;
 				boolean found = true;
 				
-				while (found) {
+				
 					List<Node> nodes = null;
-					try {
-						nodes = util.getNodes(graphId, def.getObjectType(), start, batchSize);
-					}catch(ResourceNotFoundException e) {
-						System.out.println("error while fetching neo4j records for objectType="+objectType+", start="+start+",batchSize="+batchSize);
-						start += batchSize;
-						continue;
-					}
-					if (CollectionUtils.isNotEmpty(nodes)) {
-						start += batchSize;
-						for(Node node: nodes) {
-							Map<String, Object> metaData = node.getMetadata();
-							Map<String, Object> extProp = new HashMap<>();
-							for(String ext : externalProperties) {
-								if(metaData.keySet().contains(ext))
-									extProp.put(ext, metaData.get(ext));
+					if(CollectionUtils.isNotEmpty(ids)) {
+						try {
+							Response response = util.getDataNodes(graphId, ids);
+							nodes = (List<Node>) response.get("node_list");
+							if (CollectionUtils.isNotEmpty(nodes)) {
+								pushToCassandra(nodes, def);
 							}
-							// TODO: Add code for saving ext prop to cassandra
-							System.out.println("Id: " + node.getIdentifier() + " *** extProp: " + extProp);
-							assessmentStore.updateAssessmentProperties(node.getIdentifier(), extProp);
+						}catch(ResourceNotFoundException e) {
+							System.out.println("error while fetching neo4j records for ids: " + ids);
 						}
-						if (delay > 0) {
-							Thread.sleep(delay);
+					}else {
+						while (found) {
+							try {
+									nodes = util.getNodes(graphId, def.getObjectType(), start, batchSize);
+									if (CollectionUtils.isNotEmpty(nodes)) {
+										start += batchSize;
+										pushToCassandra(nodes, def);
+										if (delay > 0) {
+											Thread.sleep(delay);
+										}
+									} else {
+										found = false;
+										break;
+									}
+							
+							}catch(ResourceNotFoundException e) {
+								System.out.println("error while fetching neo4j records for objectType="+objectType+", start="+start+",batchSize="+batchSize);
+								start += batchSize;
+								continue;
+							}
 						}
-					} else {
-						found = false;
-						break;
 					}
-				}
 			}
+		}
+	}
+	
+	private void pushToCassandra(List<Node> nodes, DefinitionDTO def) {
+		List<String> externalProperties = getExternalPropsList(def);
+		for(Node node: nodes) {
+			Map<String, Object> metaData = node.getMetadata();
+			Map<String, Object> extProp = new HashMap<>();
+			for(String ext : externalProperties) {
+				if(metaData.keySet().contains(ext))
+					extProp.put(ext, metaData.get(ext));
+			}
+			System.out.println("Id: " + node.getIdentifier() + " *** extProp: " + extProp);
+			if(MapUtils.isNotEmpty(extProp))
+				assessmentStore.updateAssessmentProperties(node.getIdentifier(), extProp);
 		}
 	}
 	
