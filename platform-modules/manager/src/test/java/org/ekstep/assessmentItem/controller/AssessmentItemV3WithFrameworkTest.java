889df93652569c546865c19c22630de681545787
Kumar Gauraw <er.krgauraw@gmail.com>
2019-11-13 14:25:43 +0530

    Issue #SC-0000 fix: added import statement for build fix

--- platform-modules/manager/src/test/java/org/ekstep/assessmentItem/controller/AssessmentItemV3WithFrameworkTest.java
+++ platform-modules/manager/src/test/java/org/ekstep/assessmentItem/controller/AssessmentItemV3WithFrameworkTest.java
@@ -17,6 +17,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
