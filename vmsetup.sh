0da5fb68e2472896e149a048349e41e13f96b159
rheafernandes <rheafernandeshallu@gmail.com>
2019-11-29 12:53:48 +0530

    Removed extra file separator

--- vmsetup.sh
+++ vmsetup.sh
@@ -12,5 +12,5 @@ find ./ -type f -name "logback.xml" -print0 | xargs -0 sed -i -e 's/\/data\/logs
 find ./ -type f -name "application.conf" -print0 | xargs -0 sed -i -e 's/\/data\//~\//g'
 find ./ -type f -name "*.java" -print0 | xargs -0 sed -i -e 's/\/data\//~\//g'
 mvn clean install -DskipTests
-XML_REPORT_PATHS=`find /home/circleci/project/  -iname jacoco.xml | awk 'BEGIN { RS = "" ; FS = "\n"; OFS = ","}{$1=$1; print $0}'`
+XML_REPORT_PATHS=`find /home/circleci/project  -iname jacoco.xml | awk 'BEGIN { RS = "" ; FS = "\n"; OFS = ","}{$1=$1; print $0}'`
 mvn verify sonar:sonar -Dsonar.projectKey=project-sunbird_sunbird-learning-platform -Dsonar.organization=project-sunbird -Dsonar.host.url=https://sonarcloud.io -Dsonar.coverage.jacoco.xmlReportPaths=${XML_REPORT_PATHS}
