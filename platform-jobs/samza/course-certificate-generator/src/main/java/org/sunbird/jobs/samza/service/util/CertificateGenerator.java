ec94ef03fecdfc5ca4413e7eec669335997cdaea
Pradyumna Nagendra <pradyumnan@ilimi.in>
2019-12-09 16:21:41 +0530

    Issue #000 fix: Updated new content service configuration in samza jobs

--- platform-jobs/samza/course-certificate-generator/src/main/java/org/sunbird/jobs/samza/service/util/CertificateGenerator.java
+++ platform-jobs/samza/course-certificate-generator/src/main/java/org/sunbird/jobs/samza/service/util/CertificateGenerator.java
@@ -46,7 +46,7 @@ public class CertificateGenerator {
     private static final String ES_DOC_TYPE = "_doc";
     private static final String CERTIFICATE_BASE_PATH = Platform.config.hasPath("certificate.base_path")
             ? Platform.config.getString("certificate.base_path"): "http://localhost:9000/certs";
-    protected static final String KP_CONTENTE_SERVICE_BASE_URL = Platform.config.hasPath("kp.content_service.base_url")
+    protected static final String KP_CONTENT_SERVICE_BASE_URL = Platform.config.hasPath("kp.content_service.base_url")
             ? Platform.config.getString("kp.content_service.base_url"): "http://localhost:9000";
 
     private static JobLogger LOGGER = new JobLogger(CertificateGenerator.class);
@@ -253,7 +253,7 @@ public class CertificateGenerator {
 
     private Map<String,Object> getContent(String courseId, String fields) {
         try {
-            String url = KP_CONTENTE_SERVICE_BASE_URL + "/content/v3/read/" + courseId;
+            String url = KP_CONTENT_SERVICE_BASE_URL + "/content/v3/read/" + courseId;
             if(StringUtils.isNotBlank(fields))
                 url += "?fields=" + fields;
 
