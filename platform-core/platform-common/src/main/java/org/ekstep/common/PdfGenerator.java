3c47926b95a59c6834effddccb3401e943e47a48
your username <rheafernandeshallu@gmail.com>
2019-12-17 17:00:25 +0530

    Added pdf generator and test cases for pdf generator

---
+++ platform-core/platform-common/src/main/java/org/ekstep/common/PdfGenerator.java
@@ -0,0 +1,63 @@
+package org.ekstep.common;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class PdfGenerator {
+    /**
+     * Converts a Html String to a Pdf file
+     * @param htmlString
+     * @return
+     */
+    public static File convertHtmlStringToPdfFile(String htmlString) {
+        File file = new File("/data/tmp/" + getPdfFileName() + ".pdf");
+        try {
+            file.createNewFile();
+            OutputStream os = new FileOutputStream(file);
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.getInstance(document, os);
+            document.open();
+            InputStream is = new ByteArrayInputStream(htmlString.getBytes());
+            XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
+            document.close();
+            os.close();
+            return file;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static File convertHtmlFileToPdfFile(File htmlFile) {
+        File file = new File("/data/tmp/" + getPdfFileName() + ".pdf");
+        try{
+            file.createNewFile();
+            Document document = new Document();
+            OutputStream os = new FileOutputStream(file);
+            PdfWriter writer = PdfWriter.getInstance(document, os);
+            document.open();
+            InputStream is = new FileInputStream(htmlFile);
+            XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
+            document.close();
+            os.close();
+            return file;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String getPdfFileName() {
+        return "pdf_" + System.currentTimeMillis();
+    }
+
+
+}
---
+++ platform-core/platform-common/src/test/java/PdfGeneratorTest.java
@@ -0,0 +1,87 @@
+import org.ekstep.common.PdfGenerator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+public class PdfGeneratorTest {
+    private File htmlFile;
+    private String htmlString;
+    private String malFormedHtmlString;
+    private File htmlNonExistentFile;
+    private File pdfFile;
+
+    @Before
+    public void createInputFile() {
+        htmlFile = new File(System.getProperty("user.dir") + "/src/test/resources/test.html");
+        htmlNonExistentFile = new File("test.html");
+        htmlString = "<table style=\"width:100%\">\n" +
+                "    <caption>Monthly savings</caption>\n" +
+                "    <tr>\n" +
+                "        <th>Month</th>\n" +
+                "        <th>Savings</th>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "        <td>January</td>\n" +
+                "        <td>$100</td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "        <td>February</td>\n" +
+                "        <td>$50</td>\n" +
+                "    </tr>\n" +
+                "</table>";
+        malFormedHtmlString = "<table style=\"width:100%\">\n" +
+                "    <caption>Monthly savings</caption>\n" +
+                "    <tr>\n" +
+                "        <th>Month</th>\n" +
+                "        <th>Savings</th>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "        <td>January</td>\n" +
+                "        <td>$100</td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "        <td>February</td>\n" +
+                "        <td>$50</td>\n" +
+                "    </tr>\n" +
+                "</";
+    }
+
+    @After
+    public void deleteOutputFile() {
+        if(pdfFile != null && pdfFile.exists())
+            pdfFile.delete();
+        htmlString = null;
+        malFormedHtmlString = null;
+    }
+
+    @Test
+    public void convertHtmlStringToPdfFile() {
+        pdfFile = PdfGenerator.convertHtmlStringToPdfFile(htmlString);
+        Assert.assertNotNull(pdfFile);
+        Assert.assertEquals(pdfFile.getName().split("\\.")[1], "pdf");
+
+    }
+
+    @Test
+    public void convertHtmlFileToPdfFile() {
+        pdfFile = PdfGenerator.convertHtmlFileToPdfFile(htmlFile);
+        Assert.assertNotNull(pdfFile);
+        Assert.assertEquals(pdfFile.getName().split("\\.")[1], "pdf");
+    }
+
+    @Test
+    public void convertHtmlStringToPdfFileMalformedHtml() {
+        pdfFile = PdfGenerator.convertHtmlStringToPdfFile(malFormedHtmlString);
+        Assert.assertNull(pdfFile);
+    }
+
+    @Test
+    public void convertHtmlFileToPdfFileDoesntExist() {
+        pdfFile = PdfGenerator.convertHtmlFileToPdfFile(htmlNonExistentFile);
+        Assert.assertNull(pdfFile);
+    }
+
+}
