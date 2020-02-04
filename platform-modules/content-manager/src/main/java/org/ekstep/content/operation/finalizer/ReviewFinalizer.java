f7ce388989ef1e2a37bf45c947b36bddd3663d4b
amitpriyadarshi <darshi.1983@gmail.com>
2020-02-04 15:40:16 +0530

    Issue #SB-17644 feat: Validating Unpublished resource while collection Review and Publish

--- platform-modules/content-manager/src/main/java/org/ekstep/content/operation/finalizer/ReviewFinalizer.java
+++ platform-modules/content-manager/src/main/java/org/ekstep/content/operation/finalizer/ReviewFinalizer.java
@@ -1,6 +1,10 @@
 package org.ekstep.content.operation.finalizer;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections.CollectionUtils;
