adb4e01e01b1cfcd03ce2d7090ba07d467baf237
neha <neha0305verma@gmail.com>
2020-02-10 17:04:58 +0530

    Added backwared compatibility for subject and medium

--- searchIndex-platform/module/search-api/search-manager/app/managers/BasePlaySearchManager.java
+++ searchIndex-platform/module/search-api/search-manager/app/managers/BasePlaySearchManager.java
@@ -39,12 +39,18 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static akka.pattern.Patterns.ask;
+import java.io.IOException;
 
 public class BasePlaySearchManager {
 	protected ObjectMapper mapper = new ObjectMapper();
 	private static final Logger perfLogger = LogManager.getLogger("PerformanceTestLogger");
 	private static final String JSON_TYPE = "application/json";
 	private static Timeout WAIT_TIMEOUT = new Timeout(Duration.create(30000, TimeUnit.MILLISECONDS));
+	private static Boolean contentTaggingFlag = Platform.config.hasPath("content.tagging.backward_enable")?
+			Platform.config.getBoolean("content.tagging.backward_enable"): false;
+	private static List <String> contentTaggedKeys = Platform.config.hasPath("content.tagging.property") ?
+			Platform.config.getStringList("content.tagging.property"):
+			new ArrayList<>(Arrays.asList("subject","medium"));
 
 	protected Future<Response> getSearchResponse(Request request, ActorRef actor) {
 		Future<Response> res = null;
@@ -160,6 +166,7 @@ public class BasePlaySearchManager {
 						content.put("variants",variantsMap);
 						contentMap.set(contentMap.indexOf(content), content);
 					}
+					updateContentTaggedProperty(content);
                     contentMap.set(contentMap.indexOf(content), content);
 				}
 				response.getResult().put("content", contentMap);
@@ -167,6 +174,7 @@ public class BasePlaySearchManager {
 			if(response.getResult().containsKey("collections")) {
 				List<Map<String,Object>> collectionList = (List<Map<String, Object>>) response.getResult().get("collections");
 				for(Map<String,Object> collection : collectionList){
+					updateContentTaggedProperty(collection);
 					collectionList.set(collectionList.indexOf(collection), collection);
 				}
 				response.getResult().put("collections", collectionList);
@@ -357,6 +365,31 @@ public class BasePlaySearchManager {
 		}
 
 		return count;
+	}
+
+	private void updateContentTaggedProperty(Map<String,Object> content) {
+		if(contentTaggingFlag) {
+			for(String contentTagKey : contentTaggedKeys) {
+				if(content.containsKey(contentTagKey)) {
+					List<String> prop = null;
+					try {
+						Object value = content.get(contentTagKey);
+						if (value instanceof String[]) {
+							prop = Arrays.asList((String[]) value);
+						} else if(value instanceof  List) {
+							prop = (List<String>) value;
+						}else {
+							prop = mapper.readValue((String) value, List.class);
+						}
+						if (CollectionUtils.isNotEmpty(prop))
+							content.put(contentTagKey, prop.get(0));
+					} catch (IOException e) {
+						content.put(contentTagKey, (String) content.get(contentTagKey));
+					}
+
+				}
+			}
+		}
 	}
 
 }
