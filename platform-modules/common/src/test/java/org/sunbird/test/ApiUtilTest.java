b6f6b826aa8bca45b17dadb43d6401f349e3b0c3
Kumar Gauraw <er.krgauraw@gmail.com>
2020-02-20 02:20:37 +0530

    updated test case

--- platform-modules/common/src/test/java/org/sunbird/test/ApiUtilTest.java
+++ platform-modules/common/src/test/java/org/sunbird/test/ApiUtilTest.java
@@ -14,7 +14,7 @@ public class ApiUtilTest {
 		Map<String, Object> request = new HashMap<String, Object>();
 		String input = "Astronomy is one of the oldest natural sciences. Early civilizations dating back before 3000 BCE, such as the Sumerians, ancient Egyptians, and the Indus Valley Civilisation, had a predictive knowledge and a basic understanding of the motions of the Sun, Moon, and stars. The stars and planets, believed to represent gods, were often worshipped. While the explanations for the observed positions of the stars were often unscientific and lacking in evidence, these early observations laid the foundation for later astronomy, as the stars were found to traverse great circles across the sky,[9] which however did not explain the positions of the planets.";
 		request.put("text",input);
-		Response resp = ApiUtil.makeKeyWordsPostRequest(request);
+		Response resp = ApiUtil.makeKeyWordsPostRequest("do_123",request);
 		System.out.println(resp.getResult());
 	}
 }
