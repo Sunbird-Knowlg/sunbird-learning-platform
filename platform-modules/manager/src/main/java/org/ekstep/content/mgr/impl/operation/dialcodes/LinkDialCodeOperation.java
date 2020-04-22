6450cf71ce1d309189ef89d49b727338c3f25c63
amitpriyadarshi <darshi.1983@gmail.com>
2020-04-22 18:46:26 +0530

    Issue #0000 feat: Dialcode link changes for fetching collection from cassandra

--- platform-modules/manager/src/main/java/org/ekstep/content/mgr/impl/operation/dialcodes/LinkDialCodeOperation.java
+++ platform-modules/manager/src/main/java/org/ekstep/content/mgr/impl/operation/dialcodes/LinkDialCodeOperation.java
@@ -32,6 +32,9 @@ import static java.util.stream.Collectors.toList;
 public class LinkDialCodeOperation extends BaseContentManager {
 
     private final String ERR_DIALCODE_LINK_REQUEST = "Invalid Request.";
+    
+    private final List<String> publishStatusList = Arrays.asList("Live", "Unlisted");
+    private final List<String> collectionDraftProperties = Arrays.asList("identifier", "children");
 
     private final String DIALCODE_SEARCH_URI = Platform.config.hasPath("dialcode.api.search.url")
             ? Platform.config.getString("dialcode.api.search.url") : "http://localhost:8080/learning-service/v3/dialcode/search";
@@ -297,8 +300,13 @@ public class LinkDialCodeOperation extends BaseContentManager {
             validateDuplicateDialCodes(rootNodeId, existingDialcodes, rootChildren, null);
         }
 
+        if(rootHierarchy.containsKey("status") && publishStatusList.contains((String)rootHierarchy.get("status"))) {
+        	rootHierarchy.entrySet().removeIf(e -> !collectionDraftProperties.contains(e.getKey()));
+        	rootHierarchyId = getImageId(rootNodeId);
+        }
         //update cassandra
         Response response = updateCollectionHierarchy(rootHierarchyId,rootHierarchy);
+        
         if (!checkError(response))
             resultMap.get("updateSuccessList").addAll(requestMap.keySet());
         else
