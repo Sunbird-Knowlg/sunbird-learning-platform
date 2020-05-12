42f9246f039c2276fa51a7fc264d407c023646b5
Mahesh Kumar Gangula <mahesh@ilimi.in>
2020-05-13 01:16:12 +0530

    Issue #000 fix: create a separate list.

--- platform-modules/manager/src/main/java/org/ekstep/content/mgr/impl/operation/hierarchy/GetHierarchyOperation.java
+++ platform-modules/manager/src/main/java/org/ekstep/content/mgr/impl/operation/hierarchy/GetHierarchyOperation.java
@@ -421,7 +421,8 @@ public class GetHierarchyOperation extends BaseContentManager {
 
     private void updateLatestLeafNodes(List<Map<String, Object>> children, Map<String, Object> latestLeafNodes) {
         if (CollectionUtils.isNotEmpty(children)) {
-            children.stream().filter(child -> MapUtils.isNotEmpty(child)).forEach(child -> {
+            List<Map<String, Object>> filteredChildren = children.stream().filter(child -> MapUtils.isNotEmpty(child)).collect(Collectors.toList());
+            filteredChildren.forEach(child -> {
                 if(StringUtils.equalsIgnoreCase("Default", (String) child.getOrDefault("visibility", ""))) {
                     Map<String, Object> metadata = (Map<String, Object>) latestLeafNodes.getOrDefault((String)child.get("identifier"), new HashMap<String, Object>());
                     if(StringUtils.equalsIgnoreCase(ContentAPIParams.Retired.name(), (String) metadata.getOrDefault("status", ""))) {
