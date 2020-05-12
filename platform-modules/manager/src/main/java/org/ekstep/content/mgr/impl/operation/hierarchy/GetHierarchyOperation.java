4d52c1eac7d83ec93cdff57d4b0d860f7e6eddf5
Mahesh Kumar Gangula <mahesh@ilimi.in>
2020-05-13 00:51:02 +0530

    Issue #000 fix: check the children list is not empty.

--- platform-modules/manager/src/main/java/org/ekstep/content/mgr/impl/operation/hierarchy/GetHierarchyOperation.java
+++ platform-modules/manager/src/main/java/org/ekstep/content/mgr/impl/operation/hierarchy/GetHierarchyOperation.java
@@ -420,17 +420,20 @@ public class GetHierarchyOperation extends BaseContentManager {
     }
 
     private void updateLatestLeafNodes(List<Map<String, Object>> children, Map<String, Object> latestLeafNodes) {
-        children.stream().forEach(child -> {
-            if(StringUtils.equalsIgnoreCase("Default", (String) child.getOrDefault("visibility", ""))) {
-                Map<String, Object> metadata = (Map<String, Object>) latestLeafNodes.getOrDefault((String)child.get("identifier"), new HashMap<String, Object>());
-                if(StringUtils.equalsIgnoreCase(ContentAPIParams.Retired.name(), (String) metadata.getOrDefault("status", ""))) {
-                    children.remove(child);
+        if (CollectionUtils.isNotEmpty(children)) {
+            children.stream().forEach(child -> {
+                if(StringUtils.equalsIgnoreCase("Default", (String) child.getOrDefault("visibility", ""))) {
+                    Map<String, Object> metadata = (Map<String, Object>) latestLeafNodes.getOrDefault((String)child.get("identifier"), new HashMap<String, Object>());
+                    if(StringUtils.equalsIgnoreCase(ContentAPIParams.Retired.name(), (String) metadata.getOrDefault("status", ""))) {
+                        children.remove(child);
+                    } else {
+                        child.putAll(metadata);
+                    }
                 } else {
-                    child.putAll(metadata);
+                    updateLatestLeafNodes((List<Map<String, Object>>) child.getOrDefault("children", new ArrayList<Map<String, Object>>()), latestLeafNodes);
                 }
-            } else {
-                updateLatestLeafNodes((List<Map<String, Object>>) child.getOrDefault("children", new ArrayList<Map<String, Object>>()), latestLeafNodes);
-            }
-        });
+            });
+        }
+
     }
 }
