c9d4792a7e262c0cc94d8f6b34008a7264dd82be
Mahesh Kumar Gangula <maheshkumargangula@users.noreply.github.com>
2020-07-15 14:13:56 +0530

    Issue #SC-1810 feat: sync relation cache for collection.

---
+++ platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/shell/RelationCacheSyncCommand.java
@@ -0,0 +1,47 @@
+package org.ekstep.sync.tool.shell;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.ekstep.sync.tool.mgr.RelationCacheSyncManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.shell.core.CommandMarker;
+import org.springframework.shell.core.annotation.CliCommand;
+import org.springframework.shell.core.annotation.CliOption;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class RelationCacheSyncCommand implements CommandMarker {
+
+    @Autowired
+    @Qualifier("relationCacheSyncManager")
+    RelationCacheSyncManager relationCacheSyncManager;
+
+    @CliCommand(value = "sync-collection-rel-cache", help = "Generate the events to push Kafka events for Collection Relation Cache")
+    public void syncRelationCache(
+            @CliOption(key = {
+                    "ids"}, mandatory = false, help = "ids") final String[] ids,
+            @CliOption(key = {
+        "all"}, mandatory = false, unspecifiedDefaultValue="false", specifiedDefaultValue="true",  help = "Generate events for all the Collections") final String forAll,
+            @CliOption(key = {"limit"}, mandatory = false, help = "ignored identifiers to sync") final int limit
+    ) throws Exception  {
+        if (StringUtils.equalsIgnoreCase("true", forAll)) {
+            System.out.println("Generating relations sync events for all collections.");
+            int totalCollections = relationCacheSyncManager.getCollectionCount();
+            int finalLimit = (limit > 0) ? limit : totalCollections;
+            System.out.println("Total Collections: " + totalCollections + " and processing " + finalLimit + " collections.");
+            if (totalCollections > 0) {
+                relationCacheSyncManager.syncAllCollections(totalCollections, finalLimit);
+            }
+        } else if (null != ids && CollectionUtils.isEmpty(Arrays.asList(ids))) {
+            System.out.println("Generating relations sync events for collections with id: " + ids);
+            // TODO: pending.
+        } else {
+            System.out.println("Required options are not available. Please provide 'ids' or 'all'");
+        }
+    }
+}
