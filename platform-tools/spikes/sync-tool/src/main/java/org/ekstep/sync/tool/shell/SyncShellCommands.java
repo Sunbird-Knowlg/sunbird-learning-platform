31905ddcdd17e39ee3b18b6ef8be03528180750d
amitpriyadarshi <darshi.1983@gmail.com>
2020-08-07 17:21:40 +0530

    Issue #SC-1828 feat: DIAL Service to Sunbird Infra - sync dialcode

--- platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/shell/SyncShellCommands.java
+++ platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/shell/SyncShellCommands.java
@@ -140,17 +140,18 @@ public class SyncShellCommands implements CommandMarker {
 	
 	@CliCommand(value = "syncdialcodes", help = "Refresh leafNodes by Id(s) for Collection MimeTypes")
 	public void syncDialcodes(
-			@CliOption(key = {"id","ids"}, mandatory = false, help = "Unique Id of node object") final String[] ids,
-			@CliOption(key = {"filepath"}, mandatory = false, help = "File path") final String filePath) throws Exception {
+			@CliOption(key = {"id","ids"}, mandatory = false, help = "Unique Id of node object") final String[] ids
+			//@CliOption(key = {"filepath"}, mandatory = false, help = "File path") final String filePath) 
+			throws Exception {
 		long startTime = System.currentTimeMillis();
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		LocalDateTime start = LocalDateTime.now();
 
-		if(StringUtils.isNoneBlank(filePath)) {
+		/*if(StringUtils.isNoneBlank(filePath)) {
 			System.out.println("SyncShellCommands:syncDialcodes:filePath:: " + filePath);
 			syncManager.syncDialcodesByFile(filePath, "csv");
 		}
-			
+		*/	
 		if(null != ids && ids.length > 0) {
 			System.out.println("SyncShellCommands:syncDialcodes:ids:: " + ids);
 			syncManager.syncDialcodesByIds(new ArrayList<String>(Arrays.asList(ids)));
