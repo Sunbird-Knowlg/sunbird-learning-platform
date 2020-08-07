2a022cfcbaa6b368c0c3fe5003ae65bfa2142070
Mahesh Kumar Gangula <maheshkumargangula@users.noreply.github.com>
2020-08-07 20:43:56 +0530

    Issue #SC-1810 fix: use link-dialcode action.

--- platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/mgr/RelationCacheSyncManager.java
+++ platform-tools/spikes/sync-tool/src/main/java/org/ekstep/sync/tool/mgr/RelationCacheSyncManager.java
@@ -90,7 +90,7 @@ public class RelationCacheSyncManager {
 
 
     private String generateKafkaEvent(Map<String, Object> rowMap) throws JsonProcessingException {
-        rowMap.put("action", "publish-shallow-content");
+        rowMap.put("action", "link-dialcode");
         rowMap.put("iteration", 1);
         Map<String, Object> event = new HashMap<String, Object>() {{
             put("eid", "BE_JOB_REQUEST");
