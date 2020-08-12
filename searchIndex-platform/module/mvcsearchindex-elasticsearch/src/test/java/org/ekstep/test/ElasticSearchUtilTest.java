6f613edec8ca49756fadef2df9e2778be27709a1
Tanmay Ranjan <tanmay.ranjan9@gmail.com>
2020-08-12 14:30:38 +0530

    ES 7.5 test cases uncommented and removed BaseSearchTest

--- searchIndex-platform/module/mvcsearchindex-elasticsearch/src/test/java/org/ekstep/test/ElasticSearchUtilTest.java
+++ searchIndex-platform/module/mvcsearchindex-elasticsearch/src/test/java/org/ekstep/test/ElasticSearchUtilTest.java
@@ -14,26 +14,45 @@ import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.ekstep.mvcsearchindex.elasticsearch.ElasticSearchUtil;
 import org.ekstep.searchindex.util.CompositeSearchConstants;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * @author pradyumna
  *
  */
-public class ElasticSearchUtilTest extends BaseSearchTest {
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ElasticSearchUtil.class})
+@PowerMockIgnore({"javax.management.*", "sun.security.ssl.*", "javax.net.ssl.*" , "javax.crypto.*"})
+public class ElasticSearchUtilTest  {
 
 
 	private static ObjectMapper mapper = new ObjectMapper();
 	private static Random random = new Random();
 
+    @Before
+    public void setup()  {
+        MockitoAnnotations.initMocks(this);
+    }
 
 	@Test
 	public void testAddDocumentWithId() throws Exception {
 		Map<String, Object> content = getContentTestRecord();
 		String id = (String) content.get("identifier");
-		addToIndex(id, content);
+		// addToIndex(id, content);
+        String jsonIndexDocument = mapper.writeValueAsString(content);
+        PowerMockito.mockStatic(ElasticSearchUtil.class);
+        PowerMockito.doNothing().when(ElasticSearchUtil.class);
+        ElasticSearchUtil.addDocumentWithId(CompositeSearchConstants.MVC_SEARCH_INDEX,
+                id, jsonIndexDocument);
 		when(ElasticSearchUtil.getDocumentAsStringById(Mockito.anyString(),Mockito.anyString())).thenReturn(id);
 		String doc = ElasticSearchUtil.getDocumentAsStringById(CompositeSearchConstants.MVC_SEARCH_INDEX, id);
 		assertTrue(StringUtils.contains(doc, id));
@@ -42,7 +61,11 @@ public class ElasticSearchUtilTest extends BaseSearchTest {
 	public void testUpdateDocument() throws Exception {
 		Map<String, Object> content = getContentTestRecord();
 		String id = (String) content.get("identifier");
-		addToIndex(id, content);
+        String jsonIndexDocument = mapper.writeValueAsString(content);
+        PowerMockito.mockStatic(ElasticSearchUtil.class);
+        PowerMockito.doNothing().when(ElasticSearchUtil.class);
+        ElasticSearchUtil.addDocumentWithId(CompositeSearchConstants.MVC_SEARCH_INDEX,
+                id, jsonIndexDocument);
 		content.put("name", "Content_" + System.currentTimeMillis() + "_name");
 		PowerMockito.mockStatic(ElasticSearchUtil.class);
 		PowerMockito.doNothing().when(ElasticSearchUtil.class);
@@ -68,5 +91,12 @@ public class ElasticSearchUtilTest extends BaseSearchTest {
 		map.put("status", "Draft");
 		return map;
 	}
+    /*private static void addToIndex(String uniqueId, Map<String, Object> doc) throws Exception {
+        String jsonIndexDocument = mapper.writeValueAsString(doc);
+        PowerMockito.mockStatic(ElasticSearchUtil.class);
+        PowerMockito.doNothing().when(ElasticSearchUtil.class);
+        ElasticSearchUtil.addDocumentWithId(CompositeSearchConstants.MVC_SEARCH_INDEX,
+                uniqueId, jsonIndexDocument);
+    }*/
 
 }
