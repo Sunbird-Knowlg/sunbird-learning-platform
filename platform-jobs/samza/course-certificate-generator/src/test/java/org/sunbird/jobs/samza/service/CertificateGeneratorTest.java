8cab4e481c9ee6ef7447f5651fb5d960be7cee33
NehaVerma <neha0305verma@gmail.com>
2020-08-17 12:26:47 +0530

    SB-20250 : Enable Audit event while generating course certificate

--- platform-jobs/samza/course-certificate-generator/src/test/java/org/sunbird/jobs/samza/service/CertificateGeneratorTest.java
+++ platform-jobs/samza/course-certificate-generator/src/test/java/org/sunbird/jobs/samza/service/CertificateGeneratorTest.java
@@ -2,9 +2,16 @@ package org.sunbird.jobs.samza.service;
 
 import com.datastax.driver.core.Session;
 import org.apache.samza.config.Config;
+import org.apache.samza.system.SystemStream;
+import org.apache.samza.task.MessageCollector;
+import org.ekstep.common.Platform;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -13,6 +20,7 @@ import org.sunbird.jobs.samza.service.util.CertificateGenerator;
 import org.sunbird.jobs.samza.util.RedisConnect;
 import redis.clients.jedis.Jedis;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,6 +30,17 @@ import java.util.Map;
 @PowerMockIgnore({"javax.management.*", "sun.security.ssl.*", "javax.net.ssl.*" , "javax.crypto.*"})
 public class CertificateGeneratorTest {
 
+    private CertificateGenerator certificateGenerator;
+
+    private MessageCollector collectorMock;
+    @Mock(name = "certificateAuditEventStream")
+    SystemStream certificateAuditEventStream = new SystemStream("kafka", Platform.config.getString("certificate.audit.event.topic.name"));
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+        collectorMock = Mockito.mock(MessageCollector.class);
+    }
 
     @Test
     public void testGenerateCertificate() {
@@ -54,7 +73,50 @@ public class CertificateGeneratorTest {
         request.put("userId", "8454cb21-3ce9-4e30-85b5-fade097880d8");
         request.put("courseId", "do_112876410268794880162");
 
-        generator.generate(request);
+        generator.generate(request, collectorMock);
 
+    }
+
+    @Test
+    public void testPushAuditEvent() throws Exception{
+        mockAllSession();
+        Method pushAuditEventMethod = CertificateGenerator.class.getDeclaredMethod("pushAuditEvent", String.class, String.class, String.class, Map.class, MessageCollector.class);
+        pushAuditEventMethod.setAccessible(true);
+        Map<String, Object> certificate = new HashMap<String, Object>() {{
+            put("id", "4a3538d5-f671-41dd-b200-46f0853de0cc");
+        }};
+        String batchId = "0128057392291102720";
+        String courseId = "do_1127212344324751361295";
+        String userId = "874ed8a5-782e-4f6c-8f36-e0288455901e";
+        pushAuditEventMethod.invoke(certificateGenerator, userId, courseId, batchId, certificate, collectorMock);
+    }
+
+    @Test
+    public void testGenerateAuditEvent() throws Exception{
+        mockAllSession();
+        Method generateAuditEventMethod = CertificateGenerator.class.getDeclaredMethod("generateAuditEvent", String.class, String.class, String.class, Map.class);
+        generateAuditEventMethod.setAccessible(true);
+        Map<String, Object> certificate = new HashMap<String, Object>() {{
+            put("id", "4a3538d5-f671-41dd-b200-46f0853de0cc");
+        }};
+        String batchId = "0128057392291102720";
+        String courseId = "do_1127212344324751361295";
+        String userId = "874ed8a5-782e-4f6c-8f36-e0288455901e";
+        Map<String, Object> certAuditEvent = (Map<String, Object>) generateAuditEventMethod.invoke(certificateGenerator, userId, courseId, batchId, certificate);
+        Assert.assertTrue(certAuditEvent.containsKey("actor"));
+        Assert.assertTrue(certAuditEvent.containsKey("edata"));
+        Assert.assertTrue(certAuditEvent.containsKey("object"));
+        Assert.assertTrue(certAuditEvent.containsKey("context"));
+        Assert.assertTrue(certAuditEvent.containsKey("eid") && certAuditEvent.containsValue("AUDIT"));
+        Assert.assertTrue(certAuditEvent.containsKey("mid"));
+    }
+
+    public void mockAllSession(){
+        Config config = Mockito.mock(Config.class);
+        Mockito.when(config.get("redis.host")).thenReturn("localhost");
+        Mockito.when(config.getInt("redis.port")).thenReturn(6379);
+        Jedis redisConnect = new RedisConnect(config).getConnection();
+        Session session = Mockito.mock(Session.class);
+        certificateGenerator = PowerMockito.spy(new CertificateGenerator(redisConnect, session, certificateAuditEventStream));
     }
 }
