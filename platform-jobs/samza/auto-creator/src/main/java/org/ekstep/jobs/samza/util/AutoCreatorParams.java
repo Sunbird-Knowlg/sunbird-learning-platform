0af71969562ee5d64723decaccdc9b0fa213afa1
Kumar Gauraw <er.krgauraw@gmail.com>
2020-08-23 23:59:04 +0530

    Issue #SC-1847 feat: added enhancement to support content stage

--- platform-jobs/samza/auto-creator/src/main/java/org/ekstep/jobs/samza/util/AutoCreatorParams.java
+++ platform-jobs/samza/auto-creator/src/main/java/org/ekstep/jobs/samza/util/AutoCreatorParams.java
@@ -3,6 +3,6 @@ package org.ekstep.jobs.samza.util;
 public enum AutoCreatorParams {
 	channel, id, objectType, metadata, artifactUrl, status, request, filters, origin, originData, count, content, identifier,
 	repository, pkgVersion, lastPublishedBy, children, childNodes, rootId,unitId, context, collection,
-	processId, versionKey, importError, textbookInfo, unitIdentifiers, mimeType
+	processId, versionKey, importError, textbookInfo, unitIdentifiers, mimeType, stage
 
 }
