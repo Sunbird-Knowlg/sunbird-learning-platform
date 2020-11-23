d9540fad4c6ba8d1f5ccafad33795fe5428d0f95
Pradyumna Nagendra <pradyumnan@ilimi.in>
2020-11-23 11:27:10 +0530

    Issue #000 fix: Certificate genration issue fix

--- platform-jobs/samza/course-certificate-generator-v2/src/main/java/org/sunbird/jobs/samza/service/util/CertificateGenerator.java
+++ platform-jobs/samza/course-certificate-generator-v2/src/main/java/org/sunbird/jobs/samza/service/util/CertificateGenerator.java
@@ -54,8 +54,6 @@ public class CertificateGenerator {
     private static final String USER_COURSES_TABLE = "user_enrolments";
     private SimpleDateFormat formatter = null;
     private SimpleDateFormat dateFormatter = null;
-    private static final String ES_INDEX_NAME = "user-courses";
-    private static final String ES_DOC_TYPE = "_doc";
     private static final String CERTIFICATE_BASE_PATH = Platform.config.hasPath("certificate.base_path")
             ? Platform.config.getString("certificate.base_path"): "http://localhost:9000/certs";
     protected static final String KP_CONTENT_SERVICE_BASE_URL = Platform.config.hasPath("kp.content_service.base_url")
@@ -84,8 +82,10 @@ public class CertificateGenerator {
     private static final String CREATE_USER_FEED_URL = Platform.config.hasPath("create.user.feed.url")
             ? Platform.config.getString("create.user.feed.url"): "/private/user/feed/v1/create";
 
+    private static final String ASSET_READ_URL = Platform.config.hasPath("asset.read.url")
+            ? Platform.config.getString("asset.read.url"): "/asset/v4/read/";
+
     public CertificateGenerator(Jedis redisConnect, Session cassandraSession) {
-        ElasticSearchUtil.initialiseESClient(ES_INDEX_NAME, Platform.config.getString("search.es_conn_info"));
         formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
         dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
         mapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
@@ -95,7 +95,6 @@ public class CertificateGenerator {
     }
 
     public CertificateGenerator(Jedis redisConnect, Session cassandraSession, SystemStream certificateAuditEventStream) {
-        ElasticSearchUtil.initialiseESClient(ES_INDEX_NAME, Platform.config.getString("search.es_conn_info"));
         formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
         dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
         mapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
@@ -110,11 +109,12 @@ public class CertificateGenerator {
         String userId = (String) edata.get("userId");
         String courseId = (String) edata.get("courseId");
         Map<String, Object> template = (Map<String, Object>) edata.get("template");
-        String templateUrl = (String)template.get("url");
+        String templateUrl = (String)template.getOrDefault("url", getCertTemplate((String) template.getOrDefault("identifier", "")));
         if(StringUtils.isBlank(templateUrl) || !StringUtils.endsWith(templateUrl, ".svg")) {
         	LOGGER.info("CertificateGenerator:generate: Certificate is not generated for batchId : " + batchId + ", courseId : " + courseId + " and userId : " + userId + ". TemplateId: "+ (String) template.get("identifier") + " with Url: " + templateUrl + " is not supported.");
         	return;
         }
+        template.put("url", templateUrl);
         boolean reIssue = (null != edata.get(CourseCertificateParams.reIssue.name()))
                 ? (Boolean) edata.get(CourseCertificateParams.reIssue.name()): false;
 
@@ -208,7 +208,6 @@ public class CertificateGenerator {
                         put(CourseCertificateParams.batchId.name(), batchId);
                     }};
                     SunbirdCassandraUtil.update(cassandraSession, KEYSPACE, USER_COURSES_TABLE, dataToUpdate, dataToSelect);
-                    updatedES(ES_INDEX_NAME, ES_DOC_TYPE, dataToUpdate, dataToSelect);
                 }
 
                 pushAuditEvent(userId, courseId, batchId, certificate, collector);
@@ -437,20 +436,6 @@ public class CertificateGenerator {
     }
 
 
-    private void updatedES(String index, String type, Map<String, Object> dataToUpdate, Map<String, Object> dataToSelect) {
-        try {
-            String key = dataToSelect.entrySet().stream().map(entry -> (String) entry.getValue()).collect(Collectors.joining("_"));
-            String documentJson = ElasticSearchUtil.getDocumentAsStringById(index, type, key);
-            Map<String, Object> document = new HashMap<>();
-            if(StringUtils.isNotBlank(documentJson))
-                document = mapper.readValue(documentJson, Map.class);
-            document.putAll(dataToUpdate);
-            ElasticSearchUtil.updateDocument(index, type, mapper.writeValueAsString(document), key);
-        } catch (Exception e) {
-            LOGGER.error("Error while update to ES: ", e);
-        }
-    }
-
     private void populateCreatedCertificate(List<Map<String, String>> updatedCerts, Map<String, Object> certificate, String certificateName, Date issuedOn, boolean reIssue) {
         updatedCerts.add(new HashMap<String, String>(){{
             put(CourseCertificateParams.name.name(), certificateName);
@@ -573,5 +558,27 @@ public class CertificateGenerator {
             e.printStackTrace();
             LOGGER.error("CertificateGenerator:createUserFeed: createUserFeed failed : ", e);
         }
+    }
+
+    /**
+     * Get Certificate Template
+     * @param id
+     * @return
+     */
+    protected static String getCertTemplate(String id) {
+        try{
+            if(StringUtils.isNotBlank(id)) {
+                String url = KP_CONTENT_SERVICE_BASE_URL + ASSET_READ_URL + id;
+                HttpResponse<String> httpResponse = Unirest.get(url).header("Content-Type", "application/json").asString();
+                if(200 == httpResponse.getStatus()) {
+                    Response response = mapper.readValue(httpResponse.getBody(), Response.class);
+                    Map<String, Object> certTemplate = (Map<String, Object>) response.getResult().getOrDefault("content", new HashMap<>());
+                    return (String) certTemplate.getOrDefault("artifactUrl", "");
+                }
+            }
+        } catch(Exception e) {
+            LOGGER.error("Error while fetching the certificate template : " , e);
+        }
+        return null;
     }
 }
