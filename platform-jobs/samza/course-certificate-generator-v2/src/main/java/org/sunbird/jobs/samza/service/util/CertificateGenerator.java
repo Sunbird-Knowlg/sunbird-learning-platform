705dd6dd5b957dd3da92f3b6436ee813ba1bc769
Pradyumna Nagendra <pradyumnan@ilimi.in>
2020-12-02 15:52:16 +0530

    Issue #SB-21864 fix: User feed for certificates

--- platform-jobs/samza/course-certificate-generator-v2/src/main/java/org/sunbird/jobs/samza/service/util/CertificateGenerator.java
+++ platform-jobs/samza/course-certificate-generator-v2/src/main/java/org/sunbird/jobs/samza/service/util/CertificateGenerator.java
@@ -213,9 +213,10 @@ public class CertificateGenerator {
 
                 pushAuditEvent(userId, courseId, batchId, certificate, collector);
 
-                if(addCertificateToUser(certificate, courseId, batchId, oldId, recipientName, (String)certTemplate.get("name")) && certificateGenerateNotificationEnable) {
-                	notifyUser(userId, certTemplate, courseName, userResponse, issuedOn);
+                if(addCertificateToUser(certificate, courseId, batchId, oldId, recipientName, (String)certTemplate.get("name"))) {
                     createUserFeed(userId, courseId, courseName, issuedOn);
+                    if(certificateGenerateNotificationEnable)
+                	    notifyUser(userId, certTemplate, courseName, userResponse, issuedOn);
                 }
             } else {
                 LOGGER.info("CertificateGenerator:generateCertificate: Error while generation certificate for batchId : " + batchId +  ", courseId : " + courseId + " and userId : " + userId + " with error response : "  +  + httpResponse.getStatus()  + " :: " + httpResponse.getBody());
