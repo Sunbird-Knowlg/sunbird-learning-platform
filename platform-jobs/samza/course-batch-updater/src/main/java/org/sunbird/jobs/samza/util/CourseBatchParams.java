8ddc14f4c7a42114f643e8abff937d8b7a5a1970
Pradyumna Nagendra <pradyumnan@ilimi.in>
2021-03-08 11:09:25 +0530

    Issue #SB-21959 feat: Removed course batch updater samza job

--- platform-jobs/samza/course-batch-updater/src/main/java/org/sunbird/jobs/samza/util/CourseBatchParams.java
+++
@@ -1,5 +0,0 @@
-package org.sunbird.jobs.samza.util;
-
-public enum CourseBatchParams {
-    id, iteration, edata, object, open, batchId, courseId, enrollmentType, status, leafNodes, actor, type, action, userIds, reIssue, pdata, ver
-}
