f6f0919759560b49191653259d46979e8dac0fc3
aimansharief <110388201+aimansharief@users.noreply.github.com>
2023-01-18 16:30:13 +0530

    fix : updated changes in build.sh to build the dockerfile

--- pipelines/build/build.sh
+++ pipelines/build/build.sh
@@ -7,5 +7,5 @@ name=$2
 node=$3
 org=$4
 
-docker build -f build/${name}/Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
+docker build -f pipelines/build/${name}/Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
