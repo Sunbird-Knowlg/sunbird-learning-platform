cc973d11bf34c8cffc33da8af3c3cdbfadd1c4cf
Mahesh Kumar Gangula <maheshkumargangula@users.noreply.github.com>
2023-02-10 18:24:44 +0530

    Issue KN-808 feat: remove content-tool module.

--- platform-tools/spikes/content-tool/src/main/java/org/sunbird/content/tool/util/ShellCommandUtils.java
+++
@@ -1,9 +0,0 @@
-package org.sunbird.content.tool.util;
-
-public class ShellCommandUtils {
-
-    public static void print(String message) {
-        System.out.print("\r");
-        System.out.print(message);
-    }
-}
